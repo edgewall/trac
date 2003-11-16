e34da7822b0faae2f0201fa272d3f8b05710d8ab
jonas <jonas@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2003-11-16 21:12:15 +0000

    Added some icons to the timeline view
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@40 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- templates/timeline.cs
+++ templates/timeline.cs
@@ -25,47 +25,47 @@ view wiki changes<br>
 
 <table>
 
-<?cs def:day_separator(date, time) ?>
+<?cs def:day_separator(date) ?>
   <?cs if: $date != $current_date ?>
     <?cs set: $current_date = $date ?>
     <tr>
-      <td colspan="2" class="timeline-day"><?cs var:date ?>, <?cs var:time ?>:</td>
+      <td colspan="2" class="timeline-day"><?cs var:date ?>:</td>
     </tr>
   <?cs /if ?>
 <?cs /def ?>
 
 <?cs each:item = timeline.items ?>
 
-  <?cs call:day_separator(item.date, item.time) ?>
+  <?cs call:day_separator(item.date) ?>
 <!-- Changeset -->
   <?cs if:item.type == #1 ?>
     <tr>
       <td><?cs var:item.time?></td>
-      <td>change set [<a href="<?cs var:item.changeset_href ?>"><?cs var:item.data?></a>] by <?cs var:item.author ?>: <?cs var:item.message?></td>
+      <td><img align="left" src="<?cs var:htdocs_location?>/changeset.png">: change set [<a href="<?cs var:item.changeset_href ?>"><?cs var:item.data?></a>] by <?cs var:item.author ?>: <?cs var:item.message?></td>
     </tr>
 <!-- New ticket -->
   <?cs elif:item.type == #2 ?>
     <tr>
       <td><?cs var:item.time?></td>
-      <td>ticket <a href="<?cs var:item.ticket_href ?>">#<?cs var:item.data?></a> created by <?cs var:item.author ?>: <?cs var:item.message?>.</td>
+      <td><img align="left" src="<?cs var:htdocs_location?>/newticket.png">: ticket <a href="<?cs var:item.ticket_href ?>">#<?cs var:item.data?></a> created by <?cs var:item.author ?>: <?cs var:item.message?>.</td>
     </tr>
 <!-- Closed ticket -->
   <?cs elif:item.type == #3 ?>
     <tr>
       <td><?cs var:item.time?></td>
-      <td>ticket <a href="<?cs var:item.ticket_href ?>">#<?cs var:item.data?></a> closed by <?cs var:item.author ?>.</td>
+      <td><img align="left" src="<?cs var:htdocs_location?>/closedticket.png">: ticket <a href="<?cs var:item.ticket_href ?>">#<?cs var:item.data?></a> closed by <?cs var:item.author ?>.</td>
     </tr>
 <!-- Reopened ticket -->
   <?cs elif:item.type == #4 ?>
     <tr>
       <td><?cs var:item.time?></td>
-      <td>ticket <a href="<?cs var:item.ticket_href ?>">#<?cs var:item.data?></a> reopened by <?cs var:item.author ?>.</td>
+      <td><img align="left" src="<?cs var:htdocs_location?>/newticket.png">: ticket <a href="<?cs var:item.ticket_href ?>">#<?cs var:item.data?></a> reopened by <?cs var:item.author ?>.</td>
     </tr>
 <!-- Wiki change -->
   <?cs elif:item.type == #5 ?>
     <tr>
       <td><?cs var:item.time?></td>
-      <td><a href="<?cs var:item.wiki_href ?>"><?cs var:item.data?></a> edited by <?cs var:item.author ?>.</td>
+      <td><img align="left" src="<?cs var:htdocs_location?>/wiki.png">: <a href="<?cs var:item.wiki_href ?>"><?cs var:item.data?></a> edited by <?cs var:item.author ?>.</td>
     </tr>
   <?cs /if ?>
 <?cs /each ?>
