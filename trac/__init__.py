a1e38bc7f25be5fad04b7962d3406b7d795a3ec4
daniel <daniel@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2004-02-16 19:36:02 +0000

    Poor little __init_.py got lost
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@152 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/__init__.py
+++ trac/__init__.py
@@ -9,12 +9,29 @@ U{http://trac.edgewall.com/}
 """
 __docformat__ = 'epytext en'
 
+__version__ = '0.1'
 __url__ = 'http://trac.edgewall.com/'
 __copyright__ = '(C) 2003 Edgewall Research & Development'
-__license__ = 'GNU General Public License'
-__version__ = '0.1'
-__credits__ = """
-Visit the Trac Project at %s
+__license__ = 'GNU General Public License version 2'
+__license_long__ = """
+ Copyright (C) 2003, 2004 Edgewall Research & Development
+ Copyright (C) 2003, 2004 Jonas Borgström <jonas@edgewall.com>
+ Copyright (C) 2003, 2004 Daniel Lundin <daniel@edgewall.com>
+
+ Trac is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2 of
+ the License, or (at your option) any later version.
+
+ Trac is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA."""
+__credits__ = """Visit the Trac Project at %s
 
 Trac is brought to you by:
 -----------------------------------------------------------------
