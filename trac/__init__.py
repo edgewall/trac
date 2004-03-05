2a946a8324522583facd042986281a1b7f6aec17
daniel <daniel@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2004-03-05 15:44:13 +0000

    Trunk is 0.6 now.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@243 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/__init__.py
+++ trac/__init__.py
@@ -9,7 +9,7 @@ U{http://trac.edgewall.com/}
 """
 __docformat__ = 'epytext en'
 
-__version__ = '0.5.2'
+__version__ = '0.6-pre'
 __url__ = 'http://trac.edgewall.com/'
 __copyright__ = '(C) 2003 Edgewall Software'
 __license__ = 'GNU General Public License version 2'
