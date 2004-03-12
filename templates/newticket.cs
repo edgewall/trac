1b19132324049068dd4c2aa5fd0149c17ede1b0d
daniel <daniel@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2004-03-12 05:04:42 +0000

    A better layout for 'New ticket', using tables, especially in w3m and lynx. Fixes #138.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@266 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- templates/newticket.cs
+++ templates/newticket.cs
@@ -1,5 +1,6 @@
 <?cs include "header.cs" ?>
 <?cs include "macros.cs" ?>
+
 <div id="page-content">
 <div id="subheader-links">
 <br />
@@ -7,95 +8,101 @@
  <div id="main">
   <div id="main-content">
 
-<h3>New Ticket:</h3>
+<h3>Create New Ticket:</h3>
 
 <form action="<?cs var:cgi_location ?>" method="post">
-  <div id="nt-props-left">
-    <input type="hidden" name="mode" value="ticket" />
-    <input type="hidden" name="action" value="create" />
-    <input type="hidden" name="status" value="new" />
-    <div class="nt-prop">
-      <span class="nt-label">Reporter:</span>
-      <span class="nt-widget">
-        <input type="text" name="reporter"
-              value="<?cs var:trac.authname ?>" />
-      </span>
-    </div>
-    <div class="nt-prop">
-      <span class="nt-label">Component:</span>
-      <span class="nt-widget">
-        <?cs call:hdf_select(newticket.components, "component",
-                             newticket.default_component) ?>
-      </span>
-    </div>
-    <div class="nt-prop">
-      <span class="nt-label">Version:</span>
-      <span class="nt-widget">
-        <?cs call:hdf_select(newticket.versions, "version",
-                             newticket.default_version) ?>
-      </span>
-    </div>
-    <div class="nt-prop">
-      <span class="nt-label">Severity:</span>
-      <span class="nt-widget">
-        <?cs call:hdf_select(enums.severity, "severity",
-                             newticket.default_severity) ?>
-      </span>
-    </div>
-  </div>
-  <div id="nt-props-right">
-    <div class="nt-prop">
-    <span class="nt-label">Priority:</span>
-      <span class="nt-widget">
-        <?cs call:hdf_select(enums.priority, "priority", 
-                             newticket.default_priority) ?>
-      </span>
-    </div>
-    <div class="nt-prop">
-      <span class="nt-label">Milestone:</span>
-      <span class="nt-widget">
-        <?cs call:hdf_select(newticket.milestones, "milestone",
-                             newticket.default_milestone) ?>
-      </span>
-    </div>
-    <div class="nt-prop">
-      <span class="nt-label">Assign to:</span>
-      <span class="nt-widget"><input type="text" name="owner" /></span>
-    </div>
-  </div>
-  <div><br style="clear: both"/></div>
-  <div id="nt-props-middle">
-    <div class="nt-prop">
-      <span class="nt-label">Cc:</span>
-      <span class="nt-widget">
-        <input type="text" name="cc" size="50" />
-      </span>
-    </div>
-    <div class="nt-prop">
-      <span class="nt-label">URL:</span>
-      <span class="nt-widget">
-        <input type="text" name="url" size="50" />
-      </span>
-    </div>
-    <div class="nt-prop">
-      <span class="nt-label">Summary:</span>
-      <span class="nt-widget">
-        <input type="text" name="summary" size="50" />
-      </span>
-    </div>
-  </div>
-  <div id="nt-props-bottom">
-    <div class="nt-prop">
-      <span class="nt-label">Description:</span>
-      <span class="nt-widget">
+ <input type="hidden" name="mode" value="ticket" />
+ <input type="hidden" name="action" value="create" />
+ <input type="hidden" name="status" value="new" />
+
+ <table id="nt-upper">
+  <tr class="nt-prop">
+   <td class="nt-label">Reporter:</td>
+   <td class="nt-widget">
+    <input type="text" name="reporter" value="<?cs var:trac.authname ?>" />
+   </td>
+   <td class="nt-prop-sep">&nbsp;</td>
+   <td class="nt-label">Priority:</td>
+   <td class="nt-widget">
+    <?cs call:hdf_select(enums.priority, "priority", 
+                         newticket.default_priority) ?>
+   </td>
+  </tr>
+  <tr class="nt-prop">
+   <td class="nt-label">Component:</td>
+   <td class="nt-widget">
+    <?cs call:hdf_select(newticket.components, "component",
+                        newticket.default_component) ?>
+   </td>
+   <td class="nt-prop-sep">&nbsp;</td>
+   <td class="nt-label">Milestone:</td>
+   <td class="nt-widget">
+    <?cs call:hdf_select(newticket.milestones, "milestone",
+                         newticket.default_milestone) ?>
+   </td>
+  </tr>
+  <tr class="nt-prop">
+   <td class="nt-label">Version:</td>
+   <td class="nt-widget">
+    <?cs call:hdf_select(newticket.versions, "version",
+                         newticket.default_version) ?>
+   </td>
+   <td class="nt-prop-sep">&nbsp;</td>
+   <td class="nt-label">Assign to:</td>
+   <td class="nt-widget"><input type="text" name="owner" /></td>
+  </tr>
+  <tr class="nt-prop">
+   <td class="nt-label">Severity:</td>
+    <td class="nt-widget">
+     <?cs call:hdf_select(enums.severity, "severity",
+                          newticket.default_severity) ?>
+    </td>
+   <td class="nt-prop-sep">&nbsp;</td>
+    <td class="nt-label"></td>
+    <td class="nt-widget"></td>
+   </tr>
+ <tr>
+  <td colspan="2" id="nt-row-sep">&nbsp;</td>
+  <td class="nt-prop-sep">&nbsp;</td>
+  <td colspan="2" id="nt-row-sep">&nbsp</td>
+</tr>
+</table>
+
+<table id="nt-lower">
+ <tr>
+  <td class="nt-label">Cc:</td>
+  <td class="nt-widget">
+   <input type="text" name="cc" size="50" />
+  </td>
+ </tr>
+ <tr>
+  <td class="nt-label">URL:</td>
+  <td class="nt-widget">
+   <input type="text" name="url" size="50" />
+  </td>
+ </tr>
+ <tr>
+  <td class="nt-label">Summary:</td>
+  <td class="nt-widget">
+   <input type="text" name="summary" size="50" />
+  </td>
+ </tr>
+ <tr><td colspan="2" id="nt-row-sep">&nbsp;<hr class="hide" /></td></tr>
+ <tr>
+  <td colspan="2" id="nt-descr">Description:</td>
+ </tr>
+ <tr>
+  <td colspan="2" class="nt-widget">
         <textarea name="description" rows="10" cols="66"></textarea>
-      </span>
-    </div>
-    <div id="nt-submit">
-      <input type="reset" value="Reset" />&nbsp;
-      <input type="submit" value="Submit ticket" />
-    </div>
-  </div>
+  </td>
+ </tr>
+ <tr>
+  <td colspan="2" id="nt-submit">
+   <input type="reset" value="Reset" />&nbsp;
+   <input type="submit" value="Submit ticket" />
+  </td>
+ </tr>
+</table>
 </form>
 
 <div id="help">
