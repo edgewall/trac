6298729289f3f6dd0449c5d3023749e90bf2c2b3
daniel <daniel@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2004-05-13 19:49:49 +0000

    Added support for URL arguments in inline image URL's. Closes #358.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@503 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/Wiki.py
+++ trac/Wiki.py
@@ -230,7 +230,7 @@ class Formatter(CommonFormatter):
               r"""(?P<heading>^\s*(?P<hdepth>=+)\s.*\s(?P=hdepth)$)""",
               r"""(?P<list>^(?P<ldepth>\s+)(?:\*|[0-9]+\.) )""",
               r"""(?P<indent>^(?P<idepth>\s+)(?=[^\s]))""",
-              r"""(?P<imgurl>([a-z]+://[^ ]+)(\.png|\.jpg|\.jpeg|\.gif))""",
+              r"""(?P<imgurl>([a-z]+://[^ ]+)(\.png|\.jpg|\.jpeg|\.gif)(\\?[^\s])*)""",
               r"""(?P<url>([a-z]+://[^ ]+[^\., \)\]\}]))""",
               r"""(?P<last_table_cell>\|\|$)""",
               r"""(?P<table_cell>\|\|)"""]
