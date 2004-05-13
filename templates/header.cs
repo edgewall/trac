8c1dbe695028d3c12f8a5bd34ab19b1651158c60
daniel <daniel@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2004-05-13 19:15:03 +0000

    Missing slash in help/guide and faq links.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@499 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- templates/header.cs
+++ templates/header.cs
@@ -70,9 +70,9 @@
       logged in as <?cs var:trac.authname ?> </li>
       <li><a href="<?cs var:trac.href.logout ?>">Logout</a>
     <?cs /if ?></li>
-    <li><a accesskey="6" href="<?cs var:trac.href.wiki ?>TracGuide">Help/Guide</a></li>
-    <li style="display: none"><a accesskey="5" href="<?cs var:trac.href.wiki ?>TracFaq">FAQ</a></li>
-    <li style="display: none"><a accesskey="0" href="<?cs var:trac.href.wiki ?>TracAccessibility">Accessibility</a></li>
+    <li><a accesskey="6" href="<?cs var:trac.href.wiki ?>/TracGuide">Help/Guide</a></li>
+    <li style="display: none"><a accesskey="5" href="http://projects.edgewall.com/trac/wiki/TracFaq">FAQ</a></li>
+    <li style="display: none"><a accesskey="0" href="<?cs var:trac.href.wiki ?>/TracAccessibility">Accessibility</a></li>
     <li class="last"><a accesskey="9" href="<?cs var:trac.href.about ?>">About Trac</a></li>
    </ul>
   </div>
