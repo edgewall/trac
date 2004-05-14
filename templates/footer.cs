8393c952ca4774fb353ff36f231785a94a3095bc
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2004-05-14 16:39:57 +0000

    * Cleanup markup and styles used in the page header and footer  * Fix ticket #372
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@516 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- templates/footer.cs
+++ templates/footer.cs
@@ -1,17 +1,17 @@
-<hr class="hide"/>
 <div id="footer">
- <a class="noline" href="http://trac.edgewall.com/"><img id="footer-logo"
-     src="<?cs var:htdocs_location?>/trac_logo_mini.png" 
-      width="107" height="31" alt="[Trac Logo] " /></a> 
-  <div id="footer-left">
-    Powered by <a href="http://trac.edgewall.com/">Trac <?cs var:trac.version ?></a><br />
-    By <a href="http://www.edgewall.com/">Edgewall Software</a>.
-  </div>
-  <div id="footer-right">
-   Visit the Trac open source project at <br />
-   <a href="http://trac.edgewall.com/">http://trac.edgewall.com/</a>
-  </div>
-  <br style="clear: both" />
+ <hr />
+ <a id="tracpowered" href="http://trac.edgewall.com/"><img src="<?cs
+     var:$htdocs_location ?>/trac_logo_mini.png" height="30" width="107"
+     alt="Trac Powered"/></a>
+ <p class="left">
+  Powered by <a href="http://trac.edgewall.com/">Trac <?cs
+  var:trac.version ?></a><br />
+  By <a href="http://www.edgewall.com/">Edgewall Software</a>.
+ </p>
+ <p class="right">
+  Visit the Trac open source project at <br />
+  <a href="http://trac.edgewall.com/">http://trac.edgewall.com/</a>
+ </p>
 </div>
 
  </body>
--- templates/header.cs
+++ templates/header.cs
@@ -1,78 +1,74 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/2000/REC-xhtml1-20000126/DTD/xhtml1-strict.dtd">
 <html>
-  <head>
-    <?cs if $project.name ?>
-      <title><?cs var:project.name?><?cs if title 
-                     ?>: <?cs var:title ?><?cs /if ?> - Trac</title>
-    <?cs else ?>
-      <title>Trac : <?cs var:title ?></title>
-    <?cs /if ?>
-    <style type="text/css">
-      <!--
-      @import url("<?cs var:$htdocs_location ?>/css/trac.css");
-      @import url("<?cs var:$htdocs_location ?>/css/code.css");
-      <?cs if:trac.active_module == 'browser' || trac.active_module == 'log' || trac.active_module == 'file'?>
-      @import url("<?cs var:$htdocs_location ?>/css/browser.css");
-      <?cs elif:trac.active_module == 'timeline' ?>
-      @import url("<?cs var:$htdocs_location ?>/css/timeline.css");
-      <?cs elif:trac.active_module == 'changeset' || trac.active_module == 'wiki' ?>
-      @import url("<?cs var:$htdocs_location ?>/css/changeset.css");
-      <?cs elif:trac.active_module == 'newticket' || trac.active_module == 'ticket' ?>
-      @import url("<?cs var:$htdocs_location ?>/css/ticket.css");
-      <?cs elif:trac.active_module == 'report' ?>
-      @import url("<?cs var:$htdocs_location ?>/css/report.css");
-      <?cs elif:trac.active_module == 'search' ?>
-      @import url("<?cs var:$htdocs_location ?>/css/search.css");
-      <?cs /if ?>
-      /* Dynamically/template-generated CSS below */
-      #navbar { background: url("<?cs var:$htdocs_location ?>/topbar_gradient.png") top left #f7f7f7 }
-      #navbar a { background: url(<?cs var:$htdocs_location ?>/dots.gif) top left no-repeat; }
-      #navbar a.active,#navbar a.active:visited { background: url("<?cs var:$htdocs_location ?>/topbar_active.png") top left repeat-x #d7d7d7;}
-       -->
-    </style>
-    <script src="<?cs var:$htdocs_location ?>/trac.js" type="text/javascript"></script>
-</head>
+ <head>
+  <?cs if:project.name ?>
+   <title><?cs var:project.name?><?cs if:title ?>: <?cs var:title ?><?cs
+   /if ?> - Trac</title>
+  <?cs else ?>
+   <title>Trac: <?cs var:title ?></title>
+  <?cs /if ?>
+  <style type="text/css">
+   @import url("<?cs var:htdocs_location ?>/css/trac.css");
+   @import url("<?cs var:htdocs_location ?>/css/code.css");
+   <?cs if:trac.active_module == 'browser' || trac.active_module == 'log' || trac.active_module == 'file'?>
+   @import url("<?cs var:htdocs_location ?>/css/browser.css");
+   <?cs elif:trac.active_module == 'timeline' ?>
+   @import url("<?cs var:htdocs_location ?>/css/timeline.css");
+   <?cs elif:trac.active_module == 'changeset' || trac.active_module == 'wiki' ?>
+   @import url("<?cs var:htdocs_location ?>/css/changeset.css");
+   <?cs elif:trac.active_module == 'newticket' || trac.active_module == 'ticket' ?>
+   @import url("<?cs var:htdocs_location ?>/css/ticket.css");
+   <?cs elif:trac.active_module == 'report' ?>
+   @import url("<?cs var:htdocs_location ?>/css/report.css");
+   <?cs elif:trac.active_module == 'search' ?>
+   @import url("<?cs var:htdocs_location ?>/css/search.css");
+   <?cs /if ?>
+  </style>
+  <script src="<?cs var:$htdocs_location ?>/trac.js" type="text/javascript"></script>
+ </head>
 <body>
 
 <div id="header">
-  <a id="hdrlogo" href="<?cs var:header_logo.link ?>"><img src="<?cs var:header_logo.src ?>"
-      width="<?cs var:header_logo.width ?>"
-      height="<?cs var:header_logo.height ?>"
+  <a id="logo" href="<?cs var:header_logo.link ?>"><img src="<?cs var:header_logo.src ?>"
+      width="<?cs var:header_logo.width ?>" height="<?cs var:header_logo.height ?>"
       alt="<?cs var:header_logo.alt ?>" /></a>
-  <hr class="hide"/>
-  <h2 class="hide">Navigation</h2>
-  <div id="header-right">
-   <form id="search" action="<?cs var:trac.href.search ?>" method="get">
-    <div>
-     <label for="proj-search">Search:</label>
-     <input type="text" id="proj-search" name="q" size="10" value="" />
-     <input type="submit" value="Search" />
-     <input type="hidden" name="wiki" value="on" />
-     <input type="hidden" name="changeset" value="on" />
-     <input type="hidden" name="ticket" value="on" />
-    </div>
-   </form>
-   <ul class="subheader-links">
-    <li><?cs if $trac.authname == "anonymous" ?>
-      <a href="<?cs var:trac.href.login ?>">Login</a>
-    <?cs else ?>
-      logged in as <?cs var:trac.authname ?> </li>
-      <li><a href="<?cs var:trac.href.logout ?>">Logout</a>
-    <?cs /if ?></li>
-    <li><a accesskey="6" href="<?cs var:trac.href.wiki ?>/TracGuide">Help/Guide</a></li>
-    <li style="display: none"><a accesskey="5" href="http://projects.edgewall.com/trac/wiki/TracFaq">FAQ</a></li>
-    <li style="display: none"><a accesskey="0" href="<?cs var:trac.href.wiki ?>/TracAccessibility">Accessibility</a></li>
-    <li class="last"><a accesskey="9" href="<?cs var:trac.href.about ?>">About Trac</a></li>
-   </ul>
-  </div>
+  <hr />
+</div>
+
+<form id="search" action="<?cs var:trac.href.search ?>" method="get">
+ <div>
+  <label for="query">Search:</label>
+  <input type="text" id="proj-search" name="query" size="10" value="" />
+  <input type="submit" value="Search" />
+  <input type="hidden" name="wiki" value="on" />
+  <input type="hidden" name="changeset" value="on" />
+  <input type="hidden" name="ticket" value="on" />
  </div>
+</form>
+
+<div class="nav">
+ <h2>Navigation</h2>
+ <ul class="subheader-links">
+  <li><?cs if:trac.authname == "anonymous" ?>
+    <a href="<?cs var:trac.href.login ?>">Login</a>
+  <?cs else ?>
+    logged in as <?cs var:trac.authname ?> </li>
+    <li><a href="<?cs var:trac.href.logout ?>">Logout</a>
+  <?cs /if ?></li>
+  <li><a accesskey="6" href="<?cs var:trac.href.wiki ?>/TracGuide">Help/Guide</a></li>
+  <li style="display: none"><a accesskey="5" href="http://projects.edgewall.com/trac/wiki/TracFaq">FAQ</a></li>
+  <li style="display: none"><a accesskey="0" href="<?cs var:trac.href.wiki ?>/TracAccessibility">Accessibility</a></li>
+  <li class="last"><a accesskey="9" href="<?cs var:trac.href.about ?>">About Trac</a></li>
+ </ul>
+</div>
+
 <?cs def:navlink(text, href, id, aclname, accesskey) ?><?cs
    if $error.type || $trac.acl.+aclname ?><li><a href="<?cs var:href ?>" <?cs 
-        if $id == $trac.active_module ?>class="active"<?cs /if ?> 
-        <?cs if:$accesskey!="" ?> accesskey="<?cs var:$accesskey ?>"<?cs 
+        if $id == $trac.active_module ?>class="active"<?cs /if ?><?cs
+        if:$accesskey!="" ?> accesskey="<?cs var:$accesskey ?>"<?cs 
         /if ?>><?cs var:text ?></a></li><?cs 
-   /if ?><?cs  /def ?>
+   /if ?><?cs /def ?>
 
   <?cs if $trac.active_module == "wiki" ?>
     <?cs set:$wiki_view="wiki" ?>
@@ -92,26 +88,21 @@
     <?cs set:$browser_view="browser" ?>
   <?cs /if  ?>
 
- <ul id="navbar">
-  <?cs call:navlink("Search", $trac.href.search, "search",
-                    $trac.acl.SEARCH_VIEW, "4") ?>
-
-  <?cs call:navlink("New Ticket", $trac.href.newticket, "newticket",
-                    $trac.acl.TICKET_CREATE, "9") ?>
-
-  <?cs call:navlink("View Tickets", $trac.href.report, $ticket_view,
-                    $trac.acl.REPORT_VIEW, "") ?>
-
-  <li style="display: none"><a href="<?cs var:$trac.href.newticket ?>"
-                    accesskey="7">New Ticket (Accessibility)</a></li>
-
-  <?cs call:navlink("Browse Source", $trac.href.browser, $browser_view,
-                    $trac.acl.BROWSER_VIEW, "") ?>
-
-  <?cs call:navlink("Timeline", $trac.href.timeline, "timeline",
-                    $trac.acl.TIMELINE_VIEW, "2") ?>
-
+<div id="navbar" class="nav">
+ <ul>
   <?cs call:navlink("Wiki", $trac.href.wiki, $wiki_view,
                     $trac.acl.WIKI_VIEW, "1") ?>
+  <?cs call:navlink("Timeline", $trac.href.timeline, "timeline",
+                    $trac.acl.TIMELINE_VIEW, "2") ?>
+  <?cs call:navlink("Browse Source", $trac.href.browser, $browser_view,
+                    $trac.acl.BROWSER_VIEW, "") ?>
+  <li style="display: none"><a href="<?cs var:$trac.href.newticket ?>"
+                    accesskey="7">New Ticket (Accessibility)</a></li>
+  <?cs call:navlink("View Tickets", $trac.href.report, $ticket_view,
+                    $trac.acl.REPORT_VIEW, "") ?>
+  <?cs call:navlink("New Ticket", $trac.href.newticket, "newticket",
+                    $trac.acl.TICKET_CREATE, "9") ?>
+  <?cs call:navlink("Search", $trac.href.search, "search",
+                    $trac.acl.SEARCH_VIEW, "4") ?>
  </ul>
-
+</div>
