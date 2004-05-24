3f38eb9d2ae5e96ff835210d84821ccca673ede5
daniel <daniel@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2004-05-24 14:04:51 +0000

    Database changes for [617].
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@618 af82e41b-90c4-0310-8c96-b1721e28e2e2

---
+++ trac/upgrades/db4.py
@@ -0,0 +1,15 @@
+sql = """
+CREATE TABLE session (
+         sid             text,
+         username        text,
+         var_name        text,
+         var_value       text,
+         UNIQUE(sid,var_name)
+);
+
+CREATE INDEX session_idx ON session(sid,var_name);
+"""
+
+def do_upgrade(env, ver, cursor):
+    cursor.execute(sql)
+#    env.save_config()
