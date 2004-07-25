f817a26bbd579ec3b8fbc9df66f60a12fcd03910
jonas <jonas@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2004-07-25 13:49:25 +0000

    Removed executable bit from some css files and added simple man pages.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@835 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- setup.py
+++ setup.py
@@ -202,6 +202,7 @@ facilities.
       data_files=[(_p('share/trac/templates'), glob('templates/*')),
                   (_p('share/trac/htdocs'), glob(_p('htdocs/*.*')) + [_p('htdocs/README')]),
                   (_p('share/trac/htdocs/css'), glob(_p('htdocs/css/*'))),
+                  (_p('share/man/man1'), glob(_p('scripts/*.1'))),
                   (_p('share/trac/wiki-default'), glob(_p('wiki-default/[A-Z]*')))],
       scripts=[_p('scripts/trac-admin'),
                _p('scripts/tracd'),
