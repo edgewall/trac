c38b8cd9d1958c8a92b11fc63ec7111c7a72d407
daniel <daniel@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2004-11-12 15:40:33 +0000

    Added a wikimacro to present table of contents for a set of wiki pages (TracGuide in particular).
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@1049 af82e41b-90c4-0310-8c96-b1721e28e2e2

---
+++ trac/wikimacros/TracGuideToc.py
@@ -0,0 +1,37 @@
+# -*- coding: iso8859-1 -*-
+"""
+This macro shows a quick and dirty way to make a table-of-contents for a set
+of wiki pages.
+"""
+
+TOC = [('TracGuide',        'Index'),
+       ('TracInstall',      'Installation'),
+       ('TracUpgrade',      'Upgrading'),
+       ('TracIni',          'Configuration'),
+       ('TracAdmin',        'Administration'),
+       ('TracLogging',      'Logging'),
+       ('TracPermissions' , 'Permissions'),
+       ('TracWiki',         'The Wiki'),
+       ('TracBrowser',      'The Browser'),
+       ('TracRoadmap',      'The Roadmap'),
+       ('TracChangeset',    'Changesets'),
+       ('TracTickets',      'Tickets'),
+       ('TracReports',      'Reports'),
+       ('TracQuery',        'Custom Queries'),
+       ('TracTimeline',     'Timeline'),
+       ('TracRss',          'RSS Support'),
+       ('TracNotification', 'Notification'),
+       ]
+
+def execute(hdf, args, env):
+    html = '<div class="wiki-toc">' \
+           '<h4>Table of Contents</h4>' \
+           '<ul>'
+    curpage =  '%s' % hdf.getValue('args.page', '')
+    for ref,title in TOC:
+        if curpage == ref:
+            cls =  'class="active"'
+        else:
+            cls = ''
+        html += '<li %s><a href="%s">%s</a></li>' % (cls, ref, title)
+    return html + '</ul></div>'
