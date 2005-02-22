9eb9130cb56056c9d59ac40ea8939a153079279f
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-02-22 16:54:20 +0000

    Ported [1256] and [1257] to stable.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.8-stable@1258 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/core.py
+++ trac/core.py
@@ -239,7 +239,7 @@ def populate_hdf(hdf, env, db, req):
     hdf.setValue('header_logo.link', env.get_config('header_logo', 'link'))
     hdf.setValue('header_logo.alt', env.get_config('header_logo', 'alt'))
     src = env.get_config('header_logo', 'src')
-    src_abs = src[:7] == 'http://' and 1 or 0
+    src_abs = re.match(r'https?://', src) != None
     if not src[0] == '/' and not src_abs:
         src = htdocs_location + src
     hdf.setValue('header_logo.src', src)
