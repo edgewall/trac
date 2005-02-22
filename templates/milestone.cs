466e0d3734f29d38710e68c731f38745a4128f9c
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-02-22 13:29:13 +0000

    Improve the display of the milestone page:  * Repeat main progress bar from roadmap page on the milestone page.  * Make the maximum width of the soft table a soft-limit for better display on large screens.  * Ticket numbers (closed/total) instead of percentages in stats table.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@1251 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- templates/milestone.cs
+++ templates/milestone.cs
@@ -137,26 +137,56 @@
           var:group.total_tickets ?> ticket<?cs
           if:group.total_tickets != 1 ?>s<?cs /if ?> active"></a>
         </div>
-        <p class="percent"><?cs var:#group.percent_total ?>%</p>
+        <p class="percent"><?cs var:group.closed_tickets ?>/<?cs
+         var:group.total_tickets ?></p>
        <?cs /if ?></td>
       </tr><?cs
      /each ?>
     </table><?cs /if ?>
    </fieldset>
   </form>
-  <p class="date"><?cs
-   if:milestone.completed_date ?>
-    Completed <?cs var:milestone.completed_delta ?> ago (<?cs var:milestone.completed_date ?>)<?cs
-   elif:milestone.due_date ?><?cs
-    if:milestone.late ?>
-     <strong><?cs var:milestone.due_delta ?> late</strong><?cs
+  <div class="info">
+   <p class="date"><?cs
+    if:milestone.completed_date ?>
+     Completed <?cs var:milestone.completed_delta ?> ago (<?cs var:milestone.completed_date ?>)<?cs
+    elif:milestone.due_date ?><?cs
+     if:milestone.late ?>
+      <strong><?cs var:milestone.due_delta ?> late</strong><?cs
+     else ?>
+      Due in <?cs var:milestone.due_delta ?><?cs
+     /if ?> (<?cs var:milestone.due_date ?>)<?cs
     else ?>
-     Due in <?cs var:milestone.due_delta ?><?cs
-    /if ?> (<?cs var:milestone.due_date ?>)<?cs
-   else ?>
-    No date set<?cs
-   /if ?>
-  </p>
+     No date set<?cs
+    /if ?>
+   </p><?cs
+   with:stats = milestone.stats ?><?cs
+    if:#stats.total_tickets > #0 ?>
+     <div class="progress">
+      <a class="closed" href="<?cs
+        var:milestone.queries.closed_tickets ?>" style="width: <?cs
+        var:#stats.percent_closed ?>%" title="<?cs
+        var:#stats.closed_tickets ?> of <?cs
+        var:#stats.total_tickets ?> ticket<?cs
+        if:#stats.total_tickets != #1 ?>s<?cs /if ?> closed"></a>
+      <a class="open" href="<?cs
+        var:milestone.queries.active_tickets ?>" style="width: <?cs
+        var:#stats.percent_active ?>%" title="<?cs
+        var:#stats.active_tickets ?> of <?cs
+        var:#stats.total_tickets ?> ticket<?cs
+        if:#stats.total_tickets != #1 ?>s<?cs /if ?> active"></a>
+     </div>
+     <p class="percent"><?cs var:#stats.percent_closed ?>%</p>
+     <dl>
+      <dt>Active tickets:</dt>
+      <dd><a href="<?cs var:milestone.queries.active_tickets ?>"><?cs
+        var:stats.active_tickets ?></a></dd>
+      <dt>Closed tickets:</dt>
+      <dd><a href="<?cs var:milestone.queries.closed_tickets ?>"><?cs
+        var:stats.closed_tickets ?></a></dd>
+     </dl><?cs
+    /if ?><?cs
+   /with ?>
+  </div>
   <div class="description"><?cs var:milestone.description ?></div><?cs
   if:trac.acl.MILESTONE_MODIFY || trac.acl.MILESTONE_DELETE ?>
    <div class="buttons"><?cs
