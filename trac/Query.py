e068c52b9de61272140fbaee94f2f03d828a9f05
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-02-23 15:22:06 +0000

    Fix potential error in column computation in the query module.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@1273 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/Query.py
+++ trac/Query.py
@@ -1,7 +1,7 @@
 # -*- coding: iso8859-1 -*-
 #
-# Copyright (C) 2003, 2004 Edgewall Software
-# Copyright (C) 2003, 2004 Jonas Borgström <jonas@edgewall.com>
+# Copyright (C) 2003, 2004, 2005 Edgewall Software
+# Copyright (C) 2003, 2004, 2005 Jonas Borgström <jonas@edgewall.com>
 #
 # Trac is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -64,7 +64,8 @@ class Query:
             constraint = self.constraints[col]
             if len(constraint) == 1 and constraint[0] \
                     and not constraint[0][0] in ('!', '~', '^', '$'):
-                cols.remove(col)
+                if col in cols:
+                    cols.remove(col)
             if col == 'status' and not 'closed' in constraint \
                     and 'resolution' in cols:
                 cols.remove('resolution')
