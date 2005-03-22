1ce48d13a11abe996b065e86aed41231b25f2514
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-03-22 22:28:44 +0000

    Show milestones in new ticket form if their completed date is either 0 ''or'' NULL.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@1404 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/Ticket.py
+++ trac/Ticket.py
@@ -340,8 +340,8 @@ class NewticketModule(Module):
 
         util.sql_to_hdf(self.db, "SELECT name FROM component ORDER BY name",
                         req.hdf, 'newticket.components')
-        util.sql_to_hdf(self.db, "SELECT name FROM milestone WHERE completed=0 "
-                                 "ORDER BY name",
+        util.sql_to_hdf(self.db, "SELECT name FROM milestone WHERE "
+                                 "COALESCE(completed,0)=0 ORDER BY name",
                         req.hdf, 'newticket.milestones')
         util.sql_to_hdf(self.db, "SELECT name FROM version ORDER BY name",
                         req.hdf, 'newticket.versions')
