ccf0471a90e84b30dd0b5e65496202bc1bfb5889
otavio <otavio@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-03-29 23:47:01 +0000

    Tag 0.8.1-2 release.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/tags/debian-dev@1465 af82e41b-90c4-0310-8c96-b1721e28e2e2

---
+++ 0.8.1-2/contrib/emailfilter.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+"""
+emailfilter.py -- Email tickets to Trac.
+
+A simple MTA filter to create Trac tickets from inbound emails.
+
+Copyright 2005, Daniel Lundin <daniel@edgewall.com>
+Copyright 2005, Edgewall Software
+
+The scripts reads emails from stdin and inserts directly into a Trac database.
+MIME headers are mapped as follows:
+
+    * From: => Reporter
+    * Subject: => Summary
+    * Body => Description
+
+How to use
+----------
+ * Set TRAC_ENV_PATH to the path of your project's Trac environment
+ * Configure script as a mail (pipe) filter with your MTA
+    typically, this involves adding a line like this to /etc/aliases:
+       somename: |/path/to/email2trac.py
+    Check your MTA's documentation for specifics.
+
+Todo
+----
+  * Configure target database through env variable?
+  * Handle/discard HTML parts
+  * Attachment support
+"""
+
+TRAC_ENV_PATH = '/var/trac/test'
+
+from trac import Environment, Ticket
+
+import email
+import sys
+
+class TicketEmailParser(object):
+
+    env = None
+
+    def __init__(self, env):
+        self.env = env
+
+    def parse(self, fp):
+        msg = email.message_from_file(fp)
+        db = self.env.get_db_cnx()
+        tkt = Ticket.Ticket()
+        tkt['status'] = 'new'
+        tkt['component'] = self.env.get_config('ticket', 'default_component')
+        cursor = db.cursor()
+        cursor.execute('SELECT owner FROM component '
+                       'WHERE name=%s', tkt['component'])
+        tkt['owner'] = cursor.fetchone()[0]
+        tkt['milestone'] = self.env.get_config('ticket', 'default_milestone')
+        tkt['priority'] = self.env.get_config('ticket', 'default_priority')
+        tkt['severity'] = self.env.get_config('ticket', 'default_severity')
+        tkt['version'] = self.env.get_config('ticket', 'default_version')
+        tkt['reporter'] = msg['from']
+        tkt['summary'] = msg['subject']
+        for part in msg.walk():
+            if part.get_content_type() == 'text/plain':
+                tkt['description'] = part.get_payload(decode=1).strip()
+
+        if tkt.get('description'):
+            tkt.insert(db)
+
+if __name__ == '__main__':
+    env = Environment.Environment(TRAC_ENV_PATH, create=0)
+    tktparser = TicketEmailParser(env)
+    tktparser.parse(sys.stdin)
+
---
+++ 0.8.1-2/trac/authzperm.py
@@ -0,0 +1,86 @@
+# -*- coding: iso8859-1 -*-
+#
+# Copyright (C) 2004 Edgewall Software
+# Copyright (C) 2004 Francois Harvey <fharvey@securiweb.net>
+#
+# Trac is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Trac is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# Author: Francois Harvey <fharvey@securiweb.net>
+
+from exceptions import StandardError
+import ConfigParser
+import string
+import os
+
+class AuthzPermissionError (StandardError):
+    """Insufficient permission to view this file"""
+    def __str__ (self):
+        return 'Insufficient permission to view this file (mod_authz_svn)'
+    
+
+class AuthzPermission:
+    auth_name = ''
+    module_name = ''
+    conf_authz = None
+    authz_file = ''
+    
+    def __init__(self,env,authname):
+	self.auth_name = authname
+	
+	if env.get_config('trac','authz_module_name','') == '':
+	    self.module_name = ''
+	else:
+	    self.module_name = env.get_config('trac','authz_module_name') + ':'
+				 
+        self.autz_file = env.get_config('trac','authz_file')    
+        if env.get_config('trac','authz_file'):
+            self.conf_authz = ConfigParser.ConfigParser()
+            self.conf_authz.read( self.autz_file )
+    
+    def group_contains_user(self, group_name, user_name):
+        if self.conf_authz.has_section('groups'):
+            if self.conf_authz.has_option('groups', group_name):
+                users_list = self.conf_authz.get('groups', group_name).split(',')
+                return users_list.has_key(user_name)
+        return False
+    
+    def has_permission(self, path):
+        acc = ''
+
+        if path != None and self.conf_authz != None:
+            if self.conf_authz.has_section(self.module_name + '/') and \
+                   self.conf_authz.has_option(self.module_name  + '/',
+                                              self.auth_name):
+                acc = self.conf_authz.get(self.module_name + '/',self.auth_name)
+	    elif self.conf_authz.has_section(self.module_name + '/') and \
+	         self.conf_authz.has_option(self.module_name  + '/', '*'):
+		     acc = self.conf_authz.get(self.module_name + '/','*')
+            path_comb = ''
+            for path_ele in path.split('/'):
+                if path_ele != '':
+                    path_comb = path_comb + '/' + path_ele
+                    if self.conf_authz.has_section(self.module_name + path_comb) and \
+                           self.conf_authz.has_option(self.module_name + path_comb,self.auth_name):
+                        acc =  self.conf_authz.get(self.module_name + path_comb,self.auth_name)
+		    elif self.conf_authz.has_section(self.module_name + path_comb) and \
+		            self.conf_authz.has_option(self.module_name + path_comb,'*'):
+		        acc =  self.conf_authz.get(self.module_name + path_comb,'*')
+        else:
+            acc = 'r'
+        return acc
+
+    def assert_permission (self, path):
+        if self.has_permission(path) == '':
+            raise AuthzPermissionError()
