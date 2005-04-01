802da7c4485f0a48702d6654e3416573a3a0a9ad
mrowe <mrowe@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-04-01 08:54:01 +0000

    Change the unit test to use a MIME-type that doesn't require an optional module to work.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@1472 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/mimeviewers/test.py
+++ trac/mimeviewers/test.py
@@ -1,4 +1,4 @@
 supported_types = [(2, 'application/x-test')]
 
 def display(data, mimetype, filename, rev, env):
-    return '<pre>' + '\nTESTING: '.join(data.splitlines()) + '</pre>'
+    return '<pre>' + '\nTESTING: '.join(data.splitlines()) + '</pre>\n'
