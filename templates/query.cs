cb69bb3d3946b62ac74197d113cad607248001bd
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-06-24 17:03:06 +0000

    Follow-up to [1831], one rename missed
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@1853 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- templates/query.cs
+++ templates/query.cs
@@ -150,7 +150,7 @@
 <script type="text/javascript" src="<?cs
   var:htdocs_location ?>js/query.js"></script>
 <script type="text/javascript"><?cs set:idx = 0 ?>
- var properties={<?cs each:field = ticket.fields ?><?cs
+ var properties={<?cs each:field = query.fields ?><?cs
   var:name(field) ?>:{type:"<?cs var:field.type ?>",label:"<?cs
   var:field.label ?>",options:[<?cs
    each:option = field.options ?>"<?cs var:option ?>"<?cs
