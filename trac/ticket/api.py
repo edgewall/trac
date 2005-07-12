5998ee07ee3bb9789aef3540028de1786a4c1eeb
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-07-12 22:33:39 +0000

    Remove dependency on specific status values from ticket TracLinks. See #1545.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@1948 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/ticket/api.py
+++ trac/ticket/api.py
@@ -151,20 +151,19 @@ class TicketSystem(Component):
                 ('ticket', self._format_link)]
 
     def get_wiki_syntax(self):
-        yield (r"!?#\d+", lambda x, y, z: self._format_link(x, 'ticket', y[1:], y))
+        yield (r"!?#\d+",
+               lambda x, y, z: self._format_link(x, 'ticket', y[1:], y))
 
     def _format_link(self, formatter, ns, target, label):
         cursor = formatter.db.cursor()
-        cursor.execute("SELECT summary,status FROM ticket WHERE id=%s", (target,))
+        cursor.execute("SELECT summary,status FROM ticket WHERE id=%s",
+                       (target,))
         row = cursor.fetchone()
         if row:
             summary = util.escape(util.shorten_line(row[0]))
-            if row[1] in ('new', 'closed'):
-                return '<a class="%s ticket" href="%s" title="%s (%s)">%s</a>' \
-                       % (row[1], formatter.href.ticket(target), summary, row[1], label)
-            else:
-                return '<a class="ticket" href="%s" title="%s">%s</a>' \
-                       % (formatter.href.ticket(target), summary, label)
+            return '<a class="%s ticket" href="%s" title="%s (%s)">%s</a>' \
+                   % (row[1], formatter.href.ticket(target), summary, row[1],
+                      label)
         else:
             return '<a class="missing ticket" href="%s" rel="nofollow">%s</a>' \
                    % (formatter.href.ticket(target), label)
