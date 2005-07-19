d064d41d069176e495bc6f1f137643cd83978bf1
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-07-19 16:27:14 +0000

    Enable code fragments in changeset log messages to be properly displayed. Fixes #1632.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@2001 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/Changeset.py
+++ trac/Changeset.py
@@ -91,6 +91,7 @@ class ChangesetModule(Component):
                  'application/zip', 'zip')
         add_stylesheet(req, 'css/changeset.css')
         add_stylesheet(req, 'css/diff.css')
+        add_stylesheet(req, 'css/code.css')
         return 'changeset.cs', None
 
     # ITimelineEventProvider methods
