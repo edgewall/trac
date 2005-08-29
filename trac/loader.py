001207b2e5d6e1b9c3309c016bc6dcd5eed6b73c
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-08-29 00:08:05 +0000

    Remove support for [wiki:TracIni trac.ini] configured plugins. Plugins must now be deployed as Python Eggs. (See also TracDev/PluginDevelopment).
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@2143 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/loader.py
+++ trac/loader.py
@@ -15,8 +15,6 @@
 # Author: Christopher Lenz <cmlenz@gmx.de>
 
 import os
-import imp
-import sys
 
 try:
     import pkg_resources
@@ -29,22 +27,6 @@ __all__ = ['load_components']
 
 def load_components(env):
     loaded_components = []
-
-    # Load configured modules
-    for section in env.config.sections():
-        for name, value in env.config.options(section):
-            if name == 'module':
-                loaded_components.append(value)
-                path = env.config.get(section, 'path') or None
-                env.log.debug('Loading component module %s from %s', value,
-                              path or 'default path')
-                if path:
-                    path = [path]
-                try:
-                    load_component(value, path)
-                except ImportError, e:
-                    env.log.error('Component module %s not found', value,
-                                  exc_info=True)
 
     # Load components from the environment plugins directory
     plugins_dir = os.path.join(env.path, 'plugins')
@@ -72,49 +54,4 @@ def load_components(env):
     from trac.db_default import default_components
     for module in default_components:
         if not module in loaded_components:
-            load_component(module)
-
-def load_component(name, path=None):
-    if path and os.path.isfile(path[0]):
-        try:
-            from zipimport import zipimporter
-            zip = zipimporter(path[0])
-            return zip.load_module(name)
-        except ImportError:
-            pass
-
-    if '.' in name:
-        i = name.find('.')
-        head, tail = name[:i], name[i + 1:]
-    else:
-        head, tail = name, ''
-
-    module = _load_module(head, head, None, path)
-    if not module:
-        raise ImportError, 'No module named ' + head
-
-    while tail:
-        i = tail.find('.')
-        if i < 0:
-            i = len(tail)
-        head, tail = tail[:i], tail[i + 1:]
-        module_name = '%s.%s' % (module.__name__, head)
-        module = _load_module(head, module_name, module, path)
-        if not module:
-            raise ImportError, 'No module named ' + module_name
-
-def _load_module(part, name, parent, path=None):
-    try:
-        return sys.modules[name]
-    except KeyError:
-        try:
-            fd, path, desc = imp.find_module(part,
-                                             parent and parent.__path__ or path)
-        except ImportError:
-            return None
-        try:
-            module = imp.load_module(name, fd, path, desc)
-        finally:
-            if fd:
-                fd.close()
-        return module
+            __import__(module)
