6016e59b4135ed3f7c6e83947b4ce4780828318b
mgood <mgood@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-09-19 02:46:48 +0000

    a few occurrences of `_pages` in the Wiki api were not converted to `_index` with [1977] (fixes #1863)
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@2255 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/wiki/api.py
+++ trac/wiki/api.py
@@ -121,7 +121,7 @@ class WikiSystem(Component):
     def wiki_page_added(self, page):
         if not self.has_page(page.name):
             self.log.debug('Adding page %s to index' % page.name)
-            self._pages[page.name] = True
+            self._index[page.name] = True
 
     def wiki_page_changed(self, page, version, t, comment, author, ipnr):
         pass
@@ -129,7 +129,7 @@ class WikiSystem(Component):
     def wiki_page_deleted(self, page):
         if self.has_page(page.name):
             self.log.debug('Removing page %s from index' % page.name)
-            del self._pages[page.name]
+            del self._index[page.name]
 
     # IWikiSyntaxProvider methods
     
