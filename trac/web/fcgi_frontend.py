efc990580d0c2f1dd77c704b65265788004d2dab
mgood <mgood@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2005-10-31 15:40:43 +0000

    oops I didn't mean to check in the FastCGI changes with [2432]
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@2433 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/web/fcgi_frontend.py
+++ trac/web/fcgi_frontend.py
@@ -19,35 +19,25 @@ from trac.web.cgi_frontend import CGIRequest
 from trac.web.main import dispatch_request, get_environment, \
                           send_pretty_error, send_project_index
 
-import _thfcgi
-#import _fcgi
+import _fcgi
 import os
 import locale
 
 def run():
     locale.setlocale(locale.LC_ALL, '')
-    _thfcgi.THFCGI(_handler).run()
-    #_fcgi.Server(_handler).run()
-
-class FCGIRequest(CGIRequest):
-    def __init__(self, environ, input, output, fieldStorage):
-        self._fieldStorage = fieldStorage
-        CGIRequest.__init__(self, environ, input, output)
-
-    def _getFieldStorage(self):
-        return self._fieldStorage
+    _fcgi.Server(_handler).run()
 
 
-def _handler(_req, _env, _fieldStorage):
-    req = FCGIRequest(_env, _req.stdin, _req.out, _fieldStorage)
+def _handler(_req):
+    req = CGIRequest(_req.params, _req.stdin, _req.stdout)
     env = get_environment(req, os.environ)
 
     if not env:
         send_project_index(req, os.environ)
-        return
+        return _fcgi.CGI_REQUEST_COMPLETE, 0
 
     try:  
         dispatch_request(req.path_info, req, env)
     except Exception, e:
         send_pretty_error(e, env, req)
-    return
+    return _fcgi.FCGI_REQUEST_COMPLETE, 0
