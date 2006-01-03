395687bf9eafe8dc07e925b6a2878afb06018741
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2006-01-03 09:23:45 +0000

    Update copyright in `diff.py`.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@2719 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/versioncontrol/diff.py
+++ trac/versioncontrol/diff.py
@@ -1,7 +1,7 @@
 # -*- coding: iso-8859-1 -*-
 #
-# Copyright (C) 2004-2005 Edgewall Software
-# Copyright (C) 2004-2005 Christopher Lenz <cmlenz@gmx.de>
+# Copyright (C) 2004-2006 Edgewall Software
+# Copyright (C) 2004-2006 Christopher Lenz <cmlenz@gmx.de>
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
