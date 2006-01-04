e76363e263a5477d768fc1b5a1d81d7518416167
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2006-01-04 17:20:24 +0000

    0.9-stable: fix version number.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.9-stable@2726 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/__init__.py
+++ trac/__init__.py
@@ -10,7 +10,7 @@ U{http://trac.edgewall.com/}
 """
 __docformat__ = 'epytext en'
 
-__version__ = '0.9.2'
+__version__ = '0.9.3dev'
 __url__ = 'http://trac.edgewall.com/'
 __copyright__ = '(C) 2003-2006 Edgewall Software'
 __license__ = 'BSD'
