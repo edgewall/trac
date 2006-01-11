edgewall/trac

commit 108ecb3b6e10bfc1b62f937f2f58e4fd9f38e3ed

In the changeset information, the EDIT changes now contain the exact previous location and previous revision of the base.
Though this what not a real problem, as Subversion was nevertheless able to find the 
correct old content, this provides more useful information in the changeset view.

A `resync` operation is needed to take benefit from this change for old changesets.

Thanks to Antoine Delvaux for noticing and reporting the issue.



git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@2562 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/versioncontrol/svn_fs.py
@@ -457,6 +457,8 @@ class SubversionChangeset(Changeset):
     def get_changes(self):
         pool = Pool(self.pool)
         root = fs.revision_root(self.fs_ptr, self.rev, pool())
+        if self.rev > 0:
+            prev_root = fs.revision_root(self.fs_ptr, self.rev - 1, pool())
         editor = repos.RevisionChangeCollector(self.fs_ptr, self.rev, pool())
         e_ptr, e_baton = delta.make_editor(editor, pool())
         repos.svn_repos_replay(root, e_ptr, e_baton, pool())
@@ -470,9 +472,8 @@ class SubversionChangeset(Changeset):
                 continue
             if not (path+'/').startswith(self.scope[1:]):
                 continue
-            base_path = _scoped_path(self.scope, change.base_path)
             action = ''
-            if not change.path and base_path:
+            if not change.path and change.base_path:
                 action = Changeset.DELETE
                 deletions[change.base_path] = idx
             elif change.added:
@@ -483,8 +484,11 @@ class SubversionChangeset(Changeset):
                     action = Changeset.ADD
             else:
                 action = Changeset.EDIT
+                change.base_path = fs.node_created_path(prev_root, path, pool())
+                change.base_rev = fs.node_created_rev(prev_root, path, pool())
             kind = _kindmap[change.item_kind]
             path = path[len(self.scope) - 1:]
+            base_path = _scoped_path(self.scope, change.base_path)
             changes.append([path, kind, action, base_path, change.base_rev])
             idx += 1
 

trac/versioncontrol/tests/svn_fs.py
@@ -287,9 +287,18 @@ class SubversionRepositoryTestCase(unittest.TestCase):
         self.assertEqual('Setting property on the repository_dir root',
                          chgset.message)
         changes = chgset.get_changes()
-        self.assertEqual(('', Node.DIRECTORY, Changeset.EDIT, None, 12),
+        self.assertEqual(('', Node.DIRECTORY, Changeset.EDIT, '/', 12),
                          changes.next())
-        self.assertEqual(('trunk', Node.DIRECTORY, Changeset.EDIT, 'trunk', 12),
+        self.assertEqual(('trunk', Node.DIRECTORY, Changeset.EDIT, 'trunk', 6),
+                         changes.next())
+        self.assertRaises(StopIteration, changes.next)
+
+    def test_changeset_base_path_rev(self):
+        chgset = self.repos.get_changeset(9)
+        self.assertEqual(9, chgset.rev)
+        changes = chgset.get_changes()
+        self.assertEqual(('branches/v1x/README.txt', Node.FILE,
+                          Changeset.EDIT, 'trunk/README.txt', 3),
                          changes.next())
         self.assertRaises(StopIteration, changes.next)
         
@@ -503,7 +512,7 @@ class ScopedSubversionRepositoryTestCase(unittest.TestCase):
         self.assertEqual('Setting property on the repository_dir root',
                          chgset.message)
         changes = chgset.get_changes()
-        self.assertEqual(('', Node.DIRECTORY, Changeset.EDIT, '/', 12),
+        self.assertEqual(('', Node.DIRECTORY, Changeset.EDIT, '/', 6),
                          changes.next())
         self.assertRaises(StopIteration, changes.next)
 

commit 1b9287fa373a5c60d0b86264bedd012570dbe518

Finally got around the memory leak introduced in r2562. Closes #2485.
git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@2756 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/versioncontrol/svn_fs.py
@@ -494,6 +494,7 @@ class SubversionChangeset(Changeset):
 
     def get_changes(self):
         pool = Pool(self.pool)
+        tmp = Pool(pool)
         root = fs.revision_root(self.fs_ptr, self.rev, pool())
         editor = repos.RevisionChangeCollector(self.fs_ptr, self.rev, pool())
         e_ptr, e_baton = delta.make_editor(editor, pool())
@@ -504,6 +505,7 @@ class SubversionChangeset(Changeset):
         changes = []
         revroots = {}
         for path, change in editor.changes.items():
+            tmp.clear()
             if not self.authz.has_permission(path):
                 # FIXME: what about base_path?
                 continue
@@ -527,8 +529,8 @@ class SubversionChangeset(Changeset):
                 else:
                     b_root = fs.revision_root(self.fs_ptr, b_rev, pool())
                     revroots[b_rev] = b_root
-                change.base_path = fs.node_created_path(b_root, b_path, pool())
-                change.base_rev = fs.node_created_rev(b_root, b_path, pool())
+                change.base_path = fs.node_created_path(b_root, b_path, tmp())
+                change.base_rev = fs.node_created_rev(b_root, b_path, tmp())
             kind = _kindmap[change.item_kind]
             path = path[len(self.scope) - 1:]
             base_path = _path_within_scope(self.scope, change.base_path)