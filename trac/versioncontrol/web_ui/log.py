e3bf30196717f79b3b91f1aee1ee08841f14b845
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2006-03-24 13:52:08 +0000

    Fix typo introduced in [3044], and fix wiki formatter test that expected unescaped links for revision log links.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@3045 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/versioncontrol/web_ui/log.py
+++ trac/versioncontrol/web_ui/log.py
@@ -22,7 +22,7 @@ import urllib
 from trac.core import *
 from trac.perm import IPermissionRequestor
 from trac.util import http_date
-from trac.util.html import html
+from trac.util.markup import html
 from trac.versioncontrol import Changeset
 from trac.versioncontrol.web_ui.changeset import ChangesetModule
 from trac.versioncontrol.web_ui.util import *
