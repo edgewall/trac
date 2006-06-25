edgewall/trac

commit 6b26a190e1414676fd97054baf6b28ecdb345281

More robust traversal of the history of a Wiki page using ''Previous Change'' and ''Next Change'' links: gaps in the sequence of versions are now correctly handled.
git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@3471 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/wiki/web_ui.py
@@ -280,7 +280,9 @@ class WikiModule(Component):
         }
         num_changes = 0
         old_page = None
-        for version,t,author,comment,ipnr in page.get_history():
+        latest_page = WikiPage(self.env, page.name)
+        prev_version = next_version = None
+        for version,t,author,comment,ipnr in latest_page.get_history():
             if version == new_version:
                 if t:
                     info['time'] = format_datetime(t)
@@ -291,24 +293,27 @@ class WikiModule(Component):
             else:
                 num_changes += 1
                 if version < new_version:
+                    if not prev_version:
+                        prev_version = version
                     if (old_version and version == old_version) or \
                             not old_version:
                         old_page = WikiPage(self.env, page.name, version)
                         info['num_changes'] = num_changes
                         info['old_version'] = version
                         break
+                else:
+                    next_version = version
         req.hdf['wiki'] = info
 
         # -- prev/next links
-        if new_version > 1:
+        if prev_version:
             add_link(req, 'prev', req.href.wiki(page.name, action='diff',
-                                                version=new_version-1),
-                     'Version %d' % (new_version-1))
-        latest_page = WikiPage(self.env, page.name)
-        if new_version < latest_page.version:
+                                                version=prev_version),
+                     'Version %d' % prev_version)
+        if next_version:
             add_link(req, 'next', req.href.wiki(page.name, action='diff',
-                                                version=new_version+1),
-                     'Version %d' % (new_version+1))
+                                                version=next_version),
+                     'Version %d' % next_version)
 
         # -- text diffs
         diff_style, diff_options = get_diff_options(req)

commit 08151f5358d279402c74a740b5d2327d338534df

Fix a little bug concerning num_changes, introduced in r3471, which prevented the version information to be displayed.
git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@3475 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/wiki/web_ui.py
@@ -287,7 +287,7 @@ class WikiModule(Component):
             'latest_version': latest_page.version,
             'history_href': req.href.wiki(page.name, action='history')
         }
-        print `info`
+
         num_changes = 0
         old_page = None
         prev_version = next_version = None
@@ -300,8 +300,8 @@ class WikiModule(Component):
                 info['comment'] = comment or '--'
                 info['ipnr'] = ipnr or ''
             else:
-                num_changes += 1
                 if version < new_version:
+                    num_changes += 1
                     if not prev_version:
                         prev_version = version
                     if (old_version and version == old_version) or \