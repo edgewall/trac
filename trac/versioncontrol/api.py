0091f73d9ddb2e28de9da1a025ba0c0cfaf2b458
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2006-08-15 08:07:51 +0000

    Clean-up of the `trac.versioncontrol.api` documentation. Clarifications and PEP:0008 fixes.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@3614 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/versioncontrol/api.py
+++ trac/versioncontrol/api.py
@@ -22,25 +22,28 @@ from trac.perm import PermissionError
 
 
 class IRepositoryConnector(Interface):
-    """Extension point interface for components that provide support for a
-    specific version control system."""
+    """Provide support for a specific version control system."""
 
     def get_supported_types():
-        """Return the types of version control systems that are supported by
-        this connector, and their relative priorities.
+        """Return the types of version control systems that are supported.
+
+        Yields `(repotype, priority)` pairs, where `repotype` is used to
+        match against the configured `[trac] repository_type` value in TracIni.
         
-        Highest number is highest priority.
+        If multiple provider match a given type, the `priority` is used to
+        choose between them (highest number is highest priority).
         """
 
     def get_repository(repos_type, repos_dir, authname):
-        """Return the Repository object for the given repository type and
-        directory.
+        """Return a Repository instance for the given repository type and dir.
         """
 
 
 class RepositoryManager(Component):
-    """Component that keeps track of the supported version control systems, and
-    provides easy access to the configured implementation."""
+    """Component registering the supported version control systems,
+
+    It provides easy access to the configured implementation.
+    """
 
     connectors = ExtensionPoint(IRepositoryConnector)
 
@@ -63,8 +66,8 @@ class RepositoryManager(Component):
                         continue
                     heappush(candidates, (-prio, connector))
             if not candidates:
-                raise TracError, 'Unsupported version control system "%s"' \
-                                 % self.repository_type
+                raise TracError('Unsupported version control system "%s"'
+                                % self.repository_type)
             self._connector = heappop(candidates)[1]
         return self._connector.get_repository(self.repository_type,
                                               self.repository_dir, authname)
@@ -80,9 +83,7 @@ class NoSuchNode(TracError):
                            % (msg and '%s: ' % msg or '', path, rev))
 
 class Repository(object):
-    """
-    Base class for a repository provided by a version control system.
-    """
+    """Base class for a repository provided by a version control system."""
 
     def __init__(self, name, authz, log):
         self.name = name
@@ -90,21 +91,15 @@ class Repository(object):
         self.log = log
 
     def close(self):
-        """
-        Close the connection to the repository.
-        """
+        """Close the connection to the repository."""
         raise NotImplementedError
 
     def get_changeset(self, rev):
-        """
-        Retrieve a Changeset object that describes the changes made in
-        revision 'rev'.
-        """
+        """Retrieve a Changeset corresponding to the  given revision `rev`."""
         raise NotImplementedError
 
     def get_changesets(self, start, stop):
-        """
-        Generate Changeset belonging to the given time period (start, stop).
+        """Generate Changeset belonging to the given time period (start, stop).
         """
         rev = self.youngest_rev
         while rev:
@@ -117,8 +112,7 @@ class Repository(object):
             rev = self.previous_rev(rev)
 
     def has_node(self, path, rev=None):
-        """
-        Tell if there's a node at the specified (path,rev) combination.
+        """Tell if there's a node at the specified (path,rev) combination.
 
         When `rev` is `None`, the latest revision is implied.
         """
@@ -129,52 +123,48 @@ class Repository(object):
             return False        
     
     def get_node(self, path, rev=None):
-        """
-        Retrieve a Node (directory or file) from the repository at the
-        given path. If the rev parameter is specified, the version of the
-        node at that revision is returned, otherwise the latest version
-        of the node is returned.
+        """Retrieve a Node from the repository at the given path.
+
+        A Node represents a directory or a file at a given revision in the
+        repository.
+        If the `rev` parameter is specified, the Node corresponding to that
+        revision is returned, otherwise the Node corresponding to the youngest
+        revision is returned.
         """
         raise NotImplementedError
 
     def get_oldest_rev(self):
-        """
-        Return the oldest revision stored in the repository.
-        """
+        """Return the oldest revision stored in the repository."""
         raise NotImplementedError
     oldest_rev = property(lambda x: x.get_oldest_rev())
 
     def get_youngest_rev(self):
-        """
-        Return the youngest revision in the repository.
-        """
+        """Return the youngest revision in the repository."""
         raise NotImplementedError
     youngest_rev = property(lambda x: x.get_youngest_rev())
 
     def previous_rev(self, rev):
-        """
-        Return the revision immediately preceding the specified revision.
-        """
+        """Return the revision immediately preceding the specified revision."""
         raise NotImplementedError
 
     def next_rev(self, rev, path=''):
-        """
-        Return the revision immediately following the specified revision.
-        """
+        """Return the revision immediately following the specified revision."""
         raise NotImplementedError
 
     def rev_older_than(self, rev1, rev2):
-        """
-        Return True if rev1 is older than rev2, i.e. if rev1 comes before rev2
-        in the revision sequence.
+        """Provides a total order over revisions.
+        
+        Return `True` if `rev1` is older than `rev2`, i.e. if `rev1`
+        comes before `rev2` in the revision sequence.
         """
         raise NotImplementedError
 
     def get_youngest_rev_in_cache(self, db):
-        """
-        Return the youngest revision currently cached.
+        """Return the youngest revision currently cached.
+        
         The way revisions are sequenced is version control specific.
-        By default, one assumes that the revisions are sequenced in time.
+        By default, one assumes that the revisions are sequenced in time
+        (... which is ''not'' correct for most VCS, including Subversion).
         """
         cursor = db.cursor()
         cursor.execute("SELECT rev FROM revision ORDER BY time DESC LIMIT 1")
@@ -182,34 +172,39 @@ class Repository(object):
         return row and row[0] or None
 
     def get_path_history(self, path, rev=None, limit=None):
-        """
-        Retrieve all the revisions containing this path (no newer than 'rev').
+        """Retrieve all the revisions containing this path
+
+        If given, `rev` is used as a starting point (i.e. no revision
+        ''newer'' than `rev` should be returned).
         The result format should be the same as the one of Node.get_history()
         """
         raise NotImplementedError
 
     def normalize_path(self, path):
-        """
-        Return a canonical representation of path in the repos.
-        """
+        """Return a canonical representation of path in the repos."""
         return NotImplementedError
 
     def normalize_rev(self, rev):
-        """
-        Return a canonical representation of a revision in the repos.
-        'None' is a valid revision value and represents the youngest revision.
+        """Return a canonical representation of a revision.
+
+        It's up to the backend to decide which string values of `rev` 
+        (usually provided by the user) should be accepted, and how they 
+        should be normalized. Some backends may for instance want to match
+        against known tags or branch names.
+        
+        In addition, if `rev` is `None` or '', the youngest revision should
+        be returned.
         """
         return NotImplementedError
 
     def short_rev(self, rev):
-        """
-        Return a compact representation of a revision in the repos.
-        """
+        """Return a compact representation of a revision in the repos."""
         return self.normalize_rev(rev)
         
     def get_changes(self, old_path, old_rev, new_path, new_rev,
                     ignore_ancestry=1):
-        """
+        """Generates changes corresponding to generalized diffs.
+        
         Generator that yields change tuples (old_node, new_node, kind, change)
         for each node change between the two arbitrary (path,rev) pairs.
 
@@ -220,9 +215,7 @@ class Repository(object):
 
 
 class Node(object):
-    """
-    Represents a directory or file in the repository.
-    """
+    """Represents a directory or file in the repository at a given revision."""
 
     DIRECTORY = "dir"
     FILE = "file"
@@ -236,41 +229,44 @@ class Node(object):
     created_path = None
 
     def __init__(self, path, rev, kind):
-        assert kind in (Node.DIRECTORY, Node.FILE), "Unknown node kind %s" % kind
+        assert kind in (Node.DIRECTORY, Node.FILE), \
+               "Unknown node kind %s" % kind
         self.path = unicode(path)
         self.rev = rev
         self.kind = kind
 
     def get_content(self):
-        """
-        Return a stream for reading the content of the node. This method
-        will return None for directories. The returned object should provide
-        a read([len]) function.
+        """Return a stream for reading the content of the node.
+
+        This method will return `None` for directories.
+        The returned object must support a `read([len])` method.
         """
         raise NotImplementedError
 
     def get_entries(self):
-        """
-        Generator that yields the immediate child entries of a directory, in no
-        particular order. If the node is a file, this method returns None.
+        """Generator that yields the immediate child entries of a directory.
+
+        The entries are returned in no particular order.
+        If the node is a file, this method returns `None`.
         """
         raise NotImplementedError
 
     def get_history(self, limit=None):
-        """
-        Generator that yields (path, rev, chg) tuples, one for each revision in which
-        the node was changed. This generator will follow copies and moves of a
-        node (if the underlying version control system supports that), which
-        will be indicated by the first element of the tuple (i.e. the path)
-        changing.
+        """Provide backward history for this Node.
+        
+        Generator that yields `(path, rev, chg)` tuples, one for each revision
+        in which the node was changed. This generator will follow copies and
+        moves of a node (if the underlying version control system supports
+        that), which will be indicated by the first element of the tuple
+        (i.e. the path) changing.
         Starts with an entry for the current revision.
         """
         raise NotImplementedError
 
     def get_previous(self):
-        """
-        Return the (path, rev, chg) tuple corresponding to the previous
-        revision for that node.
+        """Return the change event corresponding to the previous revision.
+
+        This returns a `(path, rev, chg)` tuple.
         """
         skip = True
         for p in self.get_history(2):
@@ -280,17 +276,25 @@ class Node(object):
                 return p
 
     def get_properties(self):
-        """
-        Returns a dictionary containing the properties (meta-data) of the node.
+        """Returns the properties (meta-data) of the node, as a dictionary.
+
         The set of properties depends on the version control system.
         """
         raise NotImplementedError
 
     def get_content_length(self):
+        """The length in bytes of the content.
+
+        Will be `None` for a directory.
+        """
         raise NotImplementedError
     content_length = property(lambda x: x.get_content_length())
 
     def get_content_type(self):
+        """The MIME type corresponding to the content, if known.
+
+        Will be `None` for a directory.
+        """
         raise NotImplementedError
     content_type = property(lambda x: x.get_content_type())
 
@@ -307,9 +311,7 @@ class Node(object):
 
 
 class Changeset(object):
-    """
-    Represents a set of changes of a repository.
-    """
+    """Represents a set of changes committed at once in a repository."""
 
     ADD = 'add'
     COPY = 'copy'
@@ -333,21 +335,25 @@ class Changeset(object):
             wiki text or not
          * `htmlclass` enables to attach special formatting to the displayed
             property, e.g. `'author'`, `'time'`, `'message'` or `'changeset'`.
+
+        Warning: API will be improved (see #1601 and #2545).
         """
         
     def get_changes(self):
-        """
-        Generator that produces a (path, kind, change, base_path, base_rev)
-        tuple for every change in the changeset, where change can be one of
-        Changeset.ADD, Changeset.COPY, Changeset.DELETE, Changeset.EDIT or
-        Changeset.MOVE, and kind is one of Node.FILE or Node.DIRECTORY.
+        """Generator that produces a tuple for every change in the changeset
+
+        The tuple will contain `(path, kind, change, base_path, base_rev)`,
+        where `change` can be one of Changeset.ADD, Changeset.COPY,
+        Changeset.DELETE, Changeset.EDIT or Changeset.MOVE,
+        and `kind` is one of Node.FILE or Node.DIRECTORY.
         """
         raise NotImplementedError
 
 
 class PermissionDenied(PermissionError):
-    """
-    Exception raised by an authorizer if the user has insufficient permissions
+    """Exception raised by an authorizer.
+
+    This exception is raise if the user has insufficient permissions
     to view a specific part of the repository.
     """
     def __str__(self):
@@ -355,7 +361,8 @@ class PermissionDenied(PermissionError):
 
 
 class Authorizer(object):
-    """
+    """Controls the view access to parts of the repository.
+    
     Base class for authorizers that are responsible to granting or denying
     access to view certain parts of a repository.
     """
