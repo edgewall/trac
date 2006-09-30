b6b0179517bddd1b4f5a6175ad7fb6980451fe71
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2006-09-30 14:14:22 +0000

    Minor cosmetic fixes for `trac.util.autoreload`.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@3812 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/util/autoreload.py
+++ trac/util/autoreload.py
@@ -13,19 +13,21 @@
 
 import os
 import sys
-import time
 import thread
+import time
 
 _SLEEP_TIME = 1
 
 def _reloader_thread(modification_callback):
     """When this function is run from the main thread, it will force other
     threads to exit when any modules currently loaded change.
-    @param modification_callback: Function taking a single argument, the
-    modified file, and is called after a modification is detected."""
+    
+    @param modification_callback: a function taking a single argument, the
+        modified file, which is called every time a modification is detected
+    """
     mtimes = {}
     while True:
-        for filename in filter(None, [getattr(module, "__file__", None)
+        for filename in filter(None, [getattr(module, '__file__', None)
                                       for module in sys.modules.values()]):
             while not os.path.isfile(filename): # Probably in an egg or zip file
                 filename = os.path.dirname(filename)
@@ -34,7 +36,7 @@ def _reloader_thread(modification_callback):
             if not filename: # Couldn't map to physical file, so just ignore
                 continue
 
-            if filename.endswith(".pyc"):
+            if filename.endswith('.pyc'):
                 filename = filename[:-1]
 
             mtime = os.stat(filename).st_mtime
@@ -49,10 +51,10 @@ def _reloader_thread(modification_callback):
 def _restart_with_reloader():
     while True:
         args = [sys.executable] + sys.argv
-        if sys.platform == "win32":
+        if sys.platform == 'win32':
             args = ['"%s"' % arg for arg in args]
         new_environ = os.environ.copy()
-        new_environ["RUN_MAIN"] = 'true'
+        new_environ['RUN_MAIN'] = 'true'
 
         # This call reinvokes ourself and goes into the other branch of main as
         # a new process.
@@ -61,19 +63,16 @@ def _restart_with_reloader():
         if exit_code != 3:
             return exit_code
 
-def main(main_func, modification_callback):
-    """Run `main_func` and restart any time modules are changed."""
-
-    if os.environ.get("RUN_MAIN"):
+def main(func, modification_callback, *args, **kwargs):
+    """Run the given function and restart any time modules are changed."""
+    if os.environ.get('RUN_MAIN'):
         # Lanch the actual program as a child thread
-        thread.start_new_thread(main_func, ())
-
+        thread.start_new_thread(func, args, kwargs)
         try:
             # Now wait for a file modification and quit
             _reloader_thread(modification_callback)
         except KeyboardInterrupt:
             pass
-
     else:
         # Initial invocation just waits around restarting this executable
         try:
