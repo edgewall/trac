9091c2dd81c284d90b3db1607916a8f583a13c52
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2006-10-20 10:46:43 +0000

    Moved more of the Trac javascript code to be jQuery plugins, and some clean-up of the code to take better advantage of jQuery
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@4003 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- htdocs/js/trac.js
+++ htdocs/js/trac.js
@@ -1,3 +1,50 @@
+$.fn.addAnchor = function(title) {
+  title = title || "Link here";
+  return this.filter("*[@id]").each(function() {
+    $("<a class='anchor'> \u00B6</a>").attr("href", "#" + this.id)
+      .title(title).appendTo(this);
+  });
+}
+
+$.fn.checked = function(checked) {
+  if (checked == undefined) { // getter
+    if (!this.length) return false;
+    return this.get(0).checked;
+  } else { // setter
+    return this.each(function() {
+      this.checked = checked;
+    });
+  }
+}
+
+$.fn.enable = function(enabled) {
+  if (enabled == undefined) enabled = true;
+  return this.each(function() {
+    this.disabled = !enabled;
+    var label = $(this).ancestors("label");
+    if (!label.length && this.id) {
+      label = $("label[@for='" + this.id + "']");
+    }
+    if (!enabled) {
+      label.addClass("disabled");
+    } else {
+      label.removeClass("disabled");
+    }
+  });
+}
+
+$.loadStyleSheet = function(href, type) {
+  type = type || "text/css";
+  $(document).ready(function() {
+    if (document.createStyleSheet) { // MSIE
+      document.createStyleSheet(href);
+    } else {
+      $("<link rel='stylesheet type='" + type + "' href='" + href + "' />")
+        .appendTo("head");
+    }
+  });
+}
+
 // Used for dynamically updating the height of a textarea
 function resizeTextArea(id, rows) {
   var textarea = $(id).get(0);
@@ -5,40 +52,20 @@ function resizeTextArea(id, rows) {
   textarea.rows = rows;
 }
 
+// The following are defined for backwards compatibility with releases prior
+// to Trac 0.11
+
 function addEvent(elem, type, func) {
   $(elem).bind(type, func);
 }
-
-// Convenience function for the nearest ancestor element with a specific tag
-// name
+function addHeadingLinks(container, title) {
+  $.each(["h1", "h2", "h3", "h4", "h5", "h6"], function() {
+    $(this, container).addAnchor(title);
+  });
+}
+function enableControl(id, enabled) {
+  $("#" + id).enable(enabled);
+}
 function getAncestorByTagName(elem, tagName) {
   return $(elem).ancestors(tagName).get(0);
-}
-
-function enableControl(id, enabled) {
-  if (enabled == undefined) enabled = true;
-  var control = $("#" + id).get(0);
-  if (!control) return;
-  control.disabled = !enabled;
-  var label = getAncestorByTagName(control, "label");
-  if (label) {
-    label.className = enabled ? "enabled" : "disabled";
-  } else {
-    var labels = document.getElementsByTagName("label");
-    for (var i = 0; i < labels.length; i++) {
-      if (labels[i].htmlFor == id) {
-        labels[i].className = enabled ? "enabled" : "disabled";
-        break;
-      }
-    }
-  }
-}
-
-function addHeadingLinks(container, title) {
-  for (var lvl = 0; lvl <= 6; lvl++) {
-    $("h" + lvl + "[@id]", container).each(function() {
-      $(this).append("<a class=anchor title='" + title.replace(/\$id/, this.id) +
-        "' href='#" + this.id + "'> \u00B6</a>");
-    });
-  }
 }
