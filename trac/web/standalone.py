df6ef4c01cbeaeec081659519679281be0fa7f1c
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2006-11-07 13:00:36 +0000

    Disable remote address lookups in tracd. Closes #3481.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@4161 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/web/standalone.py
+++ trac/web/standalone.py
@@ -98,6 +98,10 @@ class TracHTTPRequestHandler(WSGIRequestHandler):
 
     server_version = 'tracd/' + VERSION
 
+    def address_string(self):
+        # Disable reverse name lookups
+        return self.client_address[:2][0]
+
 
 def main():
     from optparse import OptionParser, OptionValueError
