b000279247f58d42609d6a42c2dfca88b97d535f
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2006-12-07 15:42:20 +0000

    Do as we could get the version info from SilverCity. This only works if you built the package yourself with setuptools, for now. Also took the opportunity to fix #4031.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@4419 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/mimeview/silvercity.py
+++ trac/mimeview/silvercity.py
@@ -25,6 +25,14 @@ from StringIO import StringIO
 from trac.core import *
 from trac.config import ListOption
 from trac.mimeview.api import IHTMLPreviewRenderer, Mimeview
+from trac.util import get_pkginfo
+
+try:
+    import SilverCity
+    have_silvercity = True
+except ImportError:
+    have_silvercity = False
+
 
 __all__ = ['SilverCityRenderer']
 
@@ -79,10 +87,19 @@ class SilverCityRenderer(Component):
     returns_source = True
 
     def __init__(self):
+        self.log.debug("SilverCity installed? %r", have_silvercity)
+        if have_silvercity:
+            version = get_pkginfo(SilverCity).get('version')
+            # TODO: the above works only if setuptools was used to build
+            # SilverCity, which is not yet the case by default for 0.9.7.
+            # I've not been able to find an alternative way to get version.
+            self.env.systeminfo['SilverCity'] = version
         self._types = None
 
     def get_quality_ratio(self, mimetype):
         # Extend default MIME type to mode mappings with configured ones
+        if not have_silvercity:
+            return 0
         if not self._types:
             self._types = {}
             self._types.update(types)
@@ -91,7 +108,6 @@ class SilverCityRenderer(Component):
         return self._types.get(mimetype, (None, 0))[1]
 
     def render(self, req, mimetype, content, filename=None, rev=None):
-        import SilverCity
         try:
             mimetype = mimetype.split(';', 1)[0]
             typelang = self._types[mimetype]
