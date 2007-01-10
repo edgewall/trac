a2f04753e4c74ad483906237c785b582937c0d31
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-01-10 13:01:44 +0000

    trac-post-commit-hook enhancements from markus. Fixes #1310 and #1602.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@4532 af82e41b-90c4-0310-8c96-b1721e28e2e2

---
+++ contrib/trac-post-commit-hook.cmd
@@ -0,0 +1,38 @@
+@ECHO OFF
+::
+:: Trac post-commit-hook script for Windows
+::
+:: Contributed by markus, modified by cboos.
+
+:: Usage:
+::
+:: 1) Insert the following line in your post-commit.bat script
+::
+:: call %~dp0\trac-post-commit-hook.cmd %1 %2
+::
+:: 2) Check the 'Modify paths' section below, be sure to set at least TRAC_ENV
+
+
+:: ----------------------------------------------------------
+:: Modify paths here:
+
+:: -- this one *must* be set
+SET TRAC_ENV=C:\Workspace\local\trac\devel
+
+:: -- set if Python is not in the system path
+SET PYTHON_PATH=
+
+:: -- set to the folder containing trac/ if installed in a non-standard location
+SET TRAC_PATH=C:/Workspace/src/trac/trunk
+:: ----------------------------------------------------------
+
+:: Do not execute hook if trac environment does not exist
+IF NOT EXIST %TRAC_ENV% GOTO :EOF
+
+set PATH=%PYTHON_PATH%;%PATH%
+set PYTHONPATH=%TRAC_PATH%;%PYTHONPATH%
+
+SET REV=%2
+
+Python "%~dp0\trac-post-commit-hook" -p "%TRAC_ENV%" -r "%REV%" 
+
