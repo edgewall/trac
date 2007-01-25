31f9ef554286c52814c50c475d246736689a52be
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-01-25 18:28:30 +0000

    Simplify `trac.loader` a bit by:  * making use of the fact that SetupTools is now a mandatory dependency, and  * dropping support for plugins not using the `trac.plugins` entry point. Also, load `.py` source (single-file) plugins after Python eggs (closes #4383).
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@4633 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/loader.py
+++ trac/loader.py
@@ -18,16 +18,14 @@ from glob import glob
 import imp
 import os
 import sys
-from trac.config import default_dir
 try:
     set
 except NameError:
     from sets import Set as set
 
-try:
-    import pkg_resources
-except ImportError:
-    pkg_resources = None
+import pkg_resources
+
+from trac.config import default_dir
 
 __all__ = ['load_components']
 
@@ -37,9 +35,64 @@ def load_components(env):
                                                                   'plugins'))),
                     default_dir('plugins')]
 
-    # First look for Python source files in the plugins directories, which
-    # simply get imported, thereby registering them with the component manager
-    # if they define any components.
+    # Try to load any eggs from the plugins directory, and also plugins
+    # available on sys.path
+    ws = pkg_resources.working_set
+    for plugins_dir in plugins_dirs:
+        ws.add_entry(plugins_dir)
+    pkg_env = pkg_resources.Environment(plugins_dirs + sys.path)
+
+    memo = set()
+    def flatten(dists):
+         for dist in dists:
+             if dist in memo:
+                 continue
+             memo.add(dist)
+             try:
+                 predecessors = ws.resolve([dist.as_requirement()])
+                 for predecessor in flatten(predecessors):
+                     yield predecessor
+                 yield dist
+             except pkg_resources.DistributionNotFound, e:
+                 env.log.error('Skipping "%s" ("%s" not found)', dist, e)
+             except pkg_resources.VersionConflict, e:
+                 env.log.error('Skipping "%s" (version conflict: "%s")',
+                               dist, e)
+
+    for egg in flatten([pkg_env[name][0] for name in pkg_env]):
+        modules = []
+
+        for name in egg.get_entry_map('trac.plugins'):
+            # Load plugins declared via the `trac.plugins` entry point.
+            # This is the only supported option going forward, the
+            # others will be dropped at some point in the future.
+            env.log.debug('Loading egg plugin %s from %s', name, egg.location)
+            egg.activate()
+            try:
+                entry_point = egg.get_entry_info('trac.plugins', name)
+                if entry_point.module_name not in loaded_components:
+                    try:
+                        entry_point.load()
+                    except pkg_resources.DistributionNotFound, e:
+                        env.log.warning('Cannot load plugin %s because it '
+                                        'requires "%s"', name, e)
+                    modules.append(entry_point.module_name)
+                    loaded_components.append(entry_point.module_name)
+            except ImportError, e:
+                env.log.error('Failed to load plugin %s from %s', name,
+                              egg.location, exc_info=True)
+
+        if modules:
+            # Automatically enable any components provided by plugins
+            # loaded from the environment plugins directory.
+            if os.path.dirname(egg.location) == plugins_dirs[0]:
+                for module in modules:
+                    if module + '.*' not in env.config['components']:
+                        env.config['components'].set(module + '.*', 'enabled')
+
+    # Look for Python source files in the plugins directories, which simply get
+    # imported, thereby registering them with the component manager if they
+    # define any components.
     for plugins_dir in plugins_dirs:
         auto_enable = plugins_dir != default_dir('plugins')
         plugin_files = glob(os.path.join(plugins_dir, '*.py'))
@@ -58,80 +111,6 @@ def load_components(env):
             except Exception, e:
                 env.log.error('Failed to load plugin from %s', plugin_file,
                               exc_info=True)
-
-    # If setuptools is installed try to load any eggs from the plugins
-    # directory, and also plugins available on sys.path
-    if pkg_resources is not None:
-        ws = pkg_resources.working_set
-        for plugins_dir in plugins_dirs:
-            ws.add_entry(plugins_dir)
-        pkg_env = pkg_resources.Environment(plugins_dirs + sys.path)
-
-        memo = set()
-        def flatten(dists):
-             for dist in dists:
-                 if dist in memo:
-                     continue
-                 memo.add(dist)
-                 try:
-                     predecessors = ws.resolve([dist.as_requirement()])
-                     for predecessor in flatten(predecessors):
-                         yield predecessor
-                     yield dist
-                 except pkg_resources.DistributionNotFound, e:
-                     env.log.error('Skipping "%s" ("%s" not found)', dist, e)
-                 except pkg_resources.VersionConflict, e:
-                     env.log.error('Skipping "%s" (version conflict: "%s")',
-                                   dist, e)
-
-        for egg in flatten([pkg_env[name][0] for name in pkg_env]):
-            modules = []
-
-            for name in egg.get_entry_map('trac.plugins'):
-                # Load plugins declared via the `trac.plugins` entry point.
-                # This is the only supported option going forward, the
-                # others will be dropped at some point in the future.
-                env.log.debug('Loading egg plugin %s from %s', name,
-                              egg.location)
-                egg.activate()
-                try:
-                    entry_point = egg.get_entry_info('trac.plugins', name)
-                    if entry_point.module_name not in loaded_components:
-                        try:
-                            entry_point.load()
-                        except pkg_resources.DistributionNotFound, e:
-                            env.log.warning('Cannot load plugin %s because it '
-                                            'requires "%s"', name, e)
-                        modules.append(entry_point.module_name)
-                        loaded_components.append(entry_point.module_name)
-                except ImportError, e:
-                    env.log.error('Failed to load plugin %s from %s', name,
-                                  egg.location, exc_info=True)
-
-            else:
-                # Support for pre-entry-point plugins
-                if egg.has_metadata('trac_plugin.txt'):
-                    env.log.debug('Loading plugin %s', egg.location)
-                    egg.activate()
-                    for modname in egg.get_metadata_lines('trac_plugin.txt'):
-                        module = None
-                        if modname not in loaded_components:
-                            try:
-                                module = __import__(modname)
-                                loaded_components.append(modname)
-                                modules.append(modname)
-                            except ImportError, e:
-                                env.log.error('Component module %s not found',
-                                              modname, exc_info=True)
-
-            if modules:
-                # Automatically enable any components provided by plugins
-                # loaded from the environment plugins directory.
-                if os.path.dirname(egg.location) == plugins_dirs[0]:
-                    for module in modules:
-                        if module + '.*' not in env.config['components']:
-                            env.config['components'].set(module + '.*',
-                                                         'enabled')
 
     # Load default components
     from trac.db_default import default_components
