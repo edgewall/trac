edgewall/trac

commit 54e6a64e13e5a9154f18a120c17e9f0cf4484b64

Ported r4728:4729 to 0.10-stable (re #4531)
git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.10-stable@4730 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/web/standalone.py
@@ -176,9 +176,13 @@ def main():
     if not args and not options.env_parent_dir:
         parser.error('either the --env-parent-dir option or at least one '
                      'environment must be specified')
-    if options.single_env and len(args) > 1:
-        parser.error('the --single-env option cannot be used with more '
-                     'than one enviroment')
+    if options.single_env:
+        if options.env_parent_dir:
+            parser.error('the --single-env option cannot be used with '
+                         '--env-parent-dir')
+        elif len(args) > 1:
+            parser.error('the --single-env option cannot be used with '
+                         'more than one enviroment')
 
     if options.port is None:
         options.port = {
@@ -188,10 +192,21 @@ def main():
         }[options.protocol]
     server_address = (options.hostname, options.port)
 
+    # autoreload doesn't work when daemonized and using relative paths
+    if options.daemonize and options.autoreload:
+        for path in args + [options.env_parent_dir, options.pidfile]:
+            if not os.path.isabs(path):
+                parser.error('"%s" is not an absolute path.\n\n'
+                             'when using both --auto-reload and --daemonize '
+                             'all path arguments must be absolute'
+                             % path)
+
     # relative paths don't work when daemonized
     args = [os.path.abspath(a) for a in args]
     if options.env_parent_dir:
         options.env_parent_dir = os.path.abspath(options.env_parent_dir)
+    if options.pidfile:
+        options.pidfile = os.path.abspath(options.pidfile)
 
     wsgi_app = TracEnvironMiddleware(dispatch_request,
                                      options.env_parent_dir, args,

commit 09c5cc37a8036bd57b3534952575f9bf087f18c3

0.10.4dev: ported r4748 to 0.10-stable (''fix for tracd on Windows, broken by r4730'')
git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.10-stable@4753 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/web/standalone.py
@@ -205,7 +205,7 @@ def main():
     args = [os.path.abspath(a) for a in args]
     if options.env_parent_dir:
         options.env_parent_dir = os.path.abspath(options.env_parent_dir)
-    if options.pidfile:
+    if parser.has_option('pidfile') and options.pidfile:
         options.pidfile = os.path.abspath(options.pidfile)
 
     wsgi_app = TracEnvironMiddleware(dispatch_request,