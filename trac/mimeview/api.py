666f885be86b39d622cfcaaa8eae8c37c372b94c
mgood <mgood@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-02-17 20:37:39 +0000

    Ported r4778 to 0.10-stable (fixes #4763)
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.10-stable@4779 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/mimeview/api.py
+++ trac/mimeview/api.py
@@ -79,7 +79,8 @@ KNOWN_MIME_TYPES = {
     'application/x-sh':       ['sh'],
     'application/x-csh':      ['csh'],
     'application/x-troff':    ['nroff', 'roff', 'troff'],
-    'application/x-troff':    ['nroff', 'roff', 'troff'],
+    'application/x-yaml':     ['yml', 'yaml'],
+    
     'application/rss+xml':    ['rss'],
     
     'image/x-icon':           ['ico'],
