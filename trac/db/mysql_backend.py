d23e20a9013b2a6aa1616791f75a10ff9586b173
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-03-11 11:24:59 +0000

    MySQL connection wrapper improvements:   - Follow-up to r4341, ping before rollback   - Try to not close an already closed connection, should fix #4802
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@4962 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/db/mysql_backend.py
+++ trac/db/mysql_backend.py
@@ -161,6 +161,7 @@ class MySQLConnection(ConnectionWrapper):
                                   host=host, port=port, use_unicode=True)
             self._set_character_set(cnx, 'utf8')
         ConnectionWrapper.__init__(self, cnx)
+        self._is_closed = False
 
     def cast(self, column, type):
         if type == 'int':
@@ -180,5 +181,12 @@ class MySQLConnection(ConnectionWrapper):
         return self.cnx.insert_id()
 
     def rollback(self):
-        self.cnx.rollback()
-        self.cnx.ping()
+        if self.cnx.ping():
+            self.cnx.rollback()
+        else:
+            self._is_closed = True
+
+    def close(self):
+        if not self._is_closed:
+            self.cnx.close()
+            self._is_closed = True
