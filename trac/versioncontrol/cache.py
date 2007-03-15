64ebea4f400a8e66ac5fa947adb5ae7ca86a6f45
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-03-15 13:10:04 +0000

    0.10.4dev: forgot to rename one occurrence of previous_repository_dir
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.10-stable@5037 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/versioncontrol/cache.py
+++ trac/versioncontrol/cache.py
@@ -71,7 +71,7 @@ class CachedRepository(Repository):
         if repository_dir:
             if repository_dir != self.name:
                 self.log.info("'repository_dir' has changed from %r to %r"
-                              % (previous_repository_dir, self.name))
+                              % (repository_dir, self.name))
                 raise TracError("The 'repository_dir' has changed, "
                                 "a 'trac-admin resync' operation is needed.")
         elif repository_dir is None: # no 'repository_dir' stored yet
