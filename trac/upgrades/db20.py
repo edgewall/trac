7936cf88a7f9e881d6568987d55d55843cbb8c0f
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-03-24 17:11:47 +0000

    Upgrade database version 20 should succeed even for environment not using the versioncontrol subsystem. Closes #5015.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@5122 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/upgrades/db20.py
+++ trac/upgrades/db20.py
@@ -1,16 +1,22 @@
 from trac.db import Table, Column, Index, DatabaseManager
+from trac.core import TracError
 from trac.versioncontrol.cache import CACHE_YOUNGEST_REV
 
 def do_upgrade(env, ver, cursor):
-    """Modify the repository cache scheme.
+    """Modify the repository cache scheme (if needed)
 
     Now we use the 'youngest_rev' entry in the system table
     to explicitly store the youngest rev in the cache.
     """
     db = env.get_db_cnx()
-    repos = env.get_repository(None, sync=False)
-    youngest = repos.get_youngest_rev_in_cache(db) or ''
-    # deleting first, for the 0.11dev and 0.10.4dev users
-    cursor.execute("DELETE FROM system WHERE name=%s", (CACHE_YOUNGEST_REV,))
-    cursor.execute("INSERT INTO system (name, value) VALUES (%s, %s)",
-                   (CACHE_YOUNGEST_REV, youngest))
+    try:
+        repos = env.get_repository(None, sync=False)
+        youngest = repos.get_youngest_rev_in_cache(db) or ''
+        # deleting first, for the 0.11dev and 0.10.4dev users
+        cursor.execute("DELETE FROM system WHERE name=%s",
+                       (CACHE_YOUNGEST_REV,))
+        cursor.execute("INSERT INTO system (name, value) VALUES (%s, %s)",
+                       (CACHE_YOUNGEST_REV, youngest))
+    except TracError: # no repository available
+        pass
+        
