6198b46a9cbb4c9297ff389f2710faee29175917
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-04-14 14:22:31 +0000

    Preparing the first release candidate for milestone:0.10.4.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.10-stable@5204 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/__init__.py
+++ trac/__init__.py
@@ -11,7 +11,7 @@ U{http://trac.edgewall.org/}
 """
 __docformat__ = 'epytext en'
 
-__version__ = '0.10.4dev'
+__version__ = '0.10.4rc1'
 __url__ = 'http://trac.edgewall.org/'
 __copyright__ = '(C) 2003-2006 Edgewall Software'
 __license__ = 'BSD'
