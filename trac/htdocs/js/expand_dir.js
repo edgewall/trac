822c89cb4284729e0e509bba6d7fa7805e54d602
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-05-10 12:39:15 +0000

    TracBrowser: simplified the directory expansion code. In this new approach, the expander is wrapped around the <a> element corresponding to the directory entry. This is a simplification of r5351.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@5358 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/htdocs/js/expand_dir.js
+++ trac/htdocs/js/expand_dir.js
@@ -1,6 +1,7 @@
 // Enable expanding/folding folders in TracBrowser
 
-var counter = 0;
+var FOLDERID_COUNTER = 0;
+var SUBFOLDER_INDENT = 20;
 
 // enableExpandDir adds the capability to folder rows to be expanded and folded
 // It also teach the rows about their ancestors. It expects:
@@ -8,90 +9,89 @@ var counter = 0;
 //  - a `rows` jQuery object matching the newly created entry rows
 
 function enableExpandDir(parent_tr, rows) {
-  var entries = [];
-  var ancestor_folderids = [];
   // the ancestors folder ids are present in the parent_tr class attribute
+  var ancestor_folderids = [];
   if (parent_tr)
     ancestor_folderids = $.grep(parent_tr.attr("class").split(" "), 
 				function(c) { return c.match(/^f\d+$/)});
   rows.each(function () {
-    var folderid = "f" + counter++;
-    this.id = folderid;
-    if (parent_tr) {
-      entries.push("#"+folderid);
-      $(this).addClass(ancestor_folderids.join(" "));
+    var a = $(this).find("a.dir");
+
+    if (a.length) { // then the entry is a folder
+      // create new folder id
+      var folderid = "f" + FOLDERID_COUNTER++;
+      this.id = folderid;
+      $(this).addClass(folderid);
+
+      // add the expander icon
+      a.wrap('<span class="expander"></span>');
+      var expander = a.parent();
+      expander.css("cursor", "pointer")
+        .attr("title", "Expand sub-directory in place").click(toggleDir);
     }
-    $(this).addClass(folderid);
-  }).find("span.direxpand").css("cursor", "pointer").click(toggleDir);
-  
-  if (parent_tr)
-    parent_tr.get(0)._entries = entries;
+
+    // tie that row to ancestor folders
+    if (parent_tr)
+      $(this).addClass(ancestor_folderids.join(" "));
+  });
 }
 
 // handler for click event on the expander icons
 function toggleDir() {
-  var td = $(this).parent();
-  if ( $(this).attr("class") == "direxpand" ) {
-    $(this).attr("class", "dirfold").attr("title", "Fold directory");
-    expandDir(td);
-  } else {
-    $(this).attr("class", "direxpand").attr("title", "Expand directory");
-    foldDir(td.parent());
-  }
-}
-
-// expand action, which either queries the entries or show again the already
-// available but hidden entries
-function expandDir(td) {
-  var tr = td.parent();
+  var tr = $(this).parents("tr");
   var folderid = tr.get(0).id;
 
-  if (tr.get(0)._entries) { // then simply re-expand collapsed folder
-    tr.siblings("tr."+folderid).show();
-    // as the above shows all, take care to fold again what was already folded
-    restoreEntries(tr);
+  if ( tr.filter(".expanded").length ) { // then *fold*
+    tr.removeClass("expanded").addClass("collapsed");
+    tr.siblings("tr."+folderid).hide();
+    $(this).attr("title", "Re-expand directory");
     return;
   }
 
-  var a = td.children("a");
-  var href = a.attr("href");
-  var depth = parseFloat(td.css("padding-left").replace(/^(\d*\.\d*).*$/, 
-							"$1")) + 20;
-  // insert "Loading ..." row
-  tr.after('<tr><td class="name" colspan="5" style="padding-left: ' +
-	   depth + 'px"><span class="loading">Loading ' + a.text() +
-	   '...</span></td></tr>');
-
-  $.get(href, {action: "inplace"}, function(data) {
-    var rows = $(data.replace(/^<!DOCTYPE[^>]+>/, "")).filter("tr");
-    if (rows.length) {
-      // remove "Loading ..." row
-      tr.next().remove();
-      // insert rows corresponding to the folder entries
-      rows.children("td.name").css("padding-left", depth);
-      enableExpandDir(tr, rows);
-      tr.after(rows);
-    } else {
-      tr.next().find("span.loading").text("").append("<i>(empty)</i>")
-        .removeClass("loading");
-      enableExpandDir(tr, tr.next());
-    }
-  });
-}
-
-function foldDir(tr) {
-  var folderid = tr.get(0).id;
-  tr.siblings("tr."+folderid).hide();
-}
-
-function restoreEntries(tr) {
-  var entries = tr.get(0)._entries;
-  if (entries)
-    $.each(entries, function (i, entry) {
-      var entry_tr = $(entry);
-      if (entry_tr.find("span.direxpand").length)
-        foldDir(entry_tr);
-      else
-        restoreEntries(entry_tr);
+  if ( tr.filter(".collapsed").length ) { // then *expand*
+    tr.removeClass("collapsed").addClass("expanded");
+    tr.siblings("tr."+folderid).show();
+    // Note that the above will show all the already fetched subtree,
+    // so we have to fold again the folders which were already collapsed.
+    tr.siblings("tr.collapsed").each(function() {
+      $(this).siblings("tr."+this.id).hide();
     });
+  } else {                                // then *fetch*
+    var td = $(this).parent();
+    var td_class = td.attr("class");
+    var a = $(this).children("a");
+    var depth = 
+      parseFloat(td.css("padding-left").replace(/^(\d*\.\d*).*$/, "$1")) + 
+      SUBFOLDER_INDENT;
+
+    tr.addClass("expanded");
+    // insert "Loading ..." row
+    tr.after('<tr><td><span class="loading"></span></td></tr>');
+    var loading_row = tr.next();
+    loading_row.children("td").addClass(td_class)
+      .attr("colspan", tr.children("td").length)
+      .css("padding-left", depth);
+    loading_row.find("span.loading").text("Loading " + a.text() + "...");
+
+    $.get(a.attr("href"), {action: "inplace"}, function(data) {
+      var rows = $(data.replace(/^<!DOCTYPE[^>]+>/, "")).filter("tr");
+      if (rows.length) {
+        // insert rows corresponding to the folder entries
+        rows.children("td."+td_class).css("padding-left", depth);
+	// make all rows collapsible and subdir rows expandable
+        enableExpandDir(tr, rows); 
+        tr.after(rows);
+        // remove "Loading ..." row
+        loading_row.remove();
+      } else {
+        loading_row.find("span.loading").text("").append("<i>(empty)</i>")
+          .removeClass("loading");
+	// make the (empty) row collapsible
+        enableExpandDir(tr, loading_row); 
+      }
+    });
+  }
+  $(this).attr("title", "Fold directory");
 }
+
+
