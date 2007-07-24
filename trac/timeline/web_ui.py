fb826c682aaca7adc8dbc844f28897803f1b581c
cmlenz <cmlenz@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-07-24 22:03:55 +0000

    Fix missing import in backwards-compatibility code of the timeline.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@5866 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/timeline/web_ui.py
+++ trac/timeline/web_ui.py
@@ -26,6 +26,7 @@ from urlparse import urlparse
 from genshi.builder import tag
 
 from trac.config import IntOption, BoolOption
+from trac.context import Context
 from trac.core import *
 from trac.perm import IPermissionRequestor
 from trac.timeline.api import ITimelineEventProvider, TimelineEvent
