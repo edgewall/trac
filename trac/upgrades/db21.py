ee8ed06b9cafce2ff7090d7fd1080e65ec82caa3
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-08-24 15:32:47 +0000

    The db21 upgrade step now copes with a report description or a report query being None
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@5963 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/upgrades/db21.py
+++ trac/upgrades/db21.py
@@ -6,16 +6,17 @@ def do_upgrade(env, ver, cursor):
     cursor.execute('SELECT id, query, description FROM report')
     reports = cursor.fetchall()
     for report, query, description in reports:
-        # All states other than 'closed' are "active".
-        query = query.replace("IN ('new', 'assigned', 'reopened')",
-                              "<> 'closed'")
-        # Add a status column instead of adding an '*' to the owner's name for
-        # the 'assigned' state.
-        query = query.replace("(CASE status WHEN 'assigned' THEN %s "
-                              "ELSE owner END) AS owner" % owner, "owner, status")
-        description = description.replace(" * If a ticket has been accepted, "
-                                          "a '*' is appended after the "
-                                          "owner's name\n",
-                                          '')
-        cursor.execute("UPDATE report SET query=%s, description=%s "
-                       "WHERE id=%s", (query, description, report))
+        q, d = query, description
+        if query:
+            # All states other than 'closed' are "active".
+            q = q.replace("IN ('new', 'assigned', 'reopened')", "<> 'closed'")
+            # Add a status column instead of adding an '*' to the owner's name
+            # for the 'assigned' state.
+            q = q.replace("(CASE status WHEN 'assigned' THEN %s "
+                          "ELSE owner END) AS owner" % owner, "owner, status")
+        if description:
+            d = d.replace(" * If a ticket has been accepted, a '*' is"
+                          " appended after the owner's name\n", '')
+        if q != query or d != description:
+            cursor.execute("UPDATE report SET query=%s, description=%s "
+                           "WHERE id=%s", (q, d, report))
