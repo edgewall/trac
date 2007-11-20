32dd2099f97add61cb31df7af796876a95695bb1
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2007-11-20 17:50:11 +0000

    Add a sample permission plugin for illustrating the check on realm resources, related to #6211.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@6189 af82e41b-90c4-0310-8c96-b1721e28e2e2

---
+++ sample-plugins/public_wiki_policy.py
@@ -0,0 +1,58 @@
+from fnmatch import fnmatchcase
+
+from trac.config import Option
+from trac.core import *
+from trac.perm import IPermissionPolicy
+
+class PublicWikiPolicy(Component):
+    """Sample permission policy plugin illustrating how to check 
+    permission on realms.
+
+    Don't forget to integrate that plugin in the appropriate place in the
+    list of permission policies:
+    {{{
+    [trac]
+    permission_policies = PublicWikiPolicy, DefaultPermissionPolicy
+    }}}
+
+    Then you can configure which pages you want to make public:
+    {{{
+    [public_wiki]
+    view = Public*
+    modify = PublicSandbox/*
+    }}}
+
+    """
+
+    implements(IPermissionPolicy)
+
+    view = Option('public_wiki', 'view', 'Public*',
+            """Case-sensitive glob pattern used for granting view permission on
+            all Wiki pages matching it.""")
+
+    modify = Option('public_wiki', 'modify', 'Public*',
+            """Case-sensitive glob pattern used for granting modify permissions
+            on all Wiki pages matching it.""")
+
+    def check_permission(self, action, username, resource, perm):
+        if resource: # fine-grained permission check
+            if resource.realm == 'wiki': # wiki realm or resource
+                if resource.id: # ... it's a resource
+                    if action == 'WIKI_VIEW': # (think 'VIEW' here)
+                        pattern = self.view
+                    else:
+                        pattern = self.modify
+                    if fnmatchcase(resource.id, pattern):
+                        return True
+                else: # ... it's a realm
+                    return True 
+                    # this policy ''may'' grant permissions on some wiki pages
+        else: # coarse-grained permission check
+            # 
+            # support for the legacy permission checks: no resource specified
+            # and realm information in the action name itself.
+            #
+            if action.startswith('WIKI_'):
+                return True
+                # this policy ''may'' grant permissions on some wiki pages
+
