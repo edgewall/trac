dc053cd5a94ef52a25e7f7f49ec44d776ffe7446
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2008-05-13 14:30:27 +0000

    0.10.5dev: backported r6240 (''only use `[trac] base_url` for redirects when `[trac] use_base_url_for_redirect` is set, #5064).
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.10-stable@7061 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/web/main.py
+++ trac/web/main.py
@@ -389,7 +389,7 @@ def dispatch_request(environ, start_response):
     env = env_error = None
     try:
         env = _open_environment(env_path, run_once=run_once)
-        if env.base_url:
+        if env.base_url_for_redirect:
             environ['trac.base_url'] = env.base_url
     except TracError, e:
         env_error = e
