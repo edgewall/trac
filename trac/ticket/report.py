edgewall/trac

commit e88c14feacbc583a5432f91eeac8d1a85020bd8c

Make user sort in reports do the rigth thing for enumerations (comparing the value as int instead of comparing the name).
This hardcodes the name of those enumerations, but as there's no way of introspecting the schema, this is better than nothing.

Contributed by vnaum on #4715, thanks!


git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@6842 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/ticket/report.py
@@ -303,8 +303,27 @@ class ReportModule(Component):
 
             if col == sort_col:
                 header['asc'] = asc
+                
+                # this dict will have enum values for sorting
+                # and will be used in sortkey(), if non-empty:
+                sort_values = {}
+                if sort_col in ['status', 'resolution', 'priority', 'severity']:
+                    # must fetch sort values for that columns
+                    # instead of comparing them as strings
+                    if not db:
+                        db = self.env.get_db_cnx()
+                    cursor = db.cursor()
+                    cursor.execute("SELECT name," + db.cast('value', 'int') + 
+                                   " FROM enum WHERE type=%s", (sort_col,))
+                    for name, value in cursor:
+                        sort_values[name] = value
+
                 def sortkey(row):
                     val = row[idx]
+                    # check if we have sort_values, then use them as sort keys.
+                    if sort_values:
+                        return sort_values.get(val)
+                    # otherwise, continue with string comparison:
                     if isinstance(val, basestring):
                         val = val.lower()
                     return val

commit 0dff7fafb0ad2299926917af9283075850496415

Restore grouping functionality for reports, broken by r6842.
Fix contributed by Jasmin Lapalme, many thanks!

Closes #7179.


git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.11-stable@7130 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/ticket/report.py
@@ -547,23 +547,19 @@ class ReportModule(Component):
             self.env.log.debug("Colnum Names %s, Sort column %s" %
                                (str(cols), sort_col))
             order_cols = []
-            try:
-                group_idx = cols.index('__group__')
-                order_cols.append(str(group_idx))
-            except ValueError:
-                pass
+            if '__group__' in cols:
+                order_cols.append('__group__')
 
             if sort_col:
-                try:
-                    sort_idx = cols.index(sort_col) + 1
-                    order_cols.append(str(sort_idx))
-                except ValueError:
+                if sort_col in cols:
+                    order_cols.append(sort_col)
+                else:
                     raise TracError(_('Query parameter "sort=%(sort_col)s" '
                                       ' is invalid', sort_col=sort_col))
 
             # The report-query results is obtained
             asc_str = ['DESC', 'ASC']
-            asc_idx = int(req.args.get('asc','0'))
+            asc_idx = int(req.args.get('asc','1'))
             order_by = ''
             if len(order_cols) != 0:
                 dlmt = ", "