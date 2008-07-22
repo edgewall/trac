22aeaf4302785860d2cd5033ab3d5c59269704d6
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2008-07-22 08:10:12 +0000

    Don't retarget tickets for edited milestones that are not set to completed at the same time.
    Closes #5941.
    
    Patch contributed by Remy Blank.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.11-stable@7372 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/ticket/roadmap.py
+++ trac/ticket/roadmap.py
@@ -644,7 +644,7 @@ class MilestoneModule(Component):
         if milestone.exists:
             milestone.update()
             # eventually retarget opened tickets associated with the milestone
-            if 'retarget' in req.args:
+            if 'retarget' in req.args and completed:
                 cursor = db.cursor()
                 cursor.execute("UPDATE ticket SET milestone=%s WHERE "
                                "milestone=%s and status != 'closed'",
