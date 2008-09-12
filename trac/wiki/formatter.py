4d900abfd5a3aa309fec77fa8b28592f5d7cffcb
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2008-09-12 13:45:52 +0000

    0.12dev: merged [7395-7399] via svnmerge from [source:branches/0.11-stable]
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@7532 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/wiki/formatter.py
+++ trac/wiki/formatter.py
@@ -860,8 +860,6 @@ class Formatter(object):
             if not line.startswith(' '):
                 self._tabstops = []
 
-            if escape_newlines:
-                line += ' [[BR]]'
             self.in_list_item = False
             self.in_quote = False
             # Throw a bunch of regexps on the problem
@@ -876,13 +874,16 @@ class Formatter(object):
             if self.in_def_list and not line.startswith(' '):
                 self.close_def_list()
 
-            if self.in_table and line.strip()[0:2] != '||':
+            if self.in_table and not line.lstrip().startswith('||'):
                 self.close_table()
 
-            if len(result) and not self.in_list_item and not self.in_def_list \
-                    and not self.in_table:
-                self.open_paragraph()
-            self.out.write(result + os.linesep)
+            sep = os.linesep
+            if not(self.in_list_item or self.in_def_list or self.in_table):
+                if len(result):
+                    self.open_paragraph()
+                if escape_newlines and not result.rstrip().endswith('<br />'):
+                    sep = '<br />' + sep
+            self.out.write(result + sep)
             self.close_table_row()
 
         self.close_table()
