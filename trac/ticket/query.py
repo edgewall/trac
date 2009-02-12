807cc7e5a000ccbcbfdba7a339870f6cb050e0d8
rblank <rblank@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2009-02-12 00:48:16 +0000

    0.11-stable: Allow multiple keys with the same name in ticket query syntax. Patch by Stephen Compall.
    Closes #8041.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.11-stable@7863 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/ticket/query.py
+++ trac/ticket/query.py
@@ -152,13 +152,13 @@ class Query(object):
                 if field in kw_strs:
                     kw[field] = processed_values[0]
                 elif field in kw_arys:
-                    kw[field] = processed_values
+                    kw.setdefault(field, []).extend(processed_values)
                 elif field in kw_bools:
                     kw[field] = True
                 elif field == 'col':
                     cols.extend(processed_values)
                 else:
-                    constraints[field] = processed_values
+                    constraints.setdefault(field, []).extend(processed_values)
             except UnicodeError:
                 pass # field must be a str, see `get_href()`
         report = constraints.pop('report', None)
@@ -1111,6 +1111,7 @@ class TicketQueryMacro(WikiMacroBase):
                          class_=ticket['status'],
                          href=req.href.ticket(int(ticket['id'])),
                          title=shorten_line(ticket['summary']))
+
         def ticket_groups():
             groups = []
             for v, g in groupby(tickets, lambda t: t[query.group]):
