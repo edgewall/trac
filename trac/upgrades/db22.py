64a442896a47fc63543939fffcd9f88a6e2ac6c1
rblank <rblank@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2009-04-08 20:11:33 +0000

    0.12dev: Renamed the `key` column of the `cache` table to `id` for MySQL compatibility, and changed the type of the `generation` column to `int`.
    '''For people who have already upgraded their database to version 22, this requires manual intervention on the database.'''
    
    You can either remove the `cache` table and re-create it by hand:
    {{{
    DROP TABLE cache;
    CREATE TABLE cache ( id text PRIMARY KEY, generation int );
    }}}
    (for SQLite; or the equivalent for your database)
    
    Or you can downgrade the database version, drop the `cache` table, and upgrade again:
    {{{
    UPDATE system SET value=21 WHERE name='database_version';
    DROP TABLE cache;
    
    trac-admin $TRAC_ENV upgrade
    }}}
    
    Closes #8197.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@8079 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/upgrades/db22.py
+++ trac/upgrades/db22.py
@@ -2,9 +2,9 @@ from trac.db import Table, Column, DatabaseManager
 
 def do_upgrade(env, ver, cursor):
     """Add the cache table."""
-    table = Table('cache', key='key')[
-        Column('key'),
-        Column('generation')
+    table = Table('cache', key='id')[
+        Column('id'),
+        Column('generation', type='int')
     ]
     db_connector, _ = DatabaseManager(env)._get_connector()
     for stmt in db_connector.to_sql(table):
