bfa47d1f3b6eaea7fe483f2804d3cef96e525573
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2009-05-24 13:04:38 +0000

    0.11.5dev: ![[MacroList]] should be more robust when plugins give back a buggy non-unicode description.
    Fixes #8286.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.11-stable@8236 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/wiki/macros.py
+++ trac/wiki/macros.py
@@ -462,7 +462,7 @@ class MacroListMacro(WikiMacroBase):
                     try:
                         descr = macro_provider.get_macro_description(macro_name)
                         descr = format_to_html(self.env, formatter.context,
-                                               descr or '')
+                                               to_unicode(descr) or '')
                     except Exception, e:
                         descr = system_message(_("Error: Can't get description "
                                                  "for macro %(name)s",
