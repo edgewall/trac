ff744143327e924879333e93ded59a1574ed9ec4
osimons <osimons@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2009-06-30 12:19:00 +0000

    0.11.5dev: Modified the import workaround code for older versions of Genshi to only trigger if Genshi is actually installed as a zipped egg. See #7823.
    The workaround code is relevant for:
     * Genshi 0.5.2dev < genshi:changeset:947
     * Genshi 0.6dev < genshi:changeset:928
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.11-stable@8335 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/web/__init__.py
+++ trac/web/__init__.py
@@ -3,11 +3,14 @@
 # PYTHON_EGG_CACHE variable is set from there
 #
 # TODO: Remove this once the Genshi zip_safe issue has been resolved.
-try:
-    import mod_python.apache
-    import sys
-    if 'trac.web.modpython_frontend' in sys.modules:
+from pkg_resources import get_provider, ZipProvider
+if isinstance(get_provider('genshi'), ZipProvider):
+    try:
+        import mod_python.apache
+        import sys
+        if 'trac.web.modpython_frontend' in sys.modules:
+            from trac.web.api import *
+    except ImportError:
         from trac.web.api import *
-except ImportError:
+else:
     from trac.web.api import *
-
