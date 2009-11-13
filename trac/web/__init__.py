80cf6bfb10d1aaef0ee714aaf223b7488d1106a5
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2009-11-13 16:02:53 +0000

    0.12dev: finally merged r8335 and its follow-up r8354 from 0.11-stable (#7823). We might still need that workaround on trunk if Genshi is made zip_safe again (see [G1089]).
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@8809 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/web/__init__.py
+++ trac/web/__init__.py
@@ -3,11 +3,15 @@
 # PYTHON_EGG_CACHE variable is set from there
 #
 # TODO: Remove this once the Genshi zip_safe issue has been resolved.
-try:
-    import mod_python.apache
-    import sys
-    if 'trac.web.modpython_frontend' in sys.modules:
+import os
+from pkg_resources import get_distribution
+if not os.path.isdir(get_distribution('genshi').location):
+    try:
+        import mod_python.apache
+        import sys
+        if 'trac.web.modpython_frontend' in sys.modules:
+            from trac.web.api import *
+    except ImportError:
         from trac.web.api import *
-except ImportError:
+else:
     from trac.web.api import *
-
