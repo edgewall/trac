0e118c82b3eb45e937b11ae826bf2fce49a9343b
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2010-02-16 14:15:09 +0000

    versioncontrol: refactor cache tests, hopefully a bit clearer now.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@9189 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/versioncontrol/tests/cache.py
+++ trac/versioncontrol/tests/cache.py
@@ -31,25 +31,52 @@ class CacheTestCase(unittest.TestCase):
         self.db = self.env.get_db_cnx()
         self.log = self.env.log
         cursor = self.db.cursor()
-        cursor.executemany("INSERT INTO repository (id,name,value) "
-                           "VALUES (%s,%s,%s)",
-                           [(1, 'name', 'test-repos'),
-                            (1, 'youngest_rev', '')])
+        cursor.executemany("""
+            INSERT INTO repository (id,name,value) VALUES (%s,%s,%s)
+            """, [(1, 'name', 'test-repos'),
+                  (1, 'youngest_rev', '')])
 
     def tearDown(self):
         self.env.reset_db()
 
+    # Helpers
+
+    def get_repos(self, get_changeset=None, youngest_rev=1):
+        if get_changeset is None:
+            def no_changeset(rev):
+                raise NoSuchChangeset(rev)
+            get_changeset = no_changeset
+        return Mock(Repository, 'test-repos', {'name': 'test-repos', 'id': 1},
+                    self.log,
+                    get_changeset=get_changeset,
+                    get_oldest_rev=lambda: 0,
+                    get_youngest_rev=lambda: youngest_rev,
+                    normalize_rev=lambda x: get_changeset(x).rev,
+                    next_rev=(lambda x: int(x) < youngest_rev and x + 1 \
+                              or None))
+
+    def preset_cache(self, *args):
+        """Each arg is a (rev tuple, changes list of tuples) pair."""
+        cursor = self.db.cursor()
+        for i, (rev, changes) in enumerate(args):
+            cursor.execute("""
+                INSERT INTO revision (repos,rev,time,author,message)
+                VALUES (1,%s,%s,%s,%s)
+                """, rev)
+            if changes:
+                cursor.executemany("""
+                    INSERT INTO node_change
+                    (repos,rev,path,node_type,change_type,base_path,base_rev)
+                    VALUES (1,%s,%s,%s,%s,%s,%s)
+                    """, [(str(i),) + change for change in changes])
+        cursor.execute("""
+            UPDATE repository SET value=%s WHERE id=1 AND name='youngest_rev'
+            """, (str(len(args) - 1),))
+
+    # Tests
+
     def test_initial_sync_with_empty_repos(self):
-        def no_changeset(rev):
-            raise NoSuchChangeset(rev)
-            
-        repos = Mock(Repository, 'test-repos', {'name': 'test-repos', 'id': 1},
-                     self.log,
-                     get_changeset=no_changeset,
-                     get_oldest_rev=lambda: 1,
-                     get_youngest_rev=lambda: 0,
-                     normalize_rev=no_changeset,
-                     next_rev=lambda x: None)
+        repos = self.get_repos()
         cache = CachedRepository(self.env, repos, self.log)
         cache.sync()
 
@@ -62,15 +89,10 @@ class CacheTestCase(unittest.TestCase):
     def test_initial_sync(self):
         t1 = datetime(2001, 1, 1, 1, 1, 1, 0, utc)
         t2 = datetime(2002, 1, 1, 1, 1, 1, 0, utc)
+        repos = self.get_repos(get_changeset=lambda x: changesets[int(x)],
+                               youngest_rev=1)
         changes = [('trunk', Node.DIRECTORY, Changeset.ADD, None, None),
                    ('trunk/README', Node.FILE, Changeset.ADD, None, None)]
-        repos = Mock(Repository, 'test-repos', {'name': 'test-repos', 'id': 1},
-                     self.log,
-                     get_changeset=lambda x: changesets[int(x)],
-                     get_oldest_rev=lambda: 0,
-                     get_youngest_rev=lambda: 1,
-                     normalize_rev=lambda x: x,
-                     next_rev=lambda x: int(x) == 0 and 1 or None)
         changesets = [Mock(Changeset, repos, 0, '', '', t1,
                            get_changes=lambda: []),
                       Mock(Changeset, repos, 1, 'Import', 'joe', t2,
@@ -81,10 +103,13 @@ class CacheTestCase(unittest.TestCase):
         cursor = self.db.cursor()
         cursor.execute("SELECT rev,time,author,message FROM revision")
         self.assertEquals(('0', to_timestamp(t1), '', ''), cursor.fetchone())
-        self.assertEquals(('1', to_timestamp(t2), 'joe', 'Import'), cursor.fetchone())
+        self.assertEquals(('1', to_timestamp(t2), 'joe', 'Import'),
+                          cursor.fetchone())
         self.assertEquals(None, cursor.fetchone())
-        cursor.execute("SELECT rev,path,node_type,change_type,base_path,"
-                       "base_rev FROM node_change")
+        cursor.execute("""
+            SELECT rev,path,node_type,change_type,base_path,base_rev
+            FROM node_change
+            """)
         self.assertEquals(('1', 'trunk', 'D', 'A', None, None),
                           cursor.fetchone())
         self.assertEquals(('1', 'trunk/README', 'F', 'A', None, None),
@@ -95,40 +120,36 @@ class CacheTestCase(unittest.TestCase):
         t1 = datetime(2001, 1, 1, 1, 1, 1, 0, utc)
         t2 = datetime(2002, 1, 1, 1, 1, 1, 0, utc)
         t3 = datetime(2003, 1, 1, 1, 1, 1, 0, utc)
-        cursor = self.db.cursor()
-        cursor.execute("INSERT INTO revision (repos,rev,time,author,message) "
-                       "VALUES (1,0,%s,'','')",
-                       (to_timestamp(t1),))
-        cursor.execute("INSERT INTO revision (repos,rev,time,author,message) "
-                       "VALUES (1,1,%s,'joe','Import')",
-                       (to_timestamp(t2),))
-        cursor.executemany("INSERT INTO node_change (repos,rev,path,"
-                           "node_type,change_type,base_path,base_rev) "
-                           "VALUES (1,'1',%s,%s,%s,%s,%s)",
-                           [('trunk', 'D', 'A', None, None),
-                            ('trunk/README', 'F', 'A', None, None)])
-        cursor.execute("UPDATE repository SET value='1' "
-                       "WHERE id=1 AND name='youngest_rev'")
-
-        changes = [('trunk/README', Node.FILE, Changeset.EDIT, 'trunk/README', 1)]
-        repos = Mock(Repository, 'test-repos', {'name': 'test-repos', 'id': 1},
-                     self.log,
-                     get_changeset=lambda x: changeset,
-                     get_youngest_rev=lambda: 2,
-                     get_oldest_rev=lambda: 0,
-                     normalize_rev=lambda x: x,                    
-                     next_rev=lambda x: x and int(x) == 1 and 2 or None)
-        changeset = Mock(Changeset, repos, 2, 'Update', 'joe', t3,
-                         get_changes=lambda: iter(changes))
+        self.preset_cache(
+            ((0, to_timestamp(t1), '', ''), []),
+            ((1, to_timestamp(t2), 'joe', 'Import'),
+             [('trunk', 'D', 'A', None, None),
+              ('trunk/README', 'F', 'A', None, None)]),
+            )
+        repos = self.get_repos(get_changeset=lambda x: changesets[int(x)],
+                               youngest_rev=2)
+        changes = [('trunk/README', Node.FILE, Changeset.EDIT, 'trunk/README',
+                    1)]
+        changesets = [
+            None,
+            Mock(Changeset, repos, 1, '', '', t2, get_changes=lambda: []),
+            Mock(Changeset, repos, 2, 'Update', 'joe', t3,
+                 get_changes=lambda: iter(changes))
+            ]
         cache = CachedRepository(self.env, repos, self.log)
         cache.sync()
 
         cursor = self.db.cursor()
-        cursor.execute("SELECT time,author,message FROM revision WHERE rev='2'")
-        self.assertEquals((to_timestamp(t3), 'joe', 'Update'), cursor.fetchone())
+        cursor.execute("""
+            SELECT time,author,message FROM revision WHERE rev='2'
+            """)
+        self.assertEquals((to_timestamp(t3), 'joe', 'Update'),
+                          cursor.fetchone())
         self.assertEquals(None, cursor.fetchone())
-        cursor.execute("SELECT path,node_type,change_type,base_path,base_rev "
-                       "FROM node_change WHERE rev='2'")
+        cursor.execute("""
+            SELECT path,node_type,change_type,base_path,base_rev
+            FROM node_change WHERE rev='2'
+            """)
         self.assertEquals(('trunk/README', 'F', 'E', 'trunk/README', '1'),
                           cursor.fetchone())
         self.assertEquals(None, cursor.fetchone())
@@ -136,28 +157,13 @@ class CacheTestCase(unittest.TestCase):
     def test_get_changes(self):
         t1 = datetime(2001, 1, 1, 1, 1, 1, 0, utc)
         t2 = datetime(2002, 1, 1, 1, 1, 1, 0, utc)
-        cursor = self.db.cursor()
-        cursor.execute("INSERT INTO revision (repos,rev,time,author,message) "
-                       "VALUES (1,0,%s,'','')",
-                       (to_timestamp(t1),))
-        cursor.execute("INSERT INTO revision (repos,rev,time,author,message) "
-                       "VALUES (1,1,%s,'joe','Import')",
-                       (to_timestamp(t2),))
-        cursor.executemany("INSERT INTO node_change (repos,rev,path,"
-                           "node_type,change_type,base_path,base_rev) "
-                           "VALUES (1,'1',%s,%s,%s,%s,%s)",
-                           [('trunk', 'D', 'A', None, None),
-                            ('trunk/README', 'F', 'A', None, None)])
-        cursor.execute("UPDATE repository SET value='1' "
-                       "WHERE id=1 AND name='youngest_rev'")
-
-        repos = Mock(Repository, 'test-repos', {'name': 'test-repos', 'id': 1},
-                     self.log,
-                     get_changeset=lambda x: None,
-                     get_youngest_rev=lambda: 1,
-                     get_oldest_rev=lambda: 0,
-                     next_rev=lambda x: None,
-                     normalize_rev=lambda rev: rev)
+        self.preset_cache(
+            ((0, to_timestamp(t1), '', ''), []),
+            ((1, to_timestamp(t2), 'joe', 'Import'),
+             [('trunk', 'D', 'A', None, None),
+              ('trunk/RDME', 'F', 'A', None, None)]),
+            )
+        repos = self.get_repos()
         cache = CachedRepository(self.env, repos, self.log)
         self.assertEqual('1', cache.youngest_rev)
         changeset = cache.get_changeset(1)
@@ -167,7 +173,7 @@ class CacheTestCase(unittest.TestCase):
         changes = changeset.get_changes()
         self.assertEqual(('trunk', Node.DIRECTORY, Changeset.ADD, None, None),
                          changes.next())
-        self.assertEqual(('trunk/README', Node.FILE, Changeset.ADD, None, None),
+        self.assertEqual(('trunk/RDME', Node.FILE, Changeset.ADD, None, None),
                          changes.next())
         self.assertRaises(StopIteration, changes.next)
 
