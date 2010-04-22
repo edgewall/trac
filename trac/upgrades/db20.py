1d8076b07ba498714cf4fb23139d86b557031ae4
rblank <rblank@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2010-04-22 20:30:22 +0000

    db: Fixed the upgrade to DB version 20 so that it doesn't use a `Repository` object, but sticks to SQL instead.
    This was preventing upgrading from 0.10.3 with 0.12dev.
    
    Closes #9252.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@9517 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/upgrades/db20.py
+++ trac/upgrades/db20.py
@@ -1,5 +1,3 @@
-from trac.db import Table, Column, Index, DatabaseManager
-from trac.core import TracError
 from trac.versioncontrol.cache import CACHE_YOUNGEST_REV
 
 def do_upgrade(env, ver, cursor):
@@ -8,15 +6,19 @@ def do_upgrade(env, ver, cursor):
     Now we use the 'youngest_rev' entry in the system table
     to explicitly store the youngest rev in the cache.
     """
-    db = env.get_db_cnx()
-    try:
-        repos = env.get_repository()
-        youngest = repos.get_youngest_rev_in_cache(db) or ''
-    except TracError: # no repository available
-        youngest = ''
+    youngest = ''
+    cursor.execute("SELECT value FROM system WHERE name='repository_dir'")
+    for repository_dir, in cursor:
+        if repository_dir.startswith('svn:'):
+            cursor.execute("SELECT rev FROM revision "
+                           "ORDER BY -LENGTH(rev), rev DESC LIMIT 1")
+            row = cursor.fetchone()
+            youngest = row and row[0] or ''
+        else:
+            print 'Please perform a "repository resync" after this upgrade.'
+
     # deleting first, for the 0.11dev and 0.10.4dev users
     cursor.execute("DELETE FROM system WHERE name=%s",
                    (CACHE_YOUNGEST_REV,))
     cursor.execute("INSERT INTO system (name, value) VALUES (%s, %s)",
                    (CACHE_YOUNGEST_REV, youngest))
-        
