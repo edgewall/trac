d82a3a6e9e1fa6ff95dd3914ebab0d0c305a9e31
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2010-05-19 20:47:05 +0000

    0.12dev: allow PySqlite 1.1.7 to be used again, closes #9352.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@9727 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/db/sqlite_backend.py
+++ trac/db/sqlite_backend.py
@@ -153,9 +153,13 @@ class SQLiteConnector(Component):
         if not have_pysqlite:
             self.error = _("Cannot load Python bindings for SQLite")
         elif sqlite_version >= (3, 3, 3):
-            if sqlite.version_info[0] == 2 and \
+            if sqlite.version_info < (1, 1, 7):
+                self.error = _("Need at least PySqlite %(version)s or higher",
+                               version='1.1.7')
+            elif sqlite.version_info[0] == 2 and \
                     sqlite.version_info < (2, 0, 7):
-                self.error = _("Need at least PySqlite 2.0.7 or higher")
+                self.error = _("Need at least PySqlite %(version)s or higher",
+                               version='2.0.7')
         yield ('sqlite', self.error and -1 or 1)
 
     def get_connection(self, path, log=None, params={}):
