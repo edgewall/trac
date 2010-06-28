5c992566b12d5dd283a4e4634b3aa8b6737c3242
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2010-06-28 08:12:42 +0000

    0.12.1dev: ported r9924, fix for deploy
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@9926 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/env.py
+++ trac/env.py
@@ -731,7 +731,7 @@ class EnvironmentAdmin(Component):
             template = Chrome(self.env).load_template('deploy_trac.' + script,
                                                       'text')
             stream = template.generate(**data)
-            out = os.fdopen(os.open(dest, os.O_CREAT | os.O_WRONLY), 'w')
+            out = file(dest, 'w')
             try:
                 stream.render('text', out=out)
             finally:
