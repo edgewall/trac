e498d8b88df149cb9e5f506f4f8a4417d9d8cb9f
rblank <rblank@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2010-08-07 21:24:34 +0000

    0.12.1dev: Fixed the incorrect links generated in the repository index for empty Subversion repositories. Also, fixed the sorting by making the `reponame` always part of the sort key, and added sorting of repositories by author.
    Closes #9112.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.12-stable@9990 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/versioncontrol/web_ui/browser.py
+++ trac/versioncontrol/web_ui/browser.py
@@ -29,7 +29,7 @@ from trac.perm import IPermissionRequestor
 from trac.resource import ResourceNotFound
 from trac.util import embedded_numbers
 from trac.util.compat import any
-from trac.util.datefmt import http_date, utc
+from trac.util.datefmt import http_date, to_datetime, utc
 from trac.util.html import escape, Markup
 from trac.util.text import exception_to_unicode, shorten_line
 from trac.util.translation import _
@@ -493,7 +493,10 @@ class BrowserModule(Component):
                 if repos:
                     if not repos.can_view(context.perm):
                         continue
-                    youngest = repos.get_changeset(repos.youngest_rev)
+                    try:
+                        youngest = repos.get_changeset(repos.youngest_rev)
+                    except NoSuchChangeset:
+                        youngest = None
                     if self.color_scale and youngest:
                         if not timerange:
                             timerange = TimeRange(youngest.date)
@@ -501,7 +504,7 @@ class BrowserModule(Component):
                             timerange.insert(youngest.date)
                     entry = (reponame, repoinfo, repos, youngest, None)
                 else:
-                    entry = (reponame, repoinfo, None, None, "XXX")
+                    entry = (reponame, repoinfo, None, None, u"\u2013")
             except TracError, err:
                 entry = (reponame, repoinfo, None, None,
                          exception_to_unicode(err))
@@ -510,7 +513,12 @@ class BrowserModule(Component):
         # Ordering of repositories
         if order == 'date':
             def repo_order((reponame, repoinfo, repos, youngest, err)):
-                return youngest and youngest.date
+                return (youngest and youngest.date or to_datetime(0),
+                        embedded_numbers(reponame.lower()))
+        elif order == 'author':
+            def repo_order((reponame, repoinfo, repos, youngest, err)):
+                return (youngest and youngest.author.lower() or '',
+                        embedded_numbers(reponame.lower()))
         else:
             def repo_order((reponame, repoinfo, repos, youngest, err)):
                 return embedded_numbers(reponame.lower())
@@ -555,14 +563,16 @@ class BrowserModule(Component):
         # Ordering of entries
         if order == 'date':
             def file_order(a):
-                return changes[a.rev].date
+                return (changes[a.rev].date,
+                        embedded_numbers(a.name.lower()))
         elif order == 'size':
             def file_order(a):
                 return (a.content_length,
                         embedded_numbers(a.name.lower()))
         elif order == 'author':
             def file_order(a):
-                return changes[a.rev].author.lower()
+                return (changes[a.rev].author.lower(),
+                        embedded_numbers(a.name.lower()))
         else:
             def file_order(a):
                 return embedded_numbers(a.name.lower())
