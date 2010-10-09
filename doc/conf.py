3679e2a07e019cc6003b35ebffdd528bbfcba04e
rblank <rblank@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2010-10-09 23:36:26 +0000

    0.12.2dev: Updated version strings and switched back to development mode.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.12-stable@10209 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- doc/conf.py
+++ doc/conf.py
@@ -36,15 +36,15 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'Trac'
-copyright = '2008, Edgewall Software'
+copyright = '2010, Edgewall Software'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.12.1'
+version = '0.12.2'
 # The full version, including alpha/beta/rc tags.
-release = '0.12.1'
+release = '0.12.2'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
