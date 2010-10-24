292ffe1a8ad46b6c197d179e155939eca6f1bf34
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2010-10-24 15:10:25 +0000

    #8695: add support for generating PDF output for the Sphinx API documentation, when [http://code.google.com/p/rst2pdf rst2pdf] is available.
    To get started:
    {{{
    $ easy_install rst2pdf
    $ make apidoc sphinxformat=pdf
    }}}
    
    This will generate a reference manual in `build/doc/pdf/trac_dev.pdf`.
    
    Note that the styling could still benefit from some improvements, only
    a few adaptations were done so far in doc/trac_dev_pdf.style.
    
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@10278 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- doc/conf.py
+++ doc/conf.py
@@ -27,6 +27,13 @@ extensions = [
  'sphinx.ext.autodoc',
 ]
 
+# PDF support via rst2pdf (http://code.google.com/p/rst2pdf/)
+try:
+    import rst2pdf
+    extensions.append('rst2pdf.pdfbuilder')
+except ImportError:
+    pass
+
 autoclass_content = 'both'
 autodoc_member_order = 'bysource'
 
@@ -81,9 +88,10 @@ exclude_patterns = [
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'trac'
 
 
+
 # Options for HTML output
 # -----------------------
 
@@ -146,6 +154,7 @@ html_use_modindex = True
 htmlhelp_basename = 'Tracdoc'
 
 
+
 # Options for LaTeX output
 # ------------------------
 
@@ -178,3 +187,108 @@ latex_documents = [
 
 # If false, no module index is generated.
 #latex_use_modindex = True
+
+
+
+# Options for PDF output 
+# ----------------------
+# (initially copied from
+#  http://rst2pdf.googlecode.com/svn/tags/0.16/doc/manual.txt)
+
+# Grouping the document tree into PDF files. List of tuples
+# (source start file, target name, title, author, options).
+#
+# If there is more than one author, separate them with \\.
+# For example: r'Guido van Rossum\\Fred L. Drake, Jr., editor'
+#
+# The options element is a dictionary that lets you override 
+# this config per-document.
+# For example, 
+# ('index', u'MyProject', u'My Project', u'Author Name', 
+#  dict(pdf_compressed = True))
+# would mean that specific document would be compressed
+# regardless of the global pdf_compressed setting.
+
+pdf_documents = [ 
+    ('index', 'trac_dev', project, u'The Trac Team'),
+]
+
+# A comma-separated list of custom stylesheets (latest has higher precedence)
+pdf_stylesheets = [
+    'sphinx',
+    'a4',
+    'trac',
+    os.path.join(os.path.dirname(__file__), 'trac_dev_pdf.style')
+]
+
+# Create a compressed PDF
+# Use True/False or 1/0
+# Example: compressed=True
+pdf_compressed = True
+
+# A colon-separated list of folders to search for fonts. Example:
+# pdf_font_path = ['/usr/share/fonts', '/usr/share/texmf-dist/fonts/']
+
+# Language to be used for hyphenation support
+pdf_language = "en_US"
+
+# Mode for literal blocks wider than the frame. Can be
+# overflow, shrink or truncate
+pdf_fit_mode = "shrink"
+
+# Section level that forces a break page.
+# For example: 1 means top-level sections start in a new page
+# 0 means disabled
+pdf_break_level = 1
+
+# When a section starts in a new page, force it to be 'even', 'odd',
+# or just use 'any'
+#pdf_breakside = 'any'
+
+# Insert footnotes where they are defined instead of 
+# at the end.
+#pdf_inline_footnotes = True
+
+# verbosity level. 0 1 or 2
+#pdf_verbosity = 0
+
+# If false, no index is generated.
+pdf_use_index = True
+
+# If false, no modindex is generated.
+pdf_use_modindex = True
+
+# If false, no coverpage is generated.
+#pdf_use_coverpage = True
+
+# Name of the cover page template to use
+#pdf_cover_template = 'sphinxcover.tmpl'
+
+# Documents to append as an appendix to all manuals.    
+#pdf_appendices = []
+
+# Enable experimental feature to split table cells. Use it
+# if you get "DelayedTable too big" errors
+#pdf_splittables = False
+
+# Set the default DPI for images
+#pdf_default_dpi = 72
+
+# Enable rst2pdf extension modules (default is only vectorpdf)
+# you need vectorpdf if you want to use sphinx's graphviz support
+#pdf_extensions = ['vectorpdf']
+
+# Page template name for "regular" pages
+#pdf_page_template = 'cutePage'
+
+# Show Table Of Contents at the beginning?
+#pdf_use_toc = True
+
+# How many levels deep should the table of contents be?
+pdf_toc_depth = 9999
+
+# Add section number to section references
+pdf_use_numbered_links = False
+
+# Background images fitting mode
+pdf_fit_background_mode = 'scale'
