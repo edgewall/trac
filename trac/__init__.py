60fe9dd049da8852a253f051948b3ee48ac698cd
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2011-01-23 11:28:36 +0000

    0.12.2rc1: updated ChangeLog and version tag.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.12-stable@10464 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/__init__.py
+++ trac/__init__.py
@@ -16,4 +16,4 @@ from pkg_resources import DistributionNotFound, get_distribution
 try:
     __version__ = get_distribution('Trac').version
 except DistributionNotFound:
-    __version__ = '0.12.2'
+    __version__ = '0.12.2rc1'
