e8e3175b8d3c2f43c8a10d98c057f99fd3c10270
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2011-02-20 18:38:01 +0000

    versioncontrol: make the diff tests a bit stricter, in preparation of diff refactoring
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@10572 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/versioncontrol/tests/diff.py
+++ trac/versioncontrol/tests/diff.py
@@ -21,66 +21,91 @@ class DiffTestCase(unittest.TestCase):
                                      ignore_blank_lines=0)
         self.assertEqual(('equal', 0, 2, 0, 2), opcodes.next())
         self.assertEqual(('insert', 2, 2, 2, 3), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
         opcodes = diff._get_opcodes(['A', 'B'], ['A', 'B', ''],
                                      ignore_blank_lines=1)
         self.assertEqual(('equal', 0, 2, 0, 3), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
+
+        opcodes = diff._get_opcodes(['A'], ['A', 'B', ''],
+                                     ignore_blank_lines=0)
+        self.assertEqual(('equal', 0, 1, 0, 1), opcodes.next())
+        self.assertEqual(('insert', 1, 1, 1, 3), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
         opcodes = diff._get_opcodes(['A'], ['A', 'B', ''],
                                      ignore_blank_lines=1)
         self.assertEqual(('equal', 0, 1, 0, 1), opcodes.next())
         self.assertEqual(('insert', 1, 1, 1, 3), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
     def test_delete_blank_line(self):
         opcodes = diff._get_opcodes(['A', 'B', ''], ['A', 'B'],
                                      ignore_blank_lines=0)
         self.assertEqual(('equal', 0, 2, 0, 2), opcodes.next())
         self.assertEqual(('delete', 2, 3, 2, 2), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
         opcodes = diff._get_opcodes(['A', 'B', ''], ['A', 'B'],
                                      ignore_blank_lines=1)
         self.assertEqual(('equal', 0, 3, 0, 2), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
+
+        opcodes = diff._get_opcodes(['A', 'B', ''], ['A'],
+                                     ignore_blank_lines=0)
+        self.assertEqual(('equal', 0, 1, 0, 1), opcodes.next())
+        self.assertEqual(('delete', 1, 3, 1, 1), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
         opcodes = diff._get_opcodes(['A', 'B', ''], ['A'],
                                      ignore_blank_lines=1)
         self.assertEqual(('equal', 0, 1, 0, 1), opcodes.next())
         self.assertEqual(('delete', 1, 3, 1, 1), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
     def test_space_changes(self):
         opcodes = diff._get_opcodes(['A', 'B b'], ['A', 'B  b'],
                                      ignore_space_changes=0)
         self.assertEqual(('equal', 0, 1, 0, 1), opcodes.next())
         self.assertEqual(('replace', 1, 2, 1, 2), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
         opcodes = diff._get_opcodes(['A', 'B b'], ['A', 'B  b'],
                                      ignore_space_changes=1)
         self.assertEqual(('equal', 0, 2, 0, 2), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
     def test_case_changes(self):
         opcodes = diff._get_opcodes(['A', 'B b'], ['A', 'B B'],
                                      ignore_case=0)
         self.assertEqual(('equal', 0, 1, 0, 1), opcodes.next())
         self.assertEqual(('replace', 1, 2, 1, 2), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
         opcodes = diff._get_opcodes(['A', 'B b'], ['A', 'B B'],
                                      ignore_case=1)
         self.assertEqual(('equal', 0, 2, 0, 2), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
     def test_space_and_case_changes(self):
         opcodes = diff._get_opcodes(['A', 'B b'], ['A', 'B  B'],
                                      ignore_case=0, ignore_space_changes=0)
         self.assertEqual(('equal', 0, 1, 0, 1), opcodes.next())
         self.assertEqual(('replace', 1, 2, 1, 2), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
         opcodes = diff._get_opcodes(['A', 'B b'], ['A', 'B  B'],
                                      ignore_case=1, ignore_space_changes=1)
         self.assertEqual(('equal', 0, 2, 0, 2), opcodes.next())
+        self.assertRaises(StopIteration, opcodes.next)
 
     def test_grouped_opcodes_context1(self):
         opcodes = diff._get_opcodes(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
                                     ['A', 'B', 'C', 'd', 'e', 'f', 'G', 'H'])
         groups = diff._group_opcodes(opcodes, n=1)
         group = groups.next()
+        self.assertRaises(StopIteration, groups.next)
         self.assertEqual(('equal', 2, 3, 2, 3), group[0])
         self.assertEqual(('replace', 3, 6, 3, 6), group[1])
         self.assertEqual(('equal', 6, 7, 6, 7), group[2])
@@ -95,6 +120,7 @@ class DiffTestCase(unittest.TestCase):
         groups = diff._group_opcodes(opcodes, n=3)
         self.assertEqual([('insert', 0, 0, 0, 1), ('equal', 0, 3, 1, 4)],
                          groups.next())
+        self.assertRaises(StopIteration, groups.next)
 
     def test_unified_diff_no_context(self):
         diff_lines = list(diff.unified_diff(['a'], ['b']))
@@ -118,31 +144,38 @@ class DiffTestCase(unittest.TestCase):
         block = changes[0][0]
         self.assertEquals(block['type'], 'mod')
         self.assertEquals(str(block['base']['lines'][0]), '<del></del>*a')
-        self.assertEquals(str(block['changed']['lines'][0]), '<ins>&nbsp;</ins>*a')
+        self.assertEquals(str(block['changed']['lines'][0]),
+                          '<ins>&nbsp;</ins>*a')
 
     def test_whitespace_marked_up2(self):
         """Related to #5795"""
         changes = diff.diff_blocks(['   a'], ['   b'])
         block = changes[0][0]
         self.assertEquals(block['type'], 'mod')
-        self.assertEquals(str(block['base']['lines'][0]), '&nbsp; &nbsp;<del>a</del>')
-        self.assertEquals(str(block['changed']['lines'][0]), '&nbsp; &nbsp;<ins>b</ins>')
+        self.assertEquals(str(block['base']['lines'][0]),
+                          '&nbsp; &nbsp;<del>a</del>')
+        self.assertEquals(str(block['changed']['lines'][0]),
+                          '&nbsp; &nbsp;<ins>b</ins>')
 
     def test_whitespace_marked_up3(self):
         """Related to #5795"""
         changes = diff.diff_blocks(['a   '], ['b   '])
         block = changes[0][0]
         self.assertEquals(block['type'], 'mod')
-        self.assertEquals(str(block['base']['lines'][0]), '<del>a</del>&nbsp; &nbsp;')
-        self.assertEquals(str(block['changed']['lines'][0]), '<ins>b</ins>&nbsp; &nbsp;')
+        self.assertEquals(str(block['base']['lines'][0]),
+                          '<del>a</del>&nbsp; &nbsp;')
+        self.assertEquals(str(block['changed']['lines'][0]),
+                          '<ins>b</ins>&nbsp; &nbsp;')
 
     def test_expandtabs_works_right(self):
         """Regression test for #4557"""
         changes = diff.diff_blocks(['aa\tb'], ['aaxb'])
         block = changes[0][0]
         self.assertEquals(block['type'], 'mod')
-        self.assertEquals(str(block['base']['lines'][0]), 'aa<del>&nbsp; &nbsp; &nbsp; </del>b')
-        self.assertEquals(str(block['changed']['lines'][0]), 'aa<ins>x</ins>b')
+        self.assertEquals(str(block['base']['lines'][0]),
+                          'aa<del>&nbsp; &nbsp; &nbsp; </del>b')
+        self.assertEquals(str(block['changed']['lines'][0]),
+                          'aa<ins>x</ins>b')
 
 def suite():
     return unittest.makeSuite(DiffTestCase, 'test')
