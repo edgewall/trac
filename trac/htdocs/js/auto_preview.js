1d5590276aec6dcc1f135336cd56fa2d000bedcf
rblank <rblank@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2011-07-11 00:05:52 +0000

    0.12.3dev: Trigger a delayed request on `blur` in the automatic preview, instead of a direct request.
    Part of #10207.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.12-stable@10742 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/htdocs/js/auto_preview.js
+++ trac/htdocs/js/auto_preview.js
@@ -54,7 +54,7 @@
         return true;
       }
       
-      $(this).keydown(trigger).keypress(trigger).blur(request);
+      $(this).keydown(trigger).keypress(trigger).blur(trigger);
     });
   }
 })(jQuery);
