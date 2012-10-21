2b92aabc14115ced83375d525d22b35e4611e1c5
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2012-10-21 19:33:03 +0000

    1.1.1dev: upgrade jQuery to [http://blog.jquery.com/2012/09/20/jquery-1-8-2-released/ 1.8.2].
    Only one glitch found (and fixed), the rest seems to work fine, so far.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@11412 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/htdocs/js/blame.js
+++ trac/htdocs/js/blame.js
@@ -89,7 +89,7 @@
               .appendTo("body");
 
             // workaround non-clickable "Close" issue in Firefox
-            if ($.browser.mozilla || $.browser.safari)
+            if ($.browser.mozilla || $.browser.webkit)
               message.find("div.inlinebuttons").next().css("clear", "right");
   
             show();
