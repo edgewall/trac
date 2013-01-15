53745d47d41828a23c38f1458b4085485e4e8dae
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-01-15 22:25:25 +0000

    0.12.5dev: update copyright year to 2013
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.12-stable@11516 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2003-2012 Edgewall Software
+# Copyright (C) 2003-2013 Edgewall Software
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
