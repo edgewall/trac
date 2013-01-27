c7832a9302a9da3648410c1e62e9f4024ceaefdc
psuter <psuter@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-01-27 13:40:12 +0000

    1.1.1dev: Updated documentation of `jquery_*_location` options to refer to the currently used versions. (See #10976 for update to jQuery UI 1.9.2 and jQuery 1.8.3)
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@11576 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/web/chrome.py
+++ trac/web/chrome.py
@@ -384,41 +384,41 @@ class Chrome(Component):
         rules will be needed in the web server.""")
 
     jquery_location = Option('trac', 'jquery_location', '',
-        """Location of the jQuery !JavaScript library (version 1.7.2).
+        """Location of the jQuery !JavaScript library (version 1.8.3).
 
         An empty value loads jQuery from the copy bundled with Trac.
 
         Alternatively, jQuery could be loaded from a CDN, for example:
-        http://code.jquery.com/jquery-1.7.2.min.js,
-        http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.min.js or
-        https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js.
+        http://code.jquery.com/jquery-1.8.3.min.js,
+        http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.3.min.js or
+        https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js.
 
         (''since 1.0'')""")
 
     jquery_ui_location = Option('trac', 'jquery_ui_location', '',
-        """Location of the jQuery UI !JavaScript library (version 1.8.21).
+        """Location of the jQuery UI !JavaScript library (version 1.9.2).
 
         An empty value loads jQuery UI from the copy bundled with Trac.
 
         Alternatively, jQuery UI could be loaded from a CDN, for example:
-        https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js
+        https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js
         or
-        http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.21/jquery-ui.min.js.
+        http://ajax.aspnetcdn.com/ajax/jquery.ui/1.9.2/jquery-ui.min.js.
 
         (''since 1.0'')""")
 
     jquery_ui_theme_location = Option('trac', 'jquery_ui_theme_location', '',
         """Location of the theme to be used with the jQuery UI !JavaScript
-        library (version 1.8.21).
+        library (version 1.9.2).
 
         An empty value loads the custom Trac jQuery UI theme from the copy
         bundled with Trac.
 
         Alternatively, a jQuery UI theme could be loaded from a CDN, for
         example:
-        https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/themes/start/jquery-ui.css
+        https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/start/jquery-ui.css
         or
-        http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.21/themes/start/jquery-ui.css.
+        http://ajax.aspnetcdn.com/ajax/jquery.ui/1.9.2/themes/start/jquery-ui.css.
 
         (''since 1.0'')""")
 
