f127adb00f6b4f71f0688edef711f3a7652be147
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-07-29 20:54:27 +0000

    1.1.2dev: Merge [11899] from 1.0-stable (fix for #11118).
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@11901 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/ticket/tests/functional.py
+++ trac/ticket/tests/functional.py
@@ -1058,6 +1058,20 @@ class TestAdminVersionDefault(FunctionalTwillTestCaseSetup):
                 % (name, name))
 
 
+class TestMilestoneAttachments(FunctionalTwillTestCaseSetup):
+    def runTest(self):
+        """Add attachments to the milestone."""
+        milestone_name = self._tester.create_milestone()
+        filename = self._tester.attach_file_to_milestone(milestone_name)
+
+        self._tester.go_to_milestone(milestone_name)
+        tc.find('Attachments <span class="trac-count">\(1\)</span>')
+        tc.find(filename)
+        tc.find('Download all attachments as:[ \n\t]*<a rel="nofollow" '
+                'href="/zip-attachment/milestone/%s/">.zip</a>'
+                % milestone_name)
+
+
 class TestNewReport(FunctionalTwillTestCaseSetup):
     def runTest(self):
         """Create a new report"""
@@ -1857,6 +1871,7 @@ def functionalSuite(suite=None):
     suite.addTest(TestAdminVersionRemoveMulti())
     suite.addTest(TestAdminVersionNonRemoval())
     suite.addTest(TestAdminVersionDefault())
+    suite.addTest(TestMilestoneAttachments())
     suite.addTest(TestNewReport())
     suite.addTest(TestReportRealmDecoration())
     suite.addTest(TestReportDynamicVariables())
