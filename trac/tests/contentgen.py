3b7b3fa246368aa57e7c2f8e553dc82384c2a60e
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-08-19 21:37:25 +0000

    1.1.2dev: Merged [11966:11967] from 1.0-stable (Refs #11273).
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@11968 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/tests/contentgen.py
+++ trac/tests/contentgen.py
@@ -12,6 +12,7 @@
 # history and logs, available at http://trac.edgewall.org/log/.
 
 import random
+import uuid
 
 try:
     all_words = [x.strip() for x in open('/usr/share/dict/words').readlines()
@@ -31,8 +32,10 @@ except IOError:
     ]
 
 
-def random_word():
+def random_word(min_length=1):
     word = random.choice(all_words)
+    while len(word) < min_length:
+        word = random.choice(all_words)
     # Do not return CamelCase words
     if word[0].isupper():
         word = word.lower().capitalize()
@@ -43,7 +46,7 @@ _random_unique_camels = []
 def random_unique_camel():
     """Returns a unique camelcase word pair"""
     while True:
-        camel = random_word().title() + random_word().title()
+        camel = random_word(2).title() + random_word(2).title()
         if not camel in _random_unique_camels:
             break
     _random_unique_camels.append(camel)
@@ -51,9 +54,15 @@ def random_unique_camel():
 
 
 def random_sentence(word_count=None):
+    """Generates a random sentence. The first word consists of the first 8
+    characters of a uuid to ensure uniqueness.
+
+    :param word_count: number of words in the sentence
+    """
     if word_count is None:
         word_count = random.randint(1, 20)
-    words = [random_word() for x in range(word_count)]
+    words = [random_word() for x in range(word_count - 1)]
+    words.insert(0, str(uuid.uuid1()).split('-')[0])
     return '%s.' % ' '.join(words)
 
 
