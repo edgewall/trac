8510ea11dcd5d15548162cbab351af4342d33f88
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-09-01 18:45:20 +0000

    1.0.2dev: Refactoring - Use `to_unicode(e)` for consistency.
    `to_unicode(e)` calls the `unicode` function on `e` to return the `TracError`'s `message` attribute as a unicode string, and is therefore equivalent to `to_unicode(e.message)`.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/1.0-stable@12008 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/versioncontrol/api.py
+++ trac/versioncontrol/api.py
@@ -360,7 +360,7 @@ class RepositoryManager(Component):
                                 "Look in the Trac log for more information "
                                 "including mitigation strategies.", 
                                 name=reponame or '(default)',
-                                error=to_unicode(e.message)))
+                                error=to_unicode(e)))
                             self.log.error("Failed to sync with repository "
                                 "\"%s\"; You may be able to reduce the impact "
                                 "of this issue by configuring [trac] "
@@ -381,7 +381,7 @@ class RepositoryManager(Component):
                         _("Can't synchronize with repository \"%(name)s\" "
                           "(%(error)s). Look in the Trac log for more "
                           "information.", name=reponame or '(default)',
-                          error=to_unicode(e.message)))
+                          error=to_unicode(e)))
                 self.log.info("Synchronized '%s' repository in %0.2f seconds",
                               reponame or '(default)', time.time() - start)
         return handler
