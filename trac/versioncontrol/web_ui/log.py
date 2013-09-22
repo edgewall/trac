5c89a8779f0051feddcb36709284bae10a3af5e8
jomae <jomae@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-09-22 15:47:17 +0000

    1.0.2dev: merge [12090-12091] from 0.12-stable (fix for #11308)
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/1.0-stable@12092 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/versioncontrol/web_ui/log.py
+++ trac/versioncontrol/web_ui/log.py
@@ -389,9 +389,10 @@ class LogModule(Component):
 
             if repos:
                 revranges = None
-                if any(c for c in ':-,' if c in revs):
+                if any(c in revs for c in ':-,'):
                     revranges = self._normalize_ranges(repos, path, revs)
-                    revs = None
+                    if revranges:
+                        revs = None
                 if 'LOG_VIEW' in formatter.perm:
                     if revranges:
                         href = formatter.href.log(repos.reponame or None,
@@ -404,7 +405,7 @@ class LogModule(Component):
                             rev = None
                         href = formatter.href.log(repos.reponame or None,
                                                   path or '/', rev=rev)
-                    if query and (revranges or revs):
+                    if query and '?' in href:
                         query = '&' + query[1:]
                     return tag.a(label, class_='source',
                                  href=href + query + fragment)
@@ -433,4 +434,7 @@ class LogModule(Component):
                 return None
             seps = splitted_ranges[1::2] + ['']
             ranges = ''.join([str(rev)+sep for rev, sep in zip(revs, seps)])
-            return Ranges(ranges)
+            try:
+                return Ranges(ranges)
+            except ValueError:
+                return None
