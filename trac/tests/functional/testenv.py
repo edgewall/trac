11bd25487692d82d6c62f7b0cf075131091f2891
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-09-26 16:20:56 +0000

    1.1.2dev: Merged [12108] from 1.0-stable. Refs #11244.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@12109 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/tests/functional/testenv.py
+++ trac/tests/functional/testenv.py
@@ -213,6 +213,11 @@ class FunctionalTestEnvironment(object):
         in `trac.ini`."""
         return self._tracadmin('config', 'get', *args)
 
+    def remove_config(self, *args):
+        """Calls trac-admin to remove the value for the given option
+        in `trac.ini`."""
+        return self._tracadmin('config', 'remove', *args)
+
     def _tracadmin(self, *args):
         """Internal utility method for calling trac-admin"""
         proc = Popen([sys.executable, os.path.join(self.trac_src, 'trac',
