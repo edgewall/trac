d1dd81f6af1e01f068b21b9749269e770dfd9abf
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-10-22 05:09:32 +0000

    1.1.2dev: Merged [12198:12199] from 1.0-stable.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@12200 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/tests/functional/tester.py
+++ trac/tests/functional/tester.py
@@ -200,6 +200,15 @@ class FunctionalTester(object):
         tc.go(report_url)
         tc.url(report_url.encode('string-escape').replace('?', '\?'))
 
+    def go_to_preferences(self, panel_label=None):
+        """Surf to the preferences page. Continue surfing to a specific
+        preferences panel if `panel_label` is specified."""
+        self.go_to_front()
+        tc.follow(r"\bPreferences\b")
+        tc.url(self.url + '/prefs')
+        if panel_label is not None:
+            tc.follow(r"\b%s\b" % panel_label)
+
     def add_comment(self, ticketid, comment=None):
         """Adds a comment to the given ticket ID, assumes ticket exists."""
         self.go_to_ticket(ticketid)
