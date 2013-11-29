480aed2803be1f11ccdf3a0a5e7d0df426d66f97
jomae <jomae@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-11-29 08:39:10 +0000

    1.0.2dev: merge [12274] from 0.12-stable (fix for #11345)
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/1.0-stable@12275 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/util/translation.py
+++ trac/util/translation.py
@@ -149,14 +149,15 @@ try:
             if not isinstance(t, Translations):
                 t = self._null_translations
             else:
-                t.add(Translations.load(locale_dir, locale or 'en_US',
-                                        'tracini'))
+                self._add(t, Translations.load(locale_dir, locale or 'en_US',
+                                               'tracini'))
                 if env_path:
                     with self._plugin_domains_lock:
                         domains = self._plugin_domains.get(env_path, {})
                         domains = domains.items()
                     for domain, dirname in domains:
-                        t.add(Translations.load(dirname, locale, domain))
+                        self._add(t, Translations.load(dirname, locale,
+                                                       domain))
             self._current.translations = t
             self._activate_failed = False
 
@@ -183,6 +184,12 @@ try:
             # active, or activation has failed.
             return self._current.translations is not None \
                    or self._activate_failed
+
+        # Internal methods
+
+        def _add(self, t, translations):
+            if isinstance(translations, Translations):
+                t.add(translations)
 
         # Delegated methods
 
