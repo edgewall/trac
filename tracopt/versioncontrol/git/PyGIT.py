11b9b9e7b22792545f2c0ee8ec9272340cf34722
jomae <jomae@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-12-23 09:59:14 +0000

    1.1.2dev: merged [12357-12358] from 1.0-stable (fix for #11164)
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@12359 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- tracopt/versioncontrol/git/PyGIT.py
+++ tracopt/versioncontrol/git/PyGIT.py
@@ -417,24 +417,28 @@ class Storage(object):
     #
 
     # called by Storage.sync()
-    def __rev_cache_sync(self, youngest_rev=None):
+    def __rev_cache_sync(self):
         """invalidates revision db cache if necessary"""
+
+        branches = self._get_branches()
 
         with self.__rev_cache_lock:
             need_update = False
-            if self.__rev_cache:
-                last_youngest_rev = self.__rev_cache.youngest_rev
-                if last_youngest_rev != youngest_rev:
-                    self.logger.debug("invalidated caches (%s != %s)"
-                                      % (last_youngest_rev, youngest_rev))
-                    need_update = True
-            else:
+            if not self.__rev_cache:
                 need_update = True # almost NOOP
+            elif branches != self.__rev_cache.branch_dict:
+                self.logger.debug('invalidated caches for %d cause repository '
+                                  'has been changed', id(self))
+                need_update = True
 
             if need_update:
                 self.__rev_cache = None
-
             return need_update
+
+    def invalidate_rev_cache(self):
+        with self.__rev_cache_lock:
+            self.__rev_cache = None
+            self.logger.debug('invalidated caches for %d', id(self))
 
     def get_rev_cache(self):
         """Retrieve revision cache
@@ -893,9 +897,7 @@ class Storage(object):
         return self.get_commits().iterkeys()
 
     def sync(self):
-        rev = self.repo.rev_list('--max-count=1', '--topo-order', '--all') \
-                       .strip()
-        return self.__rev_cache_sync(rev)
+        return self.__rev_cache_sync()
 
     @contextmanager
     def get_historian(self, sha, base_path):
