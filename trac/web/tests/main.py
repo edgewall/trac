4c4b6fe981588e57652f7b173f6933ec5d926933
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2013-12-24 06:06:49 +0000

    1.1.2dev: Merged [12366:12368] from 1.0-stable. Refs #11189.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@12369 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/web/tests/main.py
+++ trac/web/tests/main.py
@@ -11,12 +11,83 @@
 # individuals. For the exact contribution history, see the revision
 # history and logs, available at http://trac.edgewall.org/log/.
 
-from trac.util import create_file
-from trac.web.main import get_environments
-
+import os.path
 import tempfile
 import unittest
-import os.path
+
+from trac.core import Component, ComponentMeta, TracError, implements
+from trac.test import EnvironmentStub, Mock
+from trac.util import create_file
+from trac.web.auth import IAuthenticator
+from trac.web.main import RequestDispatcher, get_environments
+
+
+class AuthenticateTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.env = EnvironmentStub(disable=['trac.web.auth.LoginModule'])
+        self.request_dispatcher = RequestDispatcher(self.env)
+        self.req = Mock(chrome={'warnings': []})
+        # Make sure we have no external components hanging around in the
+        # component registry
+        self.old_registry = ComponentMeta._registry
+        ComponentMeta._registry = {}
+
+    def tearDown(self):
+        # Restore the original component registry
+        ComponentMeta._registry = self.old_registry
+
+    def test_authenticate_returns_first_successful(self):
+        class SuccessfulAuthenticator1(Component):
+            implements(IAuthenticator)
+            def authenticate(self, req):
+                return 'user1'
+        class SuccessfulAuthenticator2(Component):
+            implements(IAuthenticator)
+            def authenticate(self, req):
+                return 'user2'
+        self.assertEqual(2, len(self.request_dispatcher.authenticators))
+        self.assertIsInstance(self.request_dispatcher.authenticators[0],
+                              SuccessfulAuthenticator1)
+        self.assertIsInstance(self.request_dispatcher.authenticators[1],
+                              SuccessfulAuthenticator2)
+        self.assertEqual('user1',
+                         self.request_dispatcher.authenticate(self.req))
+
+    def test_authenticate_skips_unsuccessful(self):
+        class UnsuccessfulAuthenticator(Component):
+            implements(IAuthenticator)
+            def authenticate(self, req):
+                return None
+        class SuccessfulAuthenticator(Component):
+            implements(IAuthenticator)
+            def authenticate(self, req):
+                return 'user'
+        self.assertEqual(2, len(self.request_dispatcher.authenticators))
+        self.assertIsInstance(self.request_dispatcher.authenticators[0],
+                              UnsuccessfulAuthenticator)
+        self.assertIsInstance(self.request_dispatcher.authenticators[1],
+                              SuccessfulAuthenticator)
+        self.assertEqual('user',
+                         self.request_dispatcher.authenticate(self.req))
+
+    def test_authenticate_raises(self):
+        class RaisingAuthenticator(Component):
+            implements(IAuthenticator)
+            def authenticate(self, req):
+                raise TracError("Bad attempt")
+        class SuccessfulAuthenticator(Component):
+            implements(IAuthenticator)
+            def authenticate(self, req):
+                return 'user'
+        self.assertEqual(2, len(self.request_dispatcher.authenticators))
+        self.assertIsInstance(self.request_dispatcher.authenticators[0],
+                              RaisingAuthenticator)
+        self.assertIsInstance(self.request_dispatcher.authenticators[1],
+                              SuccessfulAuthenticator)
+        self.assertEqual('anonymous',
+                         self.request_dispatcher.authenticate(self.req))
+        self.assertEqual(1, len(self.req.chrome['warnings']))
 
 
 class EnvironmentsTestCase(unittest.TestCase):
@@ -76,6 +147,7 @@ class EnvironmentsTestCase(unittest.TestCase):
 
 def suite():
     suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(AuthenticateTestCase, 'test'))
     suite.addTest(unittest.makeSuite(EnvironmentsTestCase, 'test'))
     return suite
 
