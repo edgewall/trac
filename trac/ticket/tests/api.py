283296e5de683107dc75bdcf53036133821b0f08
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2014-02-13 21:06:03 +0000

    1.1.2dev: Fixed failing unit test, introduced in [12512]. Refs #11471.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@12517 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/ticket/tests/api.py
+++ trac/ticket/tests/api.py
@@ -74,13 +74,12 @@ class TicketSystemTestCase(unittest.TestCase):
         self.env.config.set('ticket-custom', 'test', 'textarea')
         self.env.config.set('ticket-custom', 'test.label', 'Test')
         self.env.config.set('ticket-custom', 'test.value', 'Foo bar')
-        self.env.config.set('ticket-custom', 'test.cols', '60')
         self.env.config.set('ticket-custom', 'test.rows', '4')
         self.env.config.set('ticket-custom', 'test.format', 'wiki')
         fields = TicketSystem(self.env).get_custom_fields()
         self.assertEqual({'name': 'test', 'type': 'textarea', 'label': 'Test',
-                          'value': 'Foo bar', 'width': 60, 'height': 4,
-                          'order': 0, 'format': 'wiki', 'custom': True},
+                          'value': 'Foo bar', 'height': 4, 'order': 0,
+                          'format': 'wiki', 'custom': True},
                          fields[0])
 
     def test_custom_field_time(self):
