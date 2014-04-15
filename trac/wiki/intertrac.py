ce09ec61ed96c6e9d17e1f18807515bef2971282
jomae <jomae@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2014-04-15 15:59:11 +0000

    0.12.6dev: backported [12048] from 1.0-stable (fix for #11283)
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/0.12-stable@12680 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/wiki/intertrac.py
+++ trac/wiki/intertrac.py
@@ -20,7 +20,6 @@ from genshi.builder import Element, Fragment, tag
 
 from trac.core import *
 from trac.mimeview import Context
-from trac.perm import PermissionError
 from trac.util.html import find_element
 from trac.util.translation import _
 from trac.web import IRequestHandler
@@ -52,8 +51,10 @@ class InterTracDispatcher(Component):
         link_frag = extract_link(self.env, Context.from_request(req), link)
         if isinstance(link_frag, (Element, Fragment)):
             elt = find_element(link_frag, 'href')
-            if elt is None: # most probably no permissions to view
-                raise PermissionError(_("Can't view %(link)s:", link=link))
+            if elt is None:
+                raise TracError(
+                    _("Can't view %(link)s. Resource doesn't exist or "
+                      "you don't have the required permission.", link=link))
             href = elt.attrib.get('href')
         else:
             href = req.href(link.rstrip(':'))
