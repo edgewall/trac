9bf300e27f17a3adadc74cb934f71b363932a285
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2014-08-03 06:07:41 +0000

    1.1.2dev: Merged [13060] from 1.0-stable.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@13060 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/ticket/model.py
+++ trac/ticket/model.py
@@ -1013,11 +1013,10 @@ class Milestone(object):
     def delete(self, retarget_to=None, author=None):
         """Delete the milestone.
 
-        :param author: the author of the change
-
-        :since 1.0.2: the `retarget_to` parameter is deprecated and tickets
-        should moved to another milestone by calling `move_tickets` before
-        `delete`.
+        :since 1.0.2: the `retarget_to` and `author` parameters are
+                      deprecated and will be removed in Trac 1.3.1. Tickets
+                      should be moved to another milestone by calling
+                      `move_tickets` before `delete`.
         """
         with self.env.db_transaction as db:
             self.env.log.info("Deleting milestone %s", self.name)
