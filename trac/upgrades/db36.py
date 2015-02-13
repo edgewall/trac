c31df50886ae2b5a2febbac875eb8032121c223c
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2015-02-13 07:48:35 +0000

    1.1.4dev: Missing file from [13741]. Refs #11529.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@13743 af82e41b-90c4-0310-8c96-b1721e28e2e2

---
+++ trac/upgrades/db36.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Edgewall Software
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://trac.edgewall.org/wiki/TracLicense.
+#
+# This software consists of voluntary contributions made by many
+# individuals. For the exact contribution history, see the revision
+# history and logs, available at http://trac.edgewall.org/log/.
+
+import os
+
+
+def do_upgrade(env, version, cursor):
+    authz_file = env.config.get('authz_policy', 'authz_file')
+    if authz_file and not os.path.isabs(authz_file):
+        parts = os.path.split(authz_file)
+        if len(parts) == 2 and parts[0] == 'conf':
+            env.config.set('authz_policy', 'authz_file', parts[1])
+            env.config.save()
