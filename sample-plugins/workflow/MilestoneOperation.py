806fcd2f98eaf46713a0e83fe26410574d2b5a01
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2015-02-14 05:32:37 +0000

    1.1.4dev: PEP-0008 changes to `MilestoneOperation`.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@13745 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- sample-plugins/workflow/MilestoneOperation.py
+++ sample-plugins/workflow/MilestoneOperation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2002-2013 Edgewall Software
+# Copyright (C) 2012-2015 Edgewall Software
 # Copyright (C) 2012 Franz Mayer <franz.mayer@gefasoft.de>
 # All rights reserved.
 #
@@ -11,8 +11,6 @@
 # This software consists of voluntary contributions made by many
 # individuals. For the exact contribution history, see the revision
 # history and logs, available at http://trac.edgewall.org/.
-
-from genshi.builder import tag
 
 from trac.core import Component, implements
 from trac.resource import ResourceNotFound
@@ -25,38 +23,39 @@ from trac.web.chrome import add_warning
 revision = "$Rev$"
 url = "$URL$"
 
+
 class MilestoneOperation(Component):
     """Sets milestone for specific status.
 
-=== Example ===
-{{{
-[ticket-workflow]
-resolve.operations = set_resolution,set_milestone
-resolve.milestone = invalid,wontfix,duplicate,worksforme->rejected
-}}}
+    === Example ===
+    {{{
+    [ticket-workflow]
+    resolve.operations = set_resolution,set_milestone
+    resolve.milestone = invalid,wontfix,duplicate,worksforme -> rejected
+    }}}
 
-When setting status to `duplicate` the milestone will automatically change
-to `rejected`.
+    When setting status to `duplicate` the milestone will automatically change
+    to `rejected`.
 
-'''Note:''' if user has changed milestone manually, this workflow operation
-has ''no effect''!
+    '''Note:''' if user has changed milestone manually, this workflow operation
+    has ''no effect''!
 
-=== Configuration ===
-Don't forget to add `MilestoneOperation` to the workflow option
-in `[ticket]` section. If there is no workflow option, the line will look
-like this:
-{{{
-[ticket]
-workflow = ConfigurableTicketWorkflow,MilestoneOperation
-}}}
-"""
+    === Configuration ===
+    Don't forget to add `MilestoneOperation` to the workflow option
+    in `[ticket]` section. If there is no workflow option, the line will look
+    like this:
+    {{{
+    [ticket]
+    workflow = ConfigurableTicketWorkflow,MilestoneOperation
+    }}}
+    """
 
     implements(ITicketActionController)
 
     def get_ticket_actions(self, req, ticket):
         actions_we_handle = []
         if req.authname != 'anonymous' and \
-                    'TICKET_MODIFY' in req.perm(ticket.resource):
+                'TICKET_MODIFY' in req.perm(ticket.resource):
             controller = ConfigurableTicketWorkflow(self.env)
             actions_we_handle = controller.get_actions_by_operation_for_req(
                 req, ticket, 'set_milestone')
@@ -81,7 +80,7 @@ workflow = ConfigurableTicketWorkflow,MilestoneOperation
         hint = _("For resolution %(resolutions)s the milestone will be "
                  "set to '%(milestone)s'.",
                  resolutions=resolutions, milestone=milestone)
-        return (label, None, hint)
+        return label, None, hint
 
     def get_ticket_changes(self, req, ticket, action):
         if action == 'resolve' and \
@@ -97,13 +96,14 @@ workflow = ConfigurableTicketWorkflow,MilestoneOperation
                 # ... but we don't reset it to None unless it was None
                 if new_milestone is not None or user_milestone is None:
                     try:
-                        milestone = Milestone(self.env, new_milestone)
+                        Milestone(self.env, new_milestone)
+                    except ResourceNotFound:
+                        add_warning(req, _("Milestone %(name)s does "
+                                           "not exist.", name=new_milestone))
+                    else:
                         self.log.info('changed milestone from %s to %s',
                                       old_milestone, new_milestone)
                         return {'milestone': new_milestone}
-                    except ResourceNotFound:
-                        add_warning(req, _("Milestone %(name)s does not exist.",
-                                           name=new_milestone))
         return {}
 
     def apply_action_side_effects(self, req, ticket, action):
