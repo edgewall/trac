582763f2755c75f961d8e7d0e08c7f98c053f76b
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2015-03-04 05:01:09 +0000

    1.1.4dev: Removed bad markup from [13741]. Refs #11529.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@13833 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/versioncontrol/svn_authz.py
+++ trac/versioncontrol/svn_authz.py
@@ -126,7 +126,7 @@ class AuthzSourcePolicy(Component):
         [http://svnbook.red-bean.com/en/1.5/svn.serverconfig.pathbasedauthz.html authorization (authz) file].
         To enable authz permission checking, the `AuthzSourcePolicy` permission
         policy must be added to `[trac] permission_policies`. Non-absolute
-        paths are relative to the Environment `conf` directory.")
+        paths are relative to the Environment `conf` directory.
         """)
 
     authz_module_name = Option('trac', 'authz_module_name', '',
