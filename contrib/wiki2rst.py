596e597628611133c1e9848c9b21f2083a716806
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2015-03-19 06:46:04 +0000

    1.1.4dev: Merged [13894] from 1.0-stable. Refs #11914.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@13895 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- contrib/wiki2rst.py
+++ contrib/wiki2rst.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import codecs
 import os.path
+import re
 import sys
 
 from cStringIO import StringIO
-from html2rest import html2rest
 from pkg_resources import resource_listdir, resource_string
+import html2rest
 
 from trac.loader import load_components
 from trac.test import EnvironmentStub, Mock, MockPerm
@@ -15,6 +17,65 @@ from trac.web.chrome import web_context
 from trac.web.href import Href
 from trac.wiki.formatter import format_to_html
 from trac.wiki.model import WikiPage
+
+
+class Parser(html2rest.Parser):
+
+    def __init__(self, writer=sys.stdout, encoding='utf8', relroot=None,
+                 relpath=None):
+        html2rest.Parser.__init__(self, writer, encoding, relroot, relpath)
+        self.links = {}
+
+    def end_a(self):
+        if '#pending' in self.hrefs:
+            href = self.hrefs['#pending']
+            label = self.hrefs[href]
+            key = label.lower()
+            if key not in self.links:
+                self.links[key] = (label, href)
+            elif href != self.links[key][1]:
+                alt = label
+                while True:
+                    alt += '*'
+                    if alt not in self.links:
+                        break
+                    continue
+                self.data(alt[len(label):])
+                self.hrefs[href] = alt
+                self.links[alt] = (alt, href)
+            self.data('`_')
+            del self.hrefs['#pending']
+
+    def end_body(self):
+        self.end_p()
+        for label, href in self.links.itervalues():
+            if href[0] != '#':
+                self.writeline('.. _%s: %s' % (label, href))
+        self.end_p()
+
+
+def wiki2rest(env, context, wiki):
+    text = re.sub('\r?\n', '\n', wiki.text)
+    text = re.sub(r'\[\[TracGuideToc\]\]\r?\n?', '', text)
+    html = format_to_html(env, context, text)
+    html = html.replace(u'<span class="icon">\u200b</span>', '')
+    html = re.sub(r'<em>\s*([^<]*?)\s*</em>', r'<em>\1</em>', html)
+    html = '<html><body>%s</body></html>' % html
+    writer = StringIO()
+    parser = Parser(writer, 'utf-8', None, None)
+    parser.feed(html)
+    parser.close()
+    rst = writer.getvalue().strip('\n')
+    rst = re.sub('\n{4,}', '\n\n\n', rst)
+    # sort links
+    rst = re.sub(r'(?:\n\.\. _[^\n]*)+\Z',
+                 lambda m: '\n'.join(sorted(m.group(0).split('\n'),
+                                            key=lambda v: v.lower())),
+                 rst)
+    if any(ord(c) > 0x7f for c in rst):
+        # Trac detects utf-8 using BOM
+        rst = '%s.. charset=utf-8\n\n%s' % (codecs.BOM_UTF8, rst)
+    return rst + '\n'
 
 
 def main():
@@ -42,13 +103,8 @@ def main():
         if not wiki.exists:
             continue
         context = web_context(req, wiki.resource, absurls=True)
-        html = '<html><body>%s</body></html>' % \
-               format_to_html(env, context, wiki.text).encode('utf-8')
-        out = StringIO()
-        html2rest(html, writer=out)
-        out = out.getvalue().replace(': http://trac.edgewall.org/wiki/',
-                                     ': trac/wiki/default-pages/')
-        sys.stdout.write(out)
+        rst = wiki2rest(env, context, wiki)
+        sys.stdout.write(rst)
 
 
 if __name__ == '__main__':
