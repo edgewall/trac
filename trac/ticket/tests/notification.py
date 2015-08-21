3914e5bdc23eb4655f373a02e023638ee51ee08e
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2015-08-21 02:00:46 +0000

    1.2dev: Merged [14265] from 1.0-stable.
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@14266 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/ticket/tests/notification.py
+++ trac/ticket/tests/notification.py
@@ -29,7 +29,7 @@ import trac.tests.compat
 from trac.attachment import Attachment
 from trac.notification.api import NotificationSystem
 from trac.test import EnvironmentStub, Mock, MockPerm
-from trac.tests.notification import SMTP_TEST_PORT, SMTPThreadedServer,\
+from trac.tests.notification import SMTP_TEST_PORT, SMTPThreadedServer, \
                                     parse_smtp_message
 from trac.ticket.model import Ticket
 from trac.ticket.notification import TicketChangeEvent, TicketNotifyEmail
