8b5ed5d4b583d57b1bb5980a0652582c72da36e4
jomae <jomae@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2016-03-30 17:03:38 +0000

    1.2dev: merge [14653] from 1.0-stable (fix for #12420)
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@14654 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/db/sqlite_backend.py
+++ trac/db/sqlite_backend.py
@@ -209,17 +209,21 @@ class SQLiteConnector(Component):
             if isinstance(path, unicode):  # needed with 2.4.0
                 path = path.encode('utf-8')
             # this direct connect will create the database if needed
-            cnx = sqlite.connect(path,
+            cnx = sqlite.connect(path, isolation_level=None,
                                  timeout=int(params.get('timeout', 10000)))
+            cursor = cnx.cursor()
+            _set_journal_mode(cursor, params.get('journal_mode'))
+            _set_synchronous(cursor, params.get('synchronous'))
+            cnx.isolation_level = 'DEFERRED'
         else:
             cnx = self.get_connection(path, log, params)
-        cursor = cnx.cursor()
-        _set_journal_mode(cursor, params.get('journal_mode'))
+            cursor = cnx.cursor()
         if schema is None:
             from trac.db_default import schema
         for table in schema:
             for stmt in self.to_sql(table):
                 cursor.execute(stmt)
+        cursor.close()
         cnx.commit()
 
     def destroy_db(self, path, log=None, params={}):
@@ -305,6 +309,7 @@ class SQLiteConnection(ConnectionBase, ConnectionWrapper):
         if isinstance(path, unicode):  # needed with 2.4.0
             path = path.encode('utf-8')
         cnx = sqlite.connect(path, detect_types=sqlite.PARSE_DECLTYPES,
+                             isolation_level=None,
                              check_same_thread=sqlite_version < (3, 3, 1),
                              timeout=timeout)
         # load extensions
@@ -318,6 +323,8 @@ class SQLiteConnection(ConnectionBase, ConnectionWrapper):
         cursor = cnx.cursor()
         _set_journal_mode(cursor, params.get('journal_mode'))
         _set_synchronous(cursor, params.get('synchronous'))
+        cursor.close()
+        cnx.isolation_level = 'DEFERRED'
         ConnectionWrapper.__init__(self, cnx, log)
 
     def cursor(self):
