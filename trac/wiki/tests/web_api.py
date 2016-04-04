73991ac9eb8473a9c76215e03f40d2b42ddf2f09
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2016-04-04 00:26:37 +0000

    1.2dev: Use `MockRequest` rather than `Mock`
    Refs #12211.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@14666 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/wiki/tests/web_api.py
+++ trac/wiki/tests/web_api.py
@@ -12,13 +12,11 @@
 # history and logs, available at http://trac.edgewall.org/log/.
 
 import unittest
-from cStringIO import StringIO
 
 import trac.tests.compat
 from trac.mimeview.patch import PatchRenderer
-from trac.test import EnvironmentStub, Mock
+from trac.test import EnvironmentStub, MockRequest
 from trac.web.api import RequestDone
-from trac.web.href import Href
 from trac.wiki.web_api import WikiRenderer
 
 
@@ -29,11 +27,6 @@ class WikiRendererTestCase(unittest.TestCase):
         self.mod = WikiRenderer(self.env)
 
     def test_load_stylesheet(self):
-        buf = StringIO()
-        def send(data):
-            buf.write(data)
-            raise RequestDone
-
         text = """\
 {{{#!text/x-diff
 --- a/file.txt  2014-11-13 01:16:06 +0000
@@ -43,20 +36,19 @@ class WikiRendererTestCase(unittest.TestCase):
 +new line
 }}}
 """
-        req = Mock(method='POST', path_info='/wiki_render', session={},
-                   args={'id': 'WikiStart', 'text': text},
-                   abs_href=Href('http://example.com/'), href=Href('/'),
-                   chrome={'links': {}, 'scripts': []}, perm=None,
-                   authname=None, tz=None, send=send)
+        req = MockRequest(self.env, method='POST', path_info='/wiki_render',
+                          args={'id': 'WikiStart', 'text': text})
+
         self.assertTrue(self.mod.match_request(req))
         try:
             self.mod.process_request(req)
             self.fail('RequestDone not raised')
         except RequestDone:
-            output = buf.getvalue()
-            self.assertIn('<div class="code"><pre>', output)
+            output = req.response_sent.getvalue()
+            self.assertIn('<div class="wiki-code">', output)
+            self.assertIn('<table class="trac-diff inline"', output)
             self.assertIn('jQuery.loadStyleSheet("'
-                          '/chrome/common/css/diff.css"', output)
+                          '/trac.cgi/chrome/common/css/diff.css"', output)
 
 
 def suite():
