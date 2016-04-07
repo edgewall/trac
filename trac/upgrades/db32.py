30ef9ecf01944655acb7380062a90c6b2f22ae6d
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2016-04-07 23:27:44 +0000

    1.2dev: Always remove [trac] repository_sync_per_request
    Refs #11776.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@14692 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/upgrades/db32.py
+++ trac/upgrades/db32.py
@@ -21,29 +21,28 @@ def do_upgrade(env, version, cursor):
     """
     repos_sync_per_request = \
         env.config.getlist('trac', 'repository_sync_per_request')
-    if not repos_sync_per_request:
-        return
-
     backup_config_file(env, '.db32.bak')
-    rm = RepositoryManager(env)
-    db_provider = env[DbRepositoryProvider]
+    if repos_sync_per_request:
+        rm = RepositoryManager(env)
+        db_provider = env[DbRepositoryProvider]
 
-    trac_ini_repo_names = []
-    for name, _ in rm.get_repositories():
-        trac_ini_repo_names.append(name)
-    for repos in rm.get_all_repositories().values():
-        sync_per_request = (repos['name'] or '(default)') \
-                           in repos_sync_per_request
-        if sync_per_request:
-            if repos['name'] in trac_ini_repo_names:
-                env.config.set('repositories',
-                               repos['name'] + '.sync_per_request',
-                               'true')
-            else:
-                changes = {'sync_per_request': sync_per_request}
-                db_provider.modify_repository(repos['name'], changes)
-    env.log.info("Removed [trac] repository_sync_per_request option and "
-                 "enabled sync_per_request for the following "
-                 "repositories: %s", ', '.join(repos_sync_per_request))
+        trac_ini_repo_names = []
+        for name, _ in rm.get_repositories():
+            trac_ini_repo_names.append(name)
+        for repos in rm.get_all_repositories().values():
+            sync_per_request = (repos['name'] or '(default)') \
+                               in repos_sync_per_request
+            if sync_per_request:
+                if repos['name'] in trac_ini_repo_names:
+                    env.config.set('repositories',
+                                   repos['name'] + '.sync_per_request',
+                                   'true')
+                else:
+                    changes = {'sync_per_request': sync_per_request}
+                    db_provider.modify_repository(repos['name'], changes)
+        env.log.info("Enabled sync_per_request for the following "
+                     "repositories: %s", ', '.join(repos_sync_per_request))
+
     env.config.remove('trac', 'repository_sync_per_request')
     env.config.save()
+    env.log.info("Removed [trac] repository_sync_per_request option")
