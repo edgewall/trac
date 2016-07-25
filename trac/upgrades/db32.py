920f8ace1f1a49d67d706681db8ddbebb7f2725f
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2016-07-25 21:02:17 +0000

    1.3.1dev: Merge r14998 from 1.2-stable
    Refs #12480.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@14999 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/upgrades/db32.py
+++ trac/upgrades/db32.py
@@ -19,11 +19,13 @@ from tracopt.versioncontrol.git.git_fs import GitwebProjectsRepositoryProvider
 def do_upgrade(env, version, cursor):
     """Replace list of repositories in [trac] repository_sync_per_request
     with boolean values [repositories] <repos>.sync_per_request and a list
-    of repositories in [gitweb-repositories] sync_per_request.
+    of repositories in [gitweb-repositories] sync_per_request. Move and
+    rename the Gitweb configuration options from the [git] section to
+    the [gitweb-repositories] section.
     """
     backup_config_file(env, '.db32.bak')
     repos_sync_per_request = \
-        env.config.getlist('trac', 'repository_sync_per_request')
+        env.config.getlist('trac', 'repository_sync_per_request', '(default)')
 
     for suffix in ('base', 'list', 'url'):
         option_name = 'projects_' + suffix
@@ -34,33 +36,29 @@ def do_upgrade(env, version, cursor):
                      option_name, option_name)
 
     rm = RepositoryManager(env)
-    gitweb_repository_sync_per_request = []
     if repos_sync_per_request:
-        gwrp = env[GitwebProjectsRepositoryProvider]
-        db_provider = env[DbRepositoryProvider]
-
-        trac_ini_repo_names = []
-        gitweb_repo_names = []
         for name, _ in rm.get_repositories():
-            trac_ini_repo_names.append(name)
-        for name, _ in gwrp.get_repositories():
-            gitweb_repo_names.append(name)
-        for repos in rm.get_all_repositories().values():
-            sync_per_request = (repos['name'] or '(default)') \
-                               in repos_sync_per_request
+            sync_per_request = (name or '(default)') in repos_sync_per_request
             if sync_per_request:
-                if repos['name'] in trac_ini_repo_names:
-                    env.config.set('repositories',
-                                   repos['name'] + '.sync_per_request',
-                                   'true')
-                elif repos['name'] in gitweb_repo_names:
-                    gitweb_repository_sync_per_request.append(repos['name'])
-                else:
-                    changes = {'sync_per_request': sync_per_request}
-                    db_provider.modify_repository(repos['name'], changes)
-                env.log.info("Enabled sync_per_request for %s", repos['name'])
-    env.config.set('gitweb-repositories', 'sync_per_request',
-                   ', '.join(gitweb_repository_sync_per_request))
+                env.config.set('repositories', name + '.sync_per_request',
+                               'true')
+                env.log.info("Enabled sync_per_request for %s", name)
+
+        gwrp = GitwebProjectsRepositoryProvider(env)
+        gitweb_repo_names = [name for name, _ in gwrp.get_repositories()]
+        sync_per_request = \
+            ', '.join(set(gitweb_repo_names) & set(repos_sync_per_request))
+        env.config.set('gitweb-repositories', 'sync_per_request',
+                       sync_per_request)
+        env.log.info("Enabled sync_per_request for %s", sync_per_request)
+
+        db_provider = DbRepositoryProvider(env)
+        for name, _ in db_provider.get_repositories():
+            sync_per_request = (name or '(default)') in repos_sync_per_request
+            changes = {'sync_per_request': sync_per_request}
+            db_provider.modify_repository(name, changes)
+            if sync_per_request:
+                env.log.info("Enabled sync_per_request for %s", name)
 
     env.config.remove('trac', 'repository_sync_per_request')
     env.log.info("Removed [trac] repository_sync_per_request option")
--- trac/upgrades/tests/db32.py
+++ trac/upgrades/tests/db32.py
@@ -12,32 +12,25 @@
 # history and logs, available at http://trac.edgewall.org/.
 
 import os
-import shutil
 import tempfile
 import unittest
 
 from trac.test import EnvironmentStub
 from trac.upgrades import db32
-from trac.versioncontrol.api import RepositoryManager
+from trac.versioncontrol.api import DbRepositoryProvider, RepositoryManager
 from tracopt.versioncontrol.git.git_fs import GitwebProjectsRepositoryProvider
+from tracopt.versioncontrol.svn.svn_fs import SubversionConnector
 
 VERSION = 32
 
 
 class UpgradeTestCase(unittest.TestCase):
-    """Selectively move/rename the option:
-    [trac] repository_sync_per_request ->
-        [repositories] <name>.sync_per_request
-    or if the repository is listed in [git] projects_list:
-    [trac] repository_sync_per_request ->
-        [gitweb-repositories] sync_per_request
-    """
 
     def setUp(self):
         self.env = EnvironmentStub(path=os.path.realpath(tempfile.mkdtemp()))
         self.env.config.filename = os.path.join(self.env.path, 'trac.ini')
         self.env.config.set('trac', 'repository_sync_per_request',
-                            'repos2, repos3')
+                            'repos1, repos3, repos5')
 
     def tearDown(self):
         self.env.reset_db_and_disk()
@@ -49,39 +42,139 @@ class UpgradeTestCase(unittest.TestCase):
 
         self.assertTrue(os.path.exists(config.filename + '.db32.bak'))
 
-    def test_gitweb_values_moved(self):
-        """Gitweb configuration is rewritten to the [gitweb-repositories]
-        section.
+    def test_repository_sync_per_request_default_value(self):
+        """The default repository sync_per_request attribute is set to true
+        when repository_sync_per_request is not set in trac.ini.
         """
-        self.projects_list = os.path.join(self.env.path, 'projects_list')
-        self.projects_base = os.path.dirname(self.projects_list)
-        self.projects_url = 'http://localhost/%s'
-        with open(self.projects_list, 'w') as f:
+        self.env.config.remove('trac', 'repository_sync_per_request')
+        repositories = self.env.config['repositories']
+        repositories.set('.dir', '/var/svn')
+        repositories.set('.type', 'svn')
+        repositories.set('git.dir', '/var/git')
+        repositories.set('git.type', 'git')
+
+        db32.do_upgrade(self.env, VERSION, None)
+
+        repositories = self.env.config['repositories']
+        self.assertIn('.sync_per_request', repositories)
+        self.assertTrue(repositories.getbool('.sync_per_request'))
+        self.assertNotIn('git.sync_per_request', repositories)
+        self.assertFalse(repositories.getbool('git.sync_per_request'))
+
+    def test_repository_sync_per_request_default_value_with_db(self):
+        """The default repository sync_per_request attribute is set to
+        true when repository_sync_per_request is not set in trac.ini.
+        """
+        self.env.config.remove('trac', 'repository_sync_per_request')
+        db_provider = DbRepositoryProvider(self.env)
+        db_provider.add_repository('', '/var/svn', 'svn')
+        db_provider.add_repository('git', '/var/git', 'git')
+
+        db32.do_upgrade(self.env, VERSION, None)
+
+        repos = RepositoryManager(self.env).get_all_repositories()
+        self.assertIn('', repos)
+        self.assertTrue(repos['']['sync_per_request'])
+        self.assertEqual('1', self.env.db_query("""
+            SELECT value FROM repository
+            WHERE id=1 AND name='sync_per_request'""")[0][0])
+        self.assertIn('git', repos)
+        self.assertFalse(repos['git']['sync_per_request'])
+        self.assertEqual('0', self.env.db_query("""
+            SELECT value FROM repository
+            WHERE id=2 AND name='sync_per_request'""")[0][0])
+
+    def test_gitweb_configuration_moved(self):
+        """The Gitweb configuration is moved from the [git] section to
+        the [gitweb-repositories] section.
+        """
+        projects_list = os.path.join(self.env.path, 'projects_list')
+        projects_base = os.path.dirname(projects_list)
+        projects_url = 'http://localhost/%s'
+        with open(projects_list, 'w') as f:
             f.write("""
             repos1 user1+<user1@example.com>
             repos2
             """)
-        self.env.config.set('git', 'projects_list', self.projects_list)
-        self.env.config.set('git', 'projects_base', self.projects_base)
-        self.env.config.set('git', 'projects_url', self.projects_url)
+        config = self.env.config['git']
+        config.set('projects_list', projects_list)
+        config.set('projects_base', projects_base)
+        config.set('projects_url', projects_url)
+        repos1_dir = os.path.join(projects_base, 'repos1')
+        repos2_dir = os.path.join(projects_base, 'repos2')
 
         db32.do_upgrade(self.env, VERSION, None)
 
         repos = RepositoryManager(self.env).get_all_repositories()
         self.assertIn('repos1', repos)
-        self.assertFalse(repos['repos1']['sync_per_request'])
-        self.assertEqual(os.path.join(self.projects_base, 'repos1'),
-                         repos['repos1']['dir'])
+        self.assertTrue(repos['repos1']['sync_per_request'])
+        self.assertEqual(repos1_dir, repos['repos1']['dir'])
         self.assertEqual('http://localhost/repos1', repos['repos1']['url'])
         self.assertIn('repos2', repos)
-        self.assertTrue(repos['repos2']['sync_per_request'])
-        self.assertEqual(os.path.join(self.projects_base, 'repos2'),
-                         repos['repos2']['dir'])
+        self.assertFalse(repos['repos2']['sync_per_request'])
+        self.assertEqual(repos2_dir, repos['repos2']['dir'])
         self.assertEqual('http://localhost/repos2', repos['repos2']['url'])
-        self.assertEqual('', self.env.config.get('git', 'projects_list'))
-        self.assertEqual('', self.env.config.get('git', 'projects_base'))
-        self.assertEqual('', self.env.config.get('git', 'projects_url'))
-        self.assertEqual('', self.env.config.get('trac', 'repository_sync_per_request'))
+        config = self.env.config['gitweb-repositories']
+        self.assertNotIn('projects_list', self.env.config)
+        self.assertNotIn('projects_base', self.env.config)
+        self.assertNotIn('projects_url', self.env.config)
+        self.assertNotIn('repository_sync_per_request', self.env.config)
+        self.assertEqual(projects_list, config.get('projects_list'))
+        self.assertEqual(projects_base, config.get('projects_base'))
+        self.assertEqual(projects_url, config.get('projects_url'))
+        self.assertEqual('repos1', config.get('sync_per_request'))
+
+    def test_repository_providers_disabled(self):
+        """Repository configuration is rewritten when repository providers
+        are disabled.
+        """
+        projects_list = os.path.join(self.env.path, 'projects_list')
+        projects_base = os.path.dirname(projects_list)
+        projects_url = 'http://localhost/%s'
+        with open(projects_list, 'w') as f:
+            f.write("""
+            repos1 user1+<user1@example.com>
+            repos2
+            """)
+        config = self.env.config['git']
+        config.set('projects_list', projects_list)
+        config.set('projects_base', projects_base)
+        config.set('projects_url', projects_url)
+        db_provider = DbRepositoryProvider(self.env)
+        db_provider.add_repository('repos3', '/var/git/repos3', 'git')
+        db_provider.add_repository('repos4', '/var/git/repos4', 'git')
+        config = self.env.config['repositories']
+        config.set('repos5.dir', '/var/svn/repos4')
+        config.set('repos5.type', 'svn')
+        config.set('repos6.dir', '/var/svn/repos5')
+        config.set('repos6.type', 'svn')
+        self.env.disable_component(GitwebProjectsRepositoryProvider)
+        self.env.disable_component(DbRepositoryProvider)
+        self.env.disable_component(RepositoryManager)
+
+        db32.do_upgrade(self.env, VERSION, None)
+
+        self.env.enable_component(GitwebProjectsRepositoryProvider)
+        self.env.enable_component(DbRepositoryProvider)
+        self.env.enable_component(RepositoryManager)
+        repos = RepositoryManager(self.env).get_all_repositories()
+        config = self.env.config['gitweb-repositories']
+        self.assertEqual(projects_list, config.get('projects_list'))
+        self.assertEqual(projects_base, config.get('projects_base'))
+        self.assertEqual(projects_url, config.get('projects_url'))
+        self.assertEqual('repos1', config.get('sync_per_request'))
+        self.assertIn('repos1', repos)
+        self.assertTrue(repos['repos1']['sync_per_request'])
+        self.assertIn('repos2', repos)
+        self.assertFalse(repos['repos2']['sync_per_request'])
+        self.assertIn('repos3', repos)
+        self.assertTrue(repos['repos3']['sync_per_request'])
+        self.assertIn('repos4', repos)
+        self.assertFalse(repos['repos4']['sync_per_request'])
+        self.assertIn('repos5', repos)
+        self.assertTrue(repos['repos5']['sync_per_request'])
+        self.assertIn('repos6', repos)
+        self.assertFalse(repos['repos6']['sync_per_request'])
 
 
 def test_suite():
