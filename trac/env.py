42c4cb9761b86b5d21bf6bc7a5ea445fa8f522ae
cboos <cboos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2017-02-01 10:17:19 +0000

    (#12639) adapt to new chrome API
    Also, this reverses r14906, as the builtins don't come for free in Jinja2.
    
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@15424 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/env.py
+++ trac/env.py
@@ -875,14 +875,14 @@ class EnvironmentAdmin(Component):
         # Create and copy scripts
         makedirs(script_target, overwrite=True)
         printout(_("Creating scripts."))
-        data = {'env': self.env, 'executable': sys.executable}
+        data = {'env': self.env, 'executable': sys.executable, 'repr': repr}
         for script in ('cgi', 'fcgi', 'wsgi'):
             dest = os.path.join(script_target, 'trac.' + script)
-            template = Chrome(self.env).load_template('deploy_trac.' + script,
-                                                      'text')
-            stream = template.generate(**data)
+            text = Chrome(self.env).render_template_as_string(
+                Chrome(self.env)._load_jinja_template('jdeploy_trac.' + script),
+                data, method='text')
             with open(dest, 'w') as out:
-                stream.render('text', out=out, encoding='utf-8')
+                out.write(text.encode('utf-8'))
 
     def _do_hotcopy(self, dest, no_db=None):
         if no_db not in (None, '--no-database'):
