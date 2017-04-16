edgewall/trac

commit 08777819b21170d9b06506b15f2bc20aa306ada0

1.2.2dev: Prevent configuration changes from disabling Trac instance
Refs #8790.


git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/1.2-stable@15783 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/env.py
@@ -727,17 +727,21 @@ class Environment(Component, ComponentManager):
         logfile = self.log_file
         if logtype == 'file' and not os.path.isabs(logfile):
             logfile = os.path.join(self.log_dir, logfile)
-        format = self.log_format
-        logid = 'Trac.%s' % hashlib.sha1(self.path).hexdigest()
-        if format:
-            format = format.replace('$(', '%(') \
-                           .replace('%(path)s', self.path) \
-                           .replace('%(basename)s', self.name) \
-                           .replace('%(project)s', self.project_name)
-        self.log, self._log_handler = logger_handler_factory(
-            logtype, logfile, self.log_level, logid, format=format)
+        self.log, self._log_handler = \
+            self.create_logger(self.log_type, logfile, self.log_level)
         self.log.info('-' * 32 + ' environment startup [Trac %s] ' + '-' * 32,
                       self.trac_version)
+
+    def create_logger(self, log_type, log_file, log_level):
+        log_id = 'Trac.%s' % hashlib.sha1(self.path).hexdigest()
+        log_format = self.log_format
+        if log_format:
+            log_format = format.replace('$(', '%(') \
+                               .replace('%(path)s', self.path) \
+                               .replace('%(basename)s', self.name) \
+                               .replace('%(project)s', self.project_name)
+        return logger_handler_factory(log_type, log_file, log_level,
+                                      log_id, format=log_format)
 
     def get_known_users(self, as_dict=False):
         """Returns information about all known users, i.e. users that

trac/admin/tests/web_ui.py
@@ -14,41 +14,167 @@
 import unittest
 
 import trac.tests.compat
-from trac.admin.web_ui import PluginAdminPanel
-from trac.core import Component
+from trac.admin.web_ui import AdminModule, PluginAdminPanel
+from trac.core import Component, TracError
 from trac.test import EnvironmentStub, MockRequest
+from trac.web.api import RequestDone
 
 
 class PluginAdminPanelTestCase(unittest.TestCase):
 
     def setUp(self):
         self.env = EnvironmentStub()
-        self.req = MockRequest(self.env)
-
-    def tearDown(self):
-        self.env.reset_db()
 
     def test_abstract_component_not_visible(self):
         class AbstractComponent(Component):
             abstract = True
+
         class NotAbstractComponent(Component):
             abstract = False
 
+        req = MockRequest(self.env)
         panel = PluginAdminPanel(self.env)
-        data = panel.render_admin_panel(self.req, 'general', 'plugin', None)[1]
+        data = panel.render_admin_panel(req, 'general', 'plugin', None)[1]
 
-        module = self.__class__.__module__
+        module_ = self.__class__.__module__
         components = []
         for plugin in data['plugins']:
-            if module in plugin['modules'].keys():
-                components = plugin['modules'][module]['components'].keys()
+            if module_ in plugin['modules'].keys():
+                components = plugin['modules'][module_]['components'].keys()
         self.assertNotIn('AbstractComponent', components)
         self.assertIn('NotAbstractComponent', components)
+
+
+class LoggingAdminPanelTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.env = EnvironmentStub()
+
+    def test_invalid_log_type_raises(self):
+        """Invalid log type raises TracError."""
+        req = MockRequest(self.env, path_info='/admin/general/logging',
+                          method='POST', args={'log_type': 'invalid'})
+        mod = AdminModule(self.env)
+
+        self.assertTrue(mod.match_request(req))
+        self.assertRaises(TracError, mod.process_request, req)
+
+    def test_invalid_log_level_raises(self):
+        """Invalid log level raises TracError."""
+        req = MockRequest(self.env, path_info='/admin/general/logging',
+                          method='POST',
+                          args={'log_type': 'stderr',
+                                'log_level': 'INVALID'})
+        mod = AdminModule(self.env)
+
+        self.assertTrue(mod.match_request(req))
+        self.assertRaises(TracError, mod.process_request, req)
+
+    def test_empty_log_file_raises(self):
+        """Empty log file raises TracError."""
+        req = MockRequest(self.env, path_info='/admin/general/logging',
+                          method='POST',
+                          args={'log_type': 'file',
+                                'log_level': 'DEBUG',
+                                'log_file': ''})
+        mod = AdminModule(self.env)
+
+        self.assertTrue(mod.match_request(req))
+        self.assertRaises(TracError, mod.process_request, req)
+
+    def test_invalid_log_configuration_not_saved(self):
+        """Invalid log configuration is reverted and not saved."""
+        logging_config = self.env.config['logging']
+        log_type = logging_config.get('log_type')
+        log_file = logging_config.get('log_file')
+        log_level = logging_config.get('log_level')
+        req = MockRequest(self.env, path_info='/admin/general/logging',
+                          method='POST',
+                          args={'log_type': 'file',
+                                'log_level': log_level,
+                                'log_file': '/path/to/invalid/file'})
+        mod = AdminModule(self.env)
+
+        self.assertTrue(mod.match_request(req))
+        self.assertRaises(RequestDone, mod.process_request, req)
+
+        self.assertNotEqual('file', log_type)
+        self.assertEqual(log_type, logging_config.get('log_type'))
+        self.assertEquals(log_level, logging_config.get('log_level'))
+        self.assertEquals(log_file, logging_config.get('log_file'))
+        self.assertEqual(1, len(req.chrome['warnings']))
+        self.assertIn('Changes not saved. Logger configuration error:',
+                      req.chrome['warnings'][0])
+
+    def test_change_log_type(self):
+        """Change the log type."""
+        logging_config = self.env.config['logging']
+        log_type = logging_config.get('log_type')
+        log_file = logging_config.get('log_file')
+        log_level = logging_config.get('log_level')
+        req = MockRequest(self.env, path_info='/admin/general/logging',
+                          method='POST',
+                          args={'log_type': 'file',
+                                'log_level': log_level,
+                                'log_file': log_file})
+        mod = AdminModule(self.env)
+
+        self.assertTrue(mod.match_request(req))
+        self.assertRaises(RequestDone, mod.process_request, req)
+
+        self.assertNotEqual('file', log_type)
+        self.assertEqual('file', logging_config.get('log_type'))
+        self.assertEquals(log_level, logging_config.get('log_level'))
+        self.assertEquals(log_file, logging_config.get('log_file'))
+
+    def test_change_log_level(self):
+        """Change the log level."""
+        logging_config = self.env.config['logging']
+        log_type = logging_config.get('log_type')
+        log_level = logging_config.get('log_level')
+        log_file = logging_config.get('log_file')
+        req = MockRequest(self.env, path_info='/admin/general/logging',
+                          method='POST',
+                          args={'log_type': log_type,
+                                'log_level': 'ERROR',
+                                'log_file': log_file})
+        mod = AdminModule(self.env)
+
+        self.assertTrue(mod.match_request(req))
+        self.assertRaises(RequestDone, mod.process_request, req)
+
+        self.assertEquals(log_type, logging_config.get('log_type'))
+        self.assertNotEqual('ERROR', log_level)
+        self.assertEqual('ERROR', logging_config.get('log_level'))
+        self.assertEquals(log_file, logging_config.get('log_file'))
+
+    def test_change_log_file(self):
+        """Change the log file."""
+        logging_config = self.env.config['logging']
+        log_type = 'file'
+        logging_config.set('log_type', log_type)
+        log_level = logging_config.get('log_level')
+        log_file = logging_config.get('log_file')
+        req = MockRequest(self.env, path_info='/admin/general/logging',
+                          method='POST',
+                          args={'log_type': log_type,
+                                'log_level': log_level,
+                                'log_file': 'trac.log.1'})
+        mod = AdminModule(self.env)
+
+        self.assertTrue(mod.match_request(req))
+        self.assertRaises(RequestDone, mod.process_request, req)
+
+        self.assertEquals(log_type, logging_config.get('log_type'))
+        self.assertEquals(log_level, logging_config.get('log_level'))
+        self.assertNotEqual('trac.log.1', log_file)
+        self.assertEqual('trac.log.1', logging_config.get('log_file'))
 
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(PluginAdminPanelTestCase))
+    suite.addTest(unittest.makeSuite(LoggingAdminPanelTestCase))
     return suite
 
 

trac/admin/web_ui.py
@@ -30,7 +30,8 @@ from trac.perm import PermissionSystem, IPermissionRequestor
 from trac.util.datefmt import all_timezones, pytz
 from trac.util.text import exception_to_unicode, \
                            unicode_to_base64, unicode_from_base64
-from trac.util.translation import _, Locale, get_available_locales, ngettext
+from trac.util.translation import _, Locale, get_available_locales, \
+                                  ngettext, tag_
 from trac.web.api import HTTPNotFound, IRequestHandler, \
                          is_valid_default_handler
 from trac.web.chrome import add_notice, add_stylesheet, \
@@ -280,37 +281,50 @@ class LoggingAdminPanel(Component):
                     _("Unknown log type %(type)s", type=new_type),
                     _("Invalid log type")
                 )
-            if new_type != log_type:
-                self.config.set('logging', 'log_type', new_type)
-                changed = True
-                log_type = new_type
+            new_file = req.args.get('log_file', 'trac.log')
+            if not new_file:
+                raise TracError(_("You must specify a log file"),
+                                _("Missing field"))
+            new_level = req.args.get('log_level')
+            if new_level not in LOG_LEVELS:
+                raise TracError(
+                    _("Unknown log level %(level)s", level=new_level),
+                    _("Invalid log level"))
 
-            if log_type == 'none':
-                self.config.remove('logging', 'log_level')
-                changed = True
+            # Create logger to be sure the configuration is valid.
+            try:
+                self.env.create_logger(new_type, new_file, new_level)
+            except Exception as e:
+                add_warning(req,
+                            tag_("Changes not saved. Logger configuration "
+                                 "error: %(error)s. Inspect the log for more "
+                                 "information.",
+                                 error=tag.code(exception_to_unicode(e))))
+                self.log.error("Logger configuration error: %s",
+                               exception_to_unicode(e, traceback=True))
             else:
-                new_level = req.args.get('log_level')
-                if new_level not in LOG_LEVELS:
-                    raise TracError(
-                        _("Unknown log level %(level)s", level=new_level),
-                        _("Invalid log level"))
-                if new_level != log_level:
-                    self.config.set('logging', 'log_level', new_level)
+                if new_type != log_type:
+                    self.config.set('logging', 'log_type', new_type)
                     changed = True
-                    log_level = new_level
+                    log_type = new_type
 
-            if log_type == 'file':
-                new_file = req.args.get('log_file', 'trac.log')
-                if new_file != log_file:
-                    self.config.set('logging', 'log_file', new_file)
+                if log_type == 'none':
+                    self.config.remove('logging', 'log_level')
                     changed = True
-                    log_file = new_file
-                if not log_file:
-                    raise TracError(_("You must specify a log file"),
-                                    _("Missing field"))
-            else:
-                self.config.remove('logging', 'log_file')
-                changed = True
+                else:
+                    if new_level != log_level:
+                        self.config.set('logging', 'log_level', new_level)
+                        changed = True
+                        log_level = new_level
+
+                if log_type == 'file':
+                    if new_file != log_file:
+                        self.config.set('logging', 'log_file', new_file)
+                        changed = True
+                        log_file = new_file
+                else:
+                    self.config.remove('logging', 'log_file')
+                    changed = True
 
             if changed:
                 _save_config(self.config, req, self.log),

commit 0acb3e13efc764837c581fe5ebbd7c8fe99bea1e

1.2.2dev: Fix `AttributeError` regression in r15783
Refs #8790.


git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/1.2-stable@15785 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/env.py
@@ -736,10 +736,10 @@ class Environment(Component, ComponentManager):
         log_id = 'Trac.%s' % hashlib.sha1(self.path).hexdigest()
         log_format = self.log_format
         if log_format:
-            log_format = format.replace('$(', '%(') \
-                               .replace('%(path)s', self.path) \
-                               .replace('%(basename)s', self.name) \
-                               .replace('%(project)s', self.project_name)
+            log_format = log_format.replace('$(', '%(') \
+                                   .replace('%(path)s', self.path) \
+                                   .replace('%(basename)s', self.name) \
+                                   .replace('%(project)s', self.project_name)
         return logger_handler_factory(log_type, log_file, log_level,
                                       log_id, format=log_format)
 

trac/tests/env.py
@@ -59,8 +59,8 @@ class EmptyEnvironmentTestCase(unittest.TestCase):
 class EnvironmentTestCase(unittest.TestCase):
 
     def setUp(self):
-        env_path = tempfile.mkdtemp(prefix='trac-tempenv-')
-        self.env = Environment(env_path, create=True)
+        self.env_path = tempfile.mkdtemp(prefix='trac-tempenv-')
+        self.env = Environment(self.env_path, create=True)
         self.env.config.set('trac', 'base_url',
                             'http://trac.edgewall.org/some/path')
         self.env.config.save()
@@ -100,6 +100,23 @@ class EnvironmentTestCase(unittest.TestCase):
         self.assertEqual(True, self.env.is_component_enabled(Environment))
         self.assertEqual(False, EnvironmentStub.required)
         self.assertEqual(None, self.env.is_component_enabled(EnvironmentStub))
+
+    def test_log_format(self):
+        """Configure the log_format and log to a file at WARNING level."""
+        self.env.config.set('logging', 'log_type', 'file')
+        self.env.config.set('logging', 'log_level', 'WARNING')
+        self.env.config.set('logging', 'log_format',
+                            'Trac[$(module)s] $(project)s: $(message)s')
+        self.env.config.save()
+        self.env.shutdown()
+        self.env = Environment(self.env_path)  # Reload environment
+
+        self.env.log.warning("The warning message")
+
+        with open(os.path.join(self.env.log_dir, self.env.log_file)) as f:
+            log = f.readlines()
+        self.assertEqual("Trac[env] My Project: The warning message\n",
+                         log[-1])
 
     def test_dumped_values_in_tracini(self):
         parser = RawConfigParser()