fe7848d3d945aa516f0247a1e0344c9a0069014f
jomae <jomae@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2017-05-12 06:49:54 +0000

    1.0.14dev: textarea fields with plain format preserve newlines in ticket and query views (closes #12801)
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/1.0-stable@15891 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/ticket/tests/functional.py
+++ trac/ticket/tests/functional.py
@@ -2339,6 +2339,39 @@ class RegressionTestTicket11996(FunctionalTwillTestCaseSetup):
             self._testenv.set_config('ticket', 'default_milestone', milestone)
 
 
+class RegressionTestTicket12801(FunctionalTwillTestCaseSetup):
+    def runTest(self):
+        """Textarea fields with plain format preserve newlines.
+        Test for regression http://trac.edgewall.org/ticket/12801
+        """
+        env = self._testenv.get_trac_environment()
+        try:
+            env.config.set('ticket-custom', 't12801_plain', 'textarea')
+            env.config.set('ticket-custom', 't12801_plain.format', '')
+            env.config.set('ticket-custom', 't12801_wiki', 'textarea')
+            env.config.set('ticket-custom', 't12801_wiki.format', 'wiki')
+            env.config.save()
+
+            tkt = self._tester.create_ticket(
+                info={'t12801_plain': "- '''plain 1'''\n- ''plain 2''\n",
+                      't12801_wiki': "- ''wiki 1''\n- '''wiki 2'''\n"})
+            tc.find(r"- '''plain 1'''\s*<br />- ''plain 2''")
+            tc.find(r'<li><em>wiki 1</em>\s*'
+                    r'</li><li><strong>wiki 2</strong>\s*</li>')
+
+            tc.go(self._tester.url +
+                  '/query?id=%s&row=t12801_plain&row=t12801_wiki' % tkt)
+            tc.find(r"- '''plain 1'''\s*<br />- ''plain 2''")
+            tc.find(r'<li><em>wiki 1</em>\s*'
+                    r'</li><li><strong>wiki 2</strong>\s*</li>')
+
+        finally:
+            for name in ('t12801_plain', 't12801_plain.format',
+                         't12801_wiki', 't12801_wiki.format'):
+                env.config.remove('ticket-custom', name)
+            env.config.save()
+
+
 def functionalSuite(suite=None):
     if not suite:
         import trac.tests.functional
@@ -2466,6 +2499,7 @@ def functionalSuite(suite=None):
         print "SKIP: RegressionTestTicket11176 (ConfigObj not installed)"
     suite.addTest(RegressionTestTicket11930())
     suite.addTest(RegressionTestTicket11996())
+    suite.addTest(RegressionTestTicket12801())
 
     return suite
 
