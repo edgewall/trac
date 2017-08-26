817f01f2923ed8e2c7657bc0ec8bfe258e77c57c
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2017-08-26 19:12:19 +0000

    1.0.16dev: Print error message when is not installed
    Refs #9903.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/1.0-stable@16276 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/web/standalone.py
+++ trac/web/standalone.py
@@ -27,7 +27,9 @@ import sys
 from SocketServer import ThreadingMixIn
 
 from trac import __version__ as VERSION
+from trac.util.translation import _
 from trac.util import autoreload, daemon
+from trac.util.text import printerr
 from trac.web.auth import BasicAuthentication, DigestAuthentication
 from trac.web.main import dispatch_request
 from trac.web.wsgi import WSGIServer, WSGIRequestHandler
@@ -308,8 +310,13 @@ def main():
             httpd.serve_forever()
     elif options.protocol in ('scgi', 'ajp', 'fcgi'):
         def serve():
-            server_cls = __import__('flup.server.%s' % options.protocol,
-                                    None, None, ['']).WSGIServer
+            try:
+                server_cls = __import__('flup.server.%s' % options.protocol,
+                                        None, None, ['']).WSGIServer
+            except ImportError:
+                printerr(_("Install the flup package to use the '%(protocol)s' "
+                           "protocol", protocol=options.protocol))
+                sys.exit(1)
             flup_app = wsgi_app
             if options.unquote:
                 from trac.web.fcgi_frontend import FlupMiddleware
