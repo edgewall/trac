49a4aae679abaaf21a232bcac7577bd98922c9d9
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2019-08-22 15:41:00 +0000

    1.5.1dev: Remove deprecated signature of `Request.send_error`
    Refs #12787.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@17097 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/web/api.py
+++ trac/web/api.py
@@ -767,48 +767,8 @@ class Request(object):
     def send(self, content, content_type='text/html', status=200):
         self._send(content, content_type, status)
 
-    def send_error(self, exc_info, template='error.html',
-                   content_type='text/html', status=500, env=None, data={}):
-        """Render error page.
-
-        The rendered content can also be passed to the method, when the
-        method is invoked as:
-        `send_error(exc_info, content, content_type='text/html', status=500)`
-
-        :since 1.3.3: the method that renders an error page is deprecated
-            in favor of the method that sends rendered content. The method
-            that renders an error page will be removed in 1.5.1.
-        """
-        try:
-            if isinstance(template, basestring) and template.endswith('.html'):
-                if env:
-                    from trac.web.chrome import Chrome, add_stylesheet
-                    add_stylesheet(self, 'common/css/code.css')
-                    metadata = {'content_type': 'text/html'}
-                    try:
-                        content = Chrome(env).render_template(self, template,
-                                                              data, metadata)
-                    except Exception:
-                        # second chance rendering, in "safe" mode
-                        data['trac_error_rendering'] = True
-                        content = Chrome(env).render_template(self, template,
-                                                              data, metadata)
-                else:
-                    content_type = 'text/plain'
-                    content = '%s\n\n%s: %s' % (data.get('title'),
-                                                data.get('type'),
-                                                data.get('message'))
-            else:
-                # send_error(self, exc_info, content, content_type='text/html',
-                #            status=500)
-                content = template
-        except Exception: # failed to render
-            content = get_last_traceback()
-            content_type = 'text/plain'
-
-        if isinstance(content, unicode):
-            content = content.encode('utf-8')
-
+    def send_error(self, exc_info, content, content_type='text/html',
+                   status=500):
         self._outheaders = []
         self._send(content, content_type, status, exc_info)
 
