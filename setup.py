699c780678a89d3f8bd64507035167b04f65afe2
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2019-10-15 01:16:42 +0000

    1.0.20dev: Prepare for development
    [skip ci]
    
    Refs #13183.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/1.0-stable@17210 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ def readme():
 
 setup(
     name = 'Trac',
-    version = '1.0.19',
+    version = '1.0.20',
     description = 'Integrated SCM, wiki, issue tracker and project environment',
     long_description = readme(),
     author = 'Edgewall Software',
