edgewall/trac

commit 5d5cbfd9a87f6e538767bc378389a6b025e063da

1.2.4dev: Improve error message for incorrect environment version
Refs #13061.


git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/1.2-stable@16782 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/env.py
@@ -35,7 +35,7 @@ from trac.db.api import (DatabaseManager, QueryContextManager,
 from trac.loader import load_components
 from trac.log import logger_handler_factory
 from trac.util import arity, as_bool, copytree, create_file, get_pkginfo, \
-                      is_path_below, lazy, makedirs, read_file
+                      is_path_below, lazy, makedirs
 from trac.util.concurrency import threading
 from trac.util.text import exception_to_unicode, path_to_unicode, printerr, \
                            printout
@@ -418,13 +418,15 @@ class Environment(Component, ComponentManager):
         """Verify that the provided path points to a valid Trac environment
         directory."""
         try:
-            tag = read_file(os.path.join(self.path, 'VERSION')).splitlines()[0]
-            if tag != _VERSION:
-                raise Exception(_("Unknown Trac environment type '%(type)s'",
-                                  type=tag))
+            with open(os.path.join(self.path, 'VERSION')) as f:
+                tag = f.readline().rstrip('\n')
         except Exception as e:
             raise TracError(_("No Trac environment found at %(path)s\n"
-                              "%(e)s", path=self.path, e=e))
+                              "%(e)s",
+                              path=self.path, e=exception_to_unicode(e)))
+        if tag != _VERSION:
+            raise TracError(_("Unknown Trac environment type '%(type)s'",
+                              type=tag))
 
     @lazy
     def db_exc(self):

trac/tests/env.py
@@ -22,6 +22,7 @@ from trac.core import Component, ComponentManager, TracError, implements
 from trac.env import Environment, IEnvironmentSetupParticipant, \
                      ISystemInfoProvider, open_environment
 from trac.test import EnvironmentStub, rmtree
+from trac.util import create_file
 
 
 class EnvironmentCreatedWithoutData(Environment):
@@ -66,6 +67,50 @@ class EnvironmentTestCase(unittest.TestCase):
     def tearDown(self):
         self.env.shutdown() # really closes the db connections
         rmtree(self.env.path)
+
+    def test_invalid_version_raises_trac_error(self):
+        """TracError raised when environment version is invalid."""
+        version = 'Trac Environment Version 0'
+        create_file(os.path.join(self.env.path, 'VERSION'), version + '\n')
+
+        try:
+            env = Environment(self.env.path)
+        except TracError as e:
+            self.assertEqual(
+                "Unknown Trac environment type 'Trac Environment Version 0'",
+                unicode(e))
+        else:
+            env.shutdown()
+            self.fail('TracError not raised.')
+
+    def test_version_file_empty(self):
+        """TracError raised when environment version is empty."""
+        create_file(os.path.join(self.env.path, 'VERSION'), '')
+        try:
+            env = Environment(self.env.path)
+        except TracError as e:
+            self.assertEqual(
+                "Unknown Trac environment type ''",
+                unicode(e))
+        else:
+            env.shutdown()
+            self.fail('TracError not raised.')
+
+    def test_version_file_not_found(self):
+        """TracError raised when environment version file not found."""
+        version_file = os.path.join(self.env.path, 'VERSION')
+        os.remove(version_file)
+        try:
+            env = Environment(self.env.path)
+        except TracError as e:
+            self.assertEqual(
+                "No Trac environment found at %s\n"
+                "IOError: [Errno 2] No such file or directory: %r"
+                % (self.env.path, version_file),
+                unicode(e))
+        else:
+            env.shutdown()
+            self.fail('TracError not raised.')
 
     def test_missing_config_file_raises_trac_error(self):
         """TracError is raised when config file is missing."""

commit 78f80ab30a8d554f771681918eddbe3b45595c97

1.2.7dev: Fixed regression in r16782
Refs #13320.


git-svn-id: http://trac.edgewall.org/intertrac/log:/branches/1.2-stable@17480 af82e41b-90c4-0310-8c96-b1721e28e2e2

trac/env.py
@@ -419,7 +419,7 @@ class Environment(Component, ComponentManager):
         directory."""
         try:
             with open(os.path.join(self.path, 'VERSION')) as f:
-                tag = f.readline().rstrip('\n')
+                tag = f.readline().rstrip()
         except Exception as e:
             raise TracError(_("No Trac environment found at %(path)s\n"
                               "%(e)s",