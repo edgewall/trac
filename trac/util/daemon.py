e3b039fa4fa3fb3e8020eeaab35f6c488213da4f
rjollos <rjollos@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2021-01-02 02:33:13 +0000

    1.5.3dev: Fix `-d/--daemonize` failure for `tracd`
    Refs #13336.
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@17497 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/util/daemon.py
+++ trac/util/daemon.py
@@ -68,12 +68,14 @@ def daemonize(pidfile=None, progname=None, stdin='/dev/null',
     # The process is now daemonized, redirect standard file descriptors
     for stream in sys.stdout, sys.stderr:
         stream.flush()
-    stdin = os.open(stdin, 'rb')
-    stdout = os.open(stdout, 'ab+')
-    stderr = os.open(stderr, 'ab+', 0)
-    os.dup2(stdin.fileno(), sys.stdin.fileno())
-    os.dup2(stdout.fileno(), sys.stdout.fileno())
-    os.dup2(stderr.fileno(), sys.stderr.fileno())
+    stdin = os.open(stdin, os.O_RDONLY)
+    stdout = os.open(stdout,  os.O_RDWR | os.O_APPEND)
+    stderr = os.open(stderr, os.O_RDWR | os.O_APPEND)
+    os.dup2(stdin, sys.stdin.fileno())
+    os.dup2(stdout, sys.stdout.fileno())
+    os.dup2(stderr, sys.stderr.fileno())
+    for fd in stdin, stdout, stderr:
+        os.close(fd)
 
     if pidfile:
         # Register signal handlers to ensure atexit hooks are called on exit
