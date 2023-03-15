c8cb216e52ed0d1006b3d12766430607a2ea45ed
jomae <jomae@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2023-03-15 01:51:29 +0000

    1.6dev: merge [17690] from 1.4-stable (fix for #13576)
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@17691 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- trac/env.py
+++ trac/env.py
@@ -995,7 +995,13 @@ class EnvironmentAdmin(Component):
             template = chrome.load_template('deploy_trac.' + script, text=True)
             text = chrome.render_template_string(template, data, text=True)
 
-            with open(dest, 'w', encoding='utf-8') as out:
+            fd = os.open(dest, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o777)
+            try:
+                out = os.fdopen(fd, 'w', encoding='utf-8')
+            except:
+                os.close(fd)
+                raise
+            with out:
                 out.write(text)
 
     def _do_hotcopy(self, dest, no_db=None):
