19bcd4224e04146b0de81b3c09ad21846e7234d2
jomae <jomae@af82e41b-90c4-0310-8c96-b1721e28e2e2>
2023-05-21 10:17:06 +0000

    1.6dev: workaround for build of Subversion Python bindings failing with SWIG 4.1 since `windows-2022` version `20230326.1` (refs #13602)
    [skip ci]
    
    
    git-svn-id: http://trac.edgewall.org/intertrac/log:/trunk@17700 af82e41b-90c4-0310-8c96-b1721e28e2e2

--- .github/windows-prepare.ps1
+++ .github/windows-prepare.ps1
@@ -75,6 +75,9 @@ if (-not (Verify-Binary)) {
     Expand-Archive -LiteralPath $svnarc -DestinationPath "$workspace"
     Expand-Archive -LiteralPath $sqlite_arc -DestinationPath "$workspace"
     Set-Location -LiteralPath "$workspace\subversion-$svnver"
+    # for Subversion 1.14.2 with SWIG 4.1
+    & svn diff -c1905173 https://svn.apache.org/repos/asf/subversion/branches/1.14.x/subversion/ `
+      | & "C:\Program Files\Git\usr\bin\patch.exe" -d subversion -p0
     & $python gen-make.py --release `
                           --vsnet-version=2019 `
                           "--with-apr=$vcpkg_dir" `
